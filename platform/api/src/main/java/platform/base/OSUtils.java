c3063e391318ddb753bd3cbb114aab27e29dd5d5
CrushBy <crushby@gmail.com>
2010-06-28 13:02:33 +0000

    Сделано, чтобы lpsolve не перезаписывался постоянно при старте

--- platform/api/src/main/java/platform/base/OSUtils.java
+++ platform/api/src/main/java/platform/base/OSUtils.java
@@ -12,20 +12,22 @@ public class OSUtils {
 
         String myLibName = libName + libExtension;
         File file = new File(myLibName);
-        if (!file.exists())
-           file.createNewFile();
-        InputStream in = cls.getResourceAsStream(path + myLibName);
+        if (!file.exists()) { // пока сделаем так, хотя это не очень правильно, так как желательно все-таки обновлять lpsolve
 
-        FileOutputStream out = new FileOutputStream(file);
+            file.createNewFile();
+            InputStream in = cls.getResourceAsStream(path + myLibName);
 
-        byte[] b = new byte[4096];
-        int read;
-        while ((read = in.read(b)) != -1) {
-            out.write(b, 0, read);
+            FileOutputStream out = new FileOutputStream(file);
+
+            byte[] b = new byte[4096];
+            int read;
+            while ((read = in.read(b)) != -1) {
+                out.write(b, 0, read);
+            }
+
+            in.close();
+            out.close();
         }
-
-        in.close();
-        out.close();
 
         System.loadLibrary(libName);
     }
