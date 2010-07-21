360d67cbb13658e7c715f1ed9eaa05c8c2024b28
CrushBy <crushby@gmail.com>
2010-07-21 07:18:46 +0000

    Сделано, чтобы выход из объекта работал по одному нажатию ESC

--- platform/client/src/main/java/platform/client/form/editor/TextFieldPropertyEditor.java
+++ platform/client/src/main/java/platform/client/form/editor/TextFieldPropertyEditor.java
@@ -22,7 +22,7 @@ class TextFieldPropertyEditor extends JFormattedTextField {
     public boolean processKeyBinding(KeyStroke ks, KeyEvent ke, int condition, boolean pressed) {
 
         // не ловим ввод, чтобы его словил сам JTable и обработал
-        return ke.getKeyCode() != KeyEvent.VK_ENTER && super.processKeyBinding(ks, ke, condition, pressed);
+        return (ke.getKeyCode() != KeyEvent.VK_ENTER && ke.getKeyCode() != KeyEvent.VK_ESCAPE) && super.processKeyBinding(ks, ke, condition, pressed);
     }
 
 }
