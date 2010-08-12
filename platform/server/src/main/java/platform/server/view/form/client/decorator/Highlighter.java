1bab2065a0d3d4648849b9857811c8922bc0d871
Valery.Barysok <Valery.Barysok@2236c309-8bcc-6e4b-9233-27fdfdb7650f>
2010-08-12 09:41:25 +0000

    Добавлен пример использования в описание класса.

--- platform/server/src/main/java/platform/server/view/form/client/decorator/Highlighter.java
+++ platform/server/src/main/java/platform/server/view/form/client/decorator/Highlighter.java
@@ -8,6 +8,20 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutputStream;
 
+/**
+ *  Example of use:
+ *
+ *      public DefaultFormView createDefaultRichDesign() {
+ *          DefaultFormView design = super.createDefaultRichDesign();
+ *
+ *          PropertyViewNavigator propertyView = getPropertyView(someLP);
+ *          PropertyCellView cellView = design.get(propertyView);
+ *
+ *          design.get(propertyView.toDraw).gridView.highlighter = new Highlighter(Color.yellow, cellView);
+ *
+ *          return design;
+ *      }
+ */
 public class Highlighter implements ClientSerialize {
     private Color color;
     private PropertyCellView cellView;
