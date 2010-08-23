17ba2faebfcbd44955a48be2ca9935823c340b8c
Valery.Barysok <Valery.Barysok@2236c309-8bcc-6e4b-9233-27fdfdb7650f>
2010-08-23 07:53:04 +0000

    Добавлено запоминание пароля.

--- platform/client/src/main/java/platform/client/LoginDialog.java
+++ platform/client/src/main/java/platform/client/LoginDialog.java
@@ -3,6 +3,7 @@ package platform.client;
 import com.intellij.uiDesigner.core.GridConstraints;
 import com.intellij.uiDesigner.core.GridLayoutManager;
 import com.intellij.uiDesigner.core.Spacer;
+import org.apache.commons.codec.binary.Base64;
 import platform.base.OSUtils;
 import platform.interop.ServerInfo;
 
@@ -25,10 +26,11 @@ public class LoginDialog extends JDialog {
     private JTextField loginField;
     private JPasswordField passwordField;
     private JComboBox serverHost;
+    private JCheckBox savePassword;
     private String waitMessage = "Пожалуйста, подождите...";
 
     public LoginDialog(LoginInfo defaultLoginInfo) {
-        defaultLoginInfo = restoreServerData(defaultLoginInfo);
+        defaultLoginInfo = restoreLoginData(defaultLoginInfo);
         setContentPane(contentPane);
         setAlwaysOnTop(true);
         setUndecorated(true);
@@ -121,7 +123,7 @@ public class LoginDialog extends JDialog {
             }
         }, KeyStroke.getKeyStroke(KeyEvent.VK_ESCAPE, 0), JComponent.WHEN_ANCESTOR_OF_FOCUSED_COMPONENT);
 
-        
+
     }
 
     private boolean isValid(String server) {
@@ -131,7 +133,7 @@ public class LoginDialog extends JDialog {
                 return false;
             }
             try {
-                Integer.parseInt(server.substring(pos+1));
+                Integer.parseInt(server.substring(pos + 1));
             } catch (NumberFormatException e) {
                 return false;
             }
@@ -146,8 +148,7 @@ public class LoginDialog extends JDialog {
         } else {
             if (loginField.getDocument().getLength() <= 0) {
                 return false;
-            }
-            else if (!(item instanceof ServerInfo) && !isValid(item.toString())) {
+            } else if (!(item instanceof ServerInfo) && !isValid(item.toString())) {
                 return false;
             }
         }
@@ -169,7 +170,7 @@ public class LoginDialog extends JDialog {
         Object item = serverHost.getSelectedItem();
         ServerInfo serverInfo;
         if (item instanceof ServerInfo) {
-            serverInfo = (ServerInfo)item;
+            serverInfo = (ServerInfo) item;
         } else {
             serverInfo = getServerInfo(item.toString());
         }
@@ -185,28 +186,48 @@ public class LoginDialog extends JDialog {
         try {
             FileWriter fileWr = new FileWriter(OSUtils.getUserFile(configName));
             fileWr.write(result.getServerHost() + '\n');
-            fileWr.write(result.getServerPort());
+            fileWr.write(result.getServerPort() + '\n');
+            fileWr.write(result.getUserName() + '\n');
+            fileWr.write(String.valueOf(savePassword.isSelected()) + '\n');
+            if (savePassword.isSelected()) {
+                fileWr.write(Base64.encodeBase64String(result.getPassword().getBytes()) + '\n');
+            }
             fileWr.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private LoginInfo restoreServerData(LoginInfo loginInfo) {
+    private LoginInfo restoreLoginData(LoginInfo loginInfo) {
         try {
             File file = OSUtils.getUserFile(configName, false);
             if (file.exists()) {
                 FileReader fileRd = new FileReader(file);
                 Scanner scanner = new Scanner(fileRd);
-                String serverHost = scanner.nextLine();
+                String serverHost = scanner.hasNextLine() ? scanner.nextLine() : "";
                 if (serverHost.isEmpty()) {
                     serverHost = loginInfo.getServerHost();
                 }
-                String serverPort = scanner.nextLine();
+                String serverPort = scanner.hasNextLine() ? scanner.nextLine() : "";
                 if (serverPort.isEmpty()) {
                     serverPort = loginInfo.getServerPort();
                 }
-                return new LoginInfo(serverHost, serverPort, loginInfo.getUserName(), loginInfo.getPassword());
+                String userName = scanner.hasNextLine() ? scanner.nextLine() : "";
+                if (userName.isEmpty()) {
+                    userName = loginInfo.getUserName();
+                }
+                String savePwd = scanner.hasNextLine() ? scanner.nextLine() : "";
+                savePassword.setSelected(Boolean.valueOf(savePwd));
+                String password;
+                if (scanner.hasNextLine()) {
+                    password = scanner.nextLine();
+                    byte[] pass = Base64.decodeBase64(password);
+                    password = new String(pass);
+                } else {
+                    password = loginInfo.getPassword();
+                }
+
+                return new LoginInfo(serverHost, serverPort, userName, password);
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -222,7 +243,7 @@ public class LoginDialog extends JDialog {
             return new ServerInfo(server, server, Integer.parseInt(System.getProperty("platform.client.hostport", "7652")));
         }
 
-        return new ServerInfo(server, server.substring(0, pos), Integer.parseInt(server.substring(pos+1)));
+        return new ServerInfo(server, server.substring(0, pos), Integer.parseInt(server.substring(pos + 1)));
     }
 
     private void onCancel() {
@@ -267,7 +288,7 @@ public class LoginDialog extends JDialog {
         label1.setText("");
         contentPane.add(label1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         final JPanel panel1 = new JPanel();
-        panel1.setLayout(new GridLayoutManager(2, 1, new Insets(4, 4, 4, 4), -1, -1));
+        panel1.setLayout(new GridLayoutManager(3, 1, new Insets(4, 4, 4, 4), -1, -1));
         contentPane.add(panel1, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, null, new Dimension(24, 48), null, 0, false));
         final JPanel panel2 = new JPanel();
         panel2.setLayout(new GridLayoutManager(3, 2, new Insets(0, 0, 0, 0), -1, -1));
@@ -290,7 +311,7 @@ public class LoginDialog extends JDialog {
         panel2.add(serverHost, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         final JPanel panel3 = new JPanel();
         panel3.setLayout(new GridLayoutManager(1, 2, new Insets(0, 0, 0, 0), -1, -1));
-        panel1.add(panel3, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, 1, null, null, null, 0, false));
+        panel1.add(panel3, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, 1, null, null, null, 0, false));
         final Spacer spacer1 = new Spacer();
         panel3.add(spacer1, new GridConstraints(0, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_WANT_GROW, 1, null, null, null, 0, false));
         final JPanel panel4 = new JPanel();
@@ -306,6 +327,9 @@ public class LoginDialog extends JDialog {
         buttonCancel.setMnemonic('C');
         buttonCancel.setDisplayedMnemonicIndex(0);
         panel4.add(buttonCancel, new GridConstraints(0, 1, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
+        savePassword = new JCheckBox();
+        savePassword.setText("Запомнить пароль");
+        panel1.add(savePassword, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_WEST, GridConstraints.FILL_NONE, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
         label2.setLabelFor(loginField);
         label3.setLabelFor(passwordField);
     }
