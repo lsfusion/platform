1941939929ac8fa33e0611c802f5e5bfae3bd520
AlexKirkouski <alex.kirkouski@gmail.com>
2010-10-12 14:28:27 +0000

    Устранена проблема hierarchy is inconsistent

--- platform/client/src/main/java/platform/client/serialization/ClientIdentitySerializable.java
+++ platform/client/src/main/java/platform/client/serialization/ClientIdentitySerializable.java
@@ -2,5 +2,5 @@ package platform.client.serialization;
 
 import platform.interop.serialization.IdentitySerializable;
 
-public interface ClientIdentitySerializable extends ClientCustomSerializable, IdentitySerializable<ClientSerializationPool> {
+public interface ClientIdentitySerializable extends IdentitySerializable<ClientSerializationPool> {
 }
