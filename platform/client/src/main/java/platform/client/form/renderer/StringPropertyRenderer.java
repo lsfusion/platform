7fabe49fa5b6a3a86bd97201c7963a166d38e9d0
Fantast <fantasteg@gmail.com>
2010-12-06 12:41:44 +0000

    Обработка null-значений строк.

--- platform/client/src/main/java/platform/client/form/renderer/StringPropertyRenderer.java
+++ platform/client/src/main/java/platform/client/form/renderer/StringPropertyRenderer.java
@@ -1,13 +1,20 @@
 package platform.client.form.renderer;
 
 import platform.client.form.PropertyRendererComponent;
+import platform.client.form.editor.StringPropertyEditor;
 import platform.interop.ComponentDesign;
 
 import javax.swing.*;
+import java.awt.*;
 import java.text.Format;
 
 public class StringPropertyRenderer extends LabelPropertyRenderer
-                             implements PropertyRendererComponent {
+        implements PropertyRendererComponent {
+
+    public static final String EMPTY_STRING = "Неопределено";
+
+    private static final Color normalForeground = UIManager.getColor("TextField.foreground");
+    private static final Color inactiveForeground = UIManager.getColor("TextField.inactiveForeground");
 
     public StringPropertyRenderer(Format iformat, ComponentDesign design) {
         super(iformat, design);
@@ -21,11 +28,13 @@ public class StringPropertyRenderer extends LabelPropertyRenderer
     }
 
     public void setValue(Object value, boolean isSelected, boolean hasFocus) {
-        if (value != null)
+        if (value != null) {
+            setForeground(normalForeground);
             setText(value.toString());
-        else
-            setText("");
+        } else {
+            setForeground(inactiveForeground);
+            setText(EMPTY_STRING);
+        }
         setSelected(isSelected, hasFocus);
     }
-
 }
