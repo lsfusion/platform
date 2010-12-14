7ad370a0a136bc2274497962e94453c0706b843a
Fantast <fantasteg@gmail.com>
2010-12-14 15:55:43 +0000

    Логирование select-запросов.

--- platform/server/src/main/java/platform/server/data/SQLSessionLoggerAspect.java
+++ platform/server/src/main/java/platform/server/data/SQLSessionLoggerAspect.java
@@ -16,10 +16,14 @@ public class SQLSessionLoggerAspect {
         return executeMethodAndLogTime(thisJoinPoint, queryString);
     }
 
-
     @Around("execution(int platform.server.data.SQLSession.executeDML(java.lang.String, java.util.Map)) && args(queryString, params)")
     public Object executeDML(ProceedingJoinPoint thisJoinPoint, String queryString, Map params) throws Throwable {
         return executeMethodAndLogTime(thisJoinPoint, queryString);
+    }
+
+    @Around("execution(* platform.server.data.SQLSession.executeSelect(java.lang.String, ..)) && args(select, ..)")
+    public Object executeSelect(ProceedingJoinPoint thisJoinPoint, String select) throws Throwable {
+        return executeMethodAndLogTime(thisJoinPoint, select);
     }
 
     private Object executeMethodAndLogTime(ProceedingJoinPoint thisJoinPoint, String queryString) throws Throwable {
