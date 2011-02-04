9d84fcd1e9c3a56af75e457c1578ae6060794f34
pavel-miniutka <asphalt.by@gmail.com>
2011-02-04 14:04:00 +0000

    запоминание директории FileActionPropertyEditor

--- platform/client/src/main/java/platform/client/form/editor/FileActionPropertyEditor.java
+++ platform/client/src/main/java/platform/client/form/editor/FileActionPropertyEditor.java
@@ -6,8 +6,10 @@ import platform.client.form.PropertyEditorComponent;
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import java.awt.*;
+import java.io.File;
 import java.io.IOException;
 import java.util.EventObject;
+import java.util.prefs.Preferences;
 
 public class FileActionPropertyEditor extends JFileChooser
         implements PropertyEditorComponent {
@@ -16,6 +18,9 @@ public class FileActionPropertyEditor extends JFileChooser
 
     public FileActionPropertyEditor(String filterDescription, String... fileExtensions) {
         super();
+
+        Preferences preferences = Preferences.userNodeForPackage(this.getClass());
+        setCurrentDirectory(new File(preferences.get("LATEST_DIRECTORY", "")));
 
         if (filterDescription == null || filterDescription.isEmpty()) {
             filterDescription = "Все файлы";
@@ -32,6 +37,8 @@ public class FileActionPropertyEditor extends JFileChooser
 
     public Object getCellEditorValue() {
         try {
+            Preferences preferences = Preferences.userNodeForPackage(this.getClass()); 
+            preferences.put("LATEST_DIRECTORY", this.getSelectedFile().getAbsolutePath());
             return returnValue == JFileChooser.APPROVE_OPTION ? IOUtils.getFileBytes(this.getSelectedFile()) : null;
         } catch (IOException e) {
             throw new RuntimeException("Не могу прочитать файл: " + this.getSelectedFile());
