4e15b65cfda14d23a0ce78f913ae81f7101e7fb5
CrushBy <crushby@gmail.com>
2011-06-15 08:45:39 +0000

    Изменения по RomanBusinessLogics

--- platform/roman-logics/src/main/java/roman/RomanLogicsModule.java
+++ platform/roman-logics/src/main/java/roman/RomanLogicsModule.java
@@ -1338,10 +1338,12 @@ public class RomanLogicsModule extends LogicsModule {
 
         addConstraint(addJProp("Выбранный для товара ТН ВЭД должен иметь верхний элемент", baseLM.andNot1, customCategory10Sku, 1, customCategory9Sku, 1), false);
 
-        addConstraint(addJProp("Выбранное деление должно быть связано с ТН ВЭД товара", and(false, false, true), addCProp(LogicalClass.instance, true, sku), 1,
+        addConstraint(addJProp("Категория минимальных предельных цен запрещена для выбранного кода ТНВЭД", and(false, false, true), addCProp(LogicalClass.instance, true, sku), 1,
                    customCategory10Sku, 1,
                    subCategorySku, 1,
-                   addJProp(relationCustomCategory10SubCategory, customCategory10Sku, 1, subCategorySku, 1), 1), true);                
+                   addJProp(relationCustomCategory10SubCategory, customCategory10Sku, 1, subCategorySku, 1), 1), true);
+       /*addConstraint(addJProp("Выбранный должен быть среди связанных кодов", andNot1, addCProp(LogicalClass.instance, true, article), 1,
+                   addJProp(relationCustomCategory10CustomCategoryOrigin, customCategory10Article, 1, customCategoryOriginArticle, 1), 1), true);*/
 
         // unitOfMeasure
         unitOfMeasureCategory = addDProp(idGroup, "unitOfMeasureCategory", "Единица измерения (ИД)", unitOfMeasure, category);
