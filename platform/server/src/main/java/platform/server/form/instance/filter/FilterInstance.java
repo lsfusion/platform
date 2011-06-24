822329a755d9a187e986126d346399c5fff6d19c
pavel-miniutka <asphalt.by@gmail.com>
2011-06-24 12:31:44 +0000

    исправление фильтров с нулевым значением

--- platform/server/src/main/java/platform/server/form/instance/filter/FilterInstance.java
+++ platform/server/src/main/java/platform/server/form/instance/filter/FilterInstance.java
@@ -41,10 +41,11 @@ public abstract class FilterInstance implements Updated {
                 return new AndFilterInstance(inStream, form);
             case FilterType.COMPARE:
                 CompareFilterInstance filter = new CompareFilterInstance(inStream, form);
-                if (filter.value instanceof NullValue)
+                if (!filter.negate) {
                     return new NotFilterInstance(new NotNullFilterInstance(filter.property));
-                else
-                    return filter;
+                } else {
+                    return new NotNullFilterInstance(filter.property);
+                }
             case FilterType.NOTNULL:
                 return new NotNullFilterInstance(inStream, form);
             case FilterType.ISCLASS:
