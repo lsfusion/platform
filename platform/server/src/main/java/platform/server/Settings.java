0d16739d41bfccdf49fdee9144054a15af3e4194
AlexKirkouski <alex.kirkouski@gmail.com>
2011-06-27 13:43:55 +0000

    Включил pack побольше

--- platform/server/src/main/java/platform/server/Settings.java
+++ platform/server/src/main/java/platform/server/Settings.java
@@ -25,7 +25,7 @@ public class Settings {
         this.innerGroupExprs = innerGroupExprs;
     }
 
-    public int packOnCacheComplexity = 300;
+    public int packOnCacheComplexity = 1200;
 
     public int getPackOnCacheComplexity() {
         return packOnCacheComplexity;
