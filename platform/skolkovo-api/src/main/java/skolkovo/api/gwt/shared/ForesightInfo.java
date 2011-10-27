e650268e2ee9ab27cedcd7237ba70562a708639b
CrushBy <crushby@gmail.com>
2011-10-27 06:55:35 +0000

    В ExpertProfile добавлена информация по форсайтам

---
+++ platform/skolkovo-api/src/main/java/skolkovo/api/gwt/shared/ForesightInfo.java
@@ -0,0 +1,11 @@
+package skolkovo.api.gwt.shared;
+
+import java.io.Serializable;
+
+public class ForesightInfo implements Serializable {
+    public String sID;
+    public String name;
+
+    public boolean selected;
+    public String substantiation;
+}
--- platform/skolkovo-api/src/main/java/skolkovo/api/gwt/shared/ProfileInfo.java
+++ platform/skolkovo-api/src/main/java/skolkovo/api/gwt/shared/ProfileInfo.java
@@ -6,5 +6,9 @@ public class ProfileInfo implements Serializable {
     public String expertEmail;
     public String expertName;
 
+    public boolean technical;
+    public boolean business;
+
     public VoteInfo voteInfos[];
+    public ForesightInfo foresightInfos[];
 }
