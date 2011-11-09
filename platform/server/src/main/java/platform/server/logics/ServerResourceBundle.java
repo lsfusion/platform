375530a311d64d95f208fd6a53e73352c8317e1b
Fantast <fantasteg@gmail.com>
2011-11-09 11:38:28 +0000

    Convert bundles to ANSI.

--- platform/server/src/main/java/platform/server/logics/ServerResourceBundle.java
+++ platform/server/src/main/java/platform/server/logics/ServerResourceBundle.java
@@ -1,32 +1,26 @@
 package platform.server.logics;
 
 
-import platform.server.Utf8ResourceBundle;
-
 import java.text.MessageFormat;
 import java.util.Locale;
 import java.util.ResourceBundle;
 
 public class ServerResourceBundle {
-
-    private static ResourceBundle serverResourceBundle = Utf8ResourceBundle.getBundle("ServerResourceBundle", new Locale("ru", ""));
+    private static ResourceBundle serverResourceBundle = ResourceBundle.getBundle("ServerResourceBundle", new Locale("ru", ""));
 
     public static String getString(String key) {
         return serverResourceBundle.getString(key);
     }
 
-    public static String getString(String key, Object ... params) {
-        String returnString = serverResourceBundle.getString(key);
-        return MessageFormat.format(returnString, params);
+    public static String getString(String key, Object... params) {
+        return MessageFormat.format(getString(key), params);
     }
 
     public static void load(String locale) {
         if (locale.equals("")) {
-            serverResourceBundle = Utf8ResourceBundle.getBundle("ServerResourceBundle", Locale.getDefault());
+            serverResourceBundle = ResourceBundle.getBundle("ServerResourceBundle", Locale.getDefault());
         } else {
-            serverResourceBundle = Utf8ResourceBundle.getBundle("ServerResourceBundle", new Locale(locale, ""));
-            //serverResourceBundle = Utf8ResourceBundle.getBundle("ServerResourceBundle", Locale.US);
+            serverResourceBundle = ResourceBundle.getBundle("ServerResourceBundle", new Locale(locale, ""));
         }
     }
-
 }
