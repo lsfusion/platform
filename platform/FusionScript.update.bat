13dfea9c73d77b21e827efe30725620465ec1e75
Fantast <fantasteg@gmail.com>
2012-02-24 14:52:40 +0000

    fix

--- platform/FusionScript.update.bat
+++ platform/FusionScript.update.bat
@@ -1,4 +1,4 @@
 mkdir "%USERPROFILE%\.IntelliJIdea10\config\filetypes"
 mkdir "%USERPROFILE%\.IntelliJIdea11\config\filetypes"
-copy /Y FusionScript.xml "%USERPROFILE%\.IntelliJIdea10\config\filetypes\FusionScripts.xml"
-copy /Y FusionScript.xml "%USERPROFILE%\.IntelliJIdea11\config\filetypes\FusionScripts.xml"
+copy /Y FusionScript.xml "%USERPROFILE%\.IntelliJIdea10\config\filetypes\FusionScript.xml"
+copy /Y FusionScript.xml "%USERPROFILE%\.IntelliJIdea11\config\filetypes\FusionScript.xml"
