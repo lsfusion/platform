4b624a7779bb39a7663826257df228e26d7a42cd
kostelecki <kostelecki@2236c309-8bcc-6e4b-9233-27fdfdb7650f>
2012-07-24 14:19:06 +0000

    поправки

--- platform/roman-logics/src/main/java/roman/RomanLogicsModule.java
+++ platform/roman-logics/src/main/java/roman/RomanLogicsModule.java
@@ -567,6 +567,8 @@ public class RomanLogicsModule extends LogicsModule {
     LCP addressExporterFreight;
     public LCP customStoreFreight;
     public LCP nameCustomStoreFreight;
+    public LCP warehouseFreight;
+    public LCP nameWarehouseFreight;
 
     private LCP inInvoiceFreight;
     private LCP netWeightInvoicedFreight;
@@ -2981,6 +2983,9 @@ public class RomanLogicsModule extends LogicsModule {
 
         customStoreFreight = addDProp(idGroup, "customStoreFreight", "СВХ (ИД)", customStore, freight);
         nameCustomStoreFreight = addJProp(baseGroup, "nameCustomStoreFreight", "СВХ", baseLM.name, customStoreFreight, 1);
+
+        warehouseFreight  = addDProp(idGroup, "warehouseFreight", "Склад фрахта (ИД)", BL.getModule("MasterData").getClassByName("warehouse"), freight);
+        nameWarehouseFreight = addJProp(baseGroup, "nameWarehouseFreight", "Склад фрахта", baseLM.name, warehouseFreight, 1);
 
         inInvoiceFreight = addDProp(baseGroup, "inInvoiceFreight", "Вкл.", LogicalClass.instance, invoice, freight);
         netWeightInvoicedFreight = addSGProp(baseGroup, "netWeightInvoicedFreight", "Вес инвойсов", addJProp(baseLM.and1, netWeightDocument, 1, inInvoiceFreight, 1, 2), 2);
@@ -7804,7 +7809,7 @@ public class RomanLogicsModule extends LogicsModule {
         private FreightCreateFormEntity(NavigatorElement<RomanBusinessLogics> parent, String sID, String caption) {
             super(parent, sID, caption);
 
-            objFreight = addSingleGroupObject(freight, "Фрахт", baseLM.date, baseLM.objectClassName, dateArrivalFreight, nameCustomStoreFreight, nameRouteFreight, nameExporterFreight, descriptionFreight, tonnageDataFreight, volumeDataFreight, palletCountDataFreight, nameCurrencyFreight, sumFreightFreight);
+            objFreight = addSingleGroupObject(freight, "Фрахт", baseLM.date, baseLM.objectClassName, dateArrivalFreight, nameCustomStoreFreight, nameWarehouseFreight, nameRouteFreight, nameExporterFreight, descriptionFreight, tonnageDataFreight, volumeDataFreight, palletCountDataFreight, nameCurrencyFreight, sumFreightFreight);
 //            addObjectActions(this, objFreight);
             objFreight.groupTo.setSingleClassView(ClassViewType.PANEL);
 
@@ -7843,7 +7848,7 @@ public class RomanLogicsModule extends LogicsModule {
         private FreightListFormEntity(NavigatorElement<RomanBusinessLogics> parent, String sID, String caption) {
             super(parent, sID, caption);
 
-            objFreight = addSingleGroupObject("freight", freight, "Фрахт", baseLM.date, baseLM.objectClassName, dateArrivalFreight, nameCustomStoreFreight, formLogFreight, nameRouteFreight, nameExporterFreight, descriptionFreight, netWeightInvoicedFreight, grossWeightFreight, palletNumberFreight, freightBoxNumberFreight);
+            objFreight = addSingleGroupObject("freight", freight, "Фрахт", baseLM.date, baseLM.objectClassName, dateArrivalFreight, nameCustomStoreFreight, nameWarehouseFreight, formLogFreight, nameRouteFreight, nameExporterFreight, descriptionFreight, netWeightInvoicedFreight, grossWeightFreight, palletNumberFreight, freightBoxNumberFreight);
             objFreight.groupTo.setSingleClassView(ClassViewType.GRID);
             setEditType(objFreight, PropertyEditType.READONLY);
             setEditType(formLogFreight, PropertyEditType.EDITABLE);
