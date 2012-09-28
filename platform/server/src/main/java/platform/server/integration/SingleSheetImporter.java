5267bdba53b20087d85dda24a98258912d780427
CrushBy <crushby@gmail.com>
2012-09-28 15:52:29 +0000

    Мелкие изменения в разных логиках

--- platform/server/src/main/java/platform/server/integration/SingleSheetImporter.java
+++ platform/server/src/main/java/platform/server/integration/SingleSheetImporter.java
@@ -17,7 +17,7 @@ import java.util.Map;
 public abstract class SingleSheetImporter {
     public final static int A = 0, B = 1, C = 2, D = 3, E = 4, F = 5, G = 6, H = 7, I = 8, J = 9, K = 10, L = 11, M = 12,
             N = 13, O = 14, P = 15, Q = 16, R = 17, S = 18, T = 19, U = 20, V = 21, W = 22, X = 23, Y = 24, Z = 25,
-            AA = 26, AB = 27, AC = 28, AD = 29, AE = 30, AF = 31, AG = 32, AH = 33, AI = 34, AJ = 35, AO = 40,
+            AA = 26, AB = 27, AC = 28, AD = 29, AE = 30, AF = 31, AG = 32, AH = 33, AI = 34, AJ = 35, AK = 36, AL = 37, AM = 38, AN = 39, AO = 40,
             AQ = 42, AS = 44, AX = 49;
 
     protected ImportInputTable inputTable;
