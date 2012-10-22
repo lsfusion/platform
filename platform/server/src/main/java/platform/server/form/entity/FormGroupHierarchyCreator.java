bb5645cd87b13e440e38df89f764a34c1b6ab4f6
danchanka <danchenko@gmail.com>
2012-10-22 13:54:53 +0000

    [Reports] Изменил алгоритм формирования иерархии групп

--- platform/server/src/main/java/platform/server/form/entity/FormGroupHierarchyCreator.java
+++ platform/server/src/main/java/platform/server/form/entity/FormGroupHierarchyCreator.java
@@ -44,6 +44,11 @@ public class FormGroupHierarchyCreator {
         return groupsSet;
     }
 
+    /**
+     * Строим граф по зависимостям между GroupObjectEntity.
+     * Если две группы связаны каким-нибудь свойством, фильтром и т.п., то добавляется ребро от "нижней" группы к "верхней"
+     * Порядок групп определяется порядком в form.groups
+     */
     private void addDependenciesToGraph(Map<GroupObjectEntity, Set<GroupObjectEntity>> graph) {
         for (PropertyDrawEntity<?> property : form.propertyDraws) {
             if (property.getToDraw(form) == property.propertyObject.getApplyObject(form.groups)) {
@@ -107,30 +112,83 @@ public class FormGroupHierarchyCreator {
         return graph;
     }
 
+    /**
+     * Формирование графа зависимостей в виде леса (набора деревьев)
+     *
+     * Алгоритм:
+     * Инвариант, который на каждом шаге алгоритма сохраняем: строящийся граф должен быть лесом на каждом шаге, то есть
+     * в каждую вершину не может входить более одного ребра.
+     *
+     * Перебираем все группы в том же порядке в котором они находятся в form.groups.
+     * Для каждой группы перебираем все исходящие ребра (то есть группы, от которых текущая группа зависит)
+     * Каждое ребро (шаг алгоритма) пробуем добавить в результирующий граф, инвертируя его. Если получаем нарушение
+     * инварианта, то берем это ребро, и ребро уже входящее в текущую группу, и начинаем по ним подниматься вверх по дереву
+     * пока не найдем ближайшего общего предка (LCA).
+     *      1. Если этот предок является одной из вершин двух исходных ребер, то это означает, что путь между этими вершинами
+     *      уже существует, следовательно одно из ребер надо просто выкинуть
+     *      2. Если же предком является какая-то другая вершина, или предок вообще не найден, то нам нужно объединить все
+     *      вершины обоих путей в цепочку
+     *
+     * @param graph Инвертированный ациклический граф зависимостей между группами (в виде списков смежности).
+     *              Все ребра идут от групп с большим индексом в form.groups к группам с меньшим индексом
+     * @return Результирующий граф зависимостей в виде набора деревьев (в виде списков смежжнсти)
+     */
+
     private Map<GroupObjectEntity, Set<GroupObjectEntity>> formForest(Map<GroupObjectEntity, Set<GroupObjectEntity>> graph) {
         int groupsCount = form.groups.size();
-        Set<GroupObjectEntity> innerGroups = new HashSet<GroupObjectEntity>();
         Map<GroupObjectEntity, Set<GroupObjectEntity>> newGraph = createNewGraph();
 
-        for (int i = groupsCount - 1; i >= 0; --i) {
-            if (!innerGroups.contains(form.groups.get(i))) {
-                Set<GroupObjectEntity> was = new HashSet<GroupObjectEntity>();
-                Queue<GroupObjectEntity> queue = new ArrayDeque<GroupObjectEntity>();
-                was.add(form.groups.get(i));
-                queue.add(form.groups.get(i));
-                while (!queue.isEmpty()) {
-                    GroupObjectEntity group = queue.remove();
-                    for (GroupObjectEntity nextGroup : graph.get(group)) {
-                        if (!was.contains(nextGroup)) {
-                            was.add(nextGroup);
-                            queue.add(nextGroup);
+        int[] parents = new int[groupsCount];
+        Arrays.fill(parents, -1);
+
+        for (int groupIndex = 0; groupIndex < groupsCount; groupIndex++) {
+            GroupObjectEntity currentGroup = form.groups.get(groupIndex);
+            for (GroupObjectEntity parentGroup : graph.get(currentGroup)) {
+                int parentIndex = form.groups.indexOf(parentGroup);
+                if (parents[groupIndex] == -1) {
+                    parents[groupIndex] = parentIndex;
+                } else {
+                    List<Integer> visited = new ArrayList<Integer>();
+                    int i1 = parents[groupIndex];
+                    int i2 = parentIndex;
+                    while (parents[i1] != parents[i2]) {
+                        if (parents[i1] > parents[i2]) {
+                            visited.add(i1);
+                            i1 = parents[i1];
+                       } else {
+                            visited.add(i2);
+                            i2 = parents[i2];
+                        }
+                    }
+
+                    if (i1 == i2) { // если общим предком является одна из вершин двух исходных ребер
+                        if (i1 == parents[groupIndex]) {  // если общий предок является старым предком
+                            parents[groupIndex] = parentIndex;
+                        }
+                    } else {
+                        visited.add(groupIndex);
+                        visited.add(i1);
+                        visited.add(i2);
+                        if (parents[i1] != -1) {
+                            visited.add(parents[i1]);
+                        }
+                        Collections.sort(visited);  // объединяем два пути в одну цепочку
+                        for (int i = 0; i+1 < visited.size(); i++) {
+                            parents[visited.get(i+1)] = visited.get(i);
                         }
                     }
                 }
-                innerGroups.addAll(was);
-                addDependencies(newGraph, was, false);
             }
         }
+        // формируем результирующий граф
+        for (int i = 0; i < parents.length; i++) {
+            if (parents[i] != -1) {
+                GroupObjectEntity from = form.groups.get(parents[i]);
+                GroupObjectEntity to = form.groups.get(i);
+                newGraph.get(from).add(to);
+            }
+        }
+
         return newGraph;
     }
 
