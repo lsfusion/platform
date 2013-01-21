a1c2839ed7defa70df9419982295dfa0a627de4d
Vandrouny <filipchyk.andrei@gmail.com>
2013-01-21 14:42:04 +0000

    Чтобы работали объекты

--- platform/server/src/main/java/platform/server/classes/CustomClass.java
+++ platform/server/src/main/java/platform/server/classes/CustomClass.java
@@ -344,6 +344,7 @@ public abstract class CustomClass extends ImmutableObject implements ObjectClass
             }
 
             form = createDefaultForm(LM);
+            LM.addFormEntity(form.form);
 
             return form;
         }
