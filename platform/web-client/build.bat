d085bdedcbf374ece06e3ecafbef1b1c964cf283
CrushBy <crushby@gmail.com>
2013-03-04 14:51:33 +0000

    Изменены bat-файл для сборки проекта

--- platform/web-client/build.bat
+++ platform/web-client/build.bat
@@ -3,10 +3,8 @@ set BUILD_DIR=%~dp0
 cd %BUILD_DIR%\..
 call mvn clean install
 
-cd %BUILD_DIR%
-call mvn install -P assemble,pack
-
 cd %BUILD_DIR%\..\desktop-client
-call mvn install -P assemble,sign,pack
+call assemble
 
 cd %BUILD_DIR%
+call assemble
