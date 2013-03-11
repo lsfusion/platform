8aa33dc230cc3e4ab3198efd843b2672398243be
Fantast <fantasteg@gmail.com>
2013-03-11 13:25:32 +0000

    logging fix.

--- platform/server/src/main/java/platform/server/logics/security/LogOutActionProperty.java
+++ platform/server/src/main/java/platform/server/logics/security/LogOutActionProperty.java
@@ -5,12 +5,9 @@ import platform.server.classes.ValueClass;
 import platform.server.logics.SecurityLogicsModule;
 import platform.server.logics.property.ClassPropertyInterface;
 import platform.server.logics.property.ExecutionContext;
-import platform.server.logics.property.actions.AdminActionProperty;
 import platform.server.logics.scripted.ScriptingActionProperty;
 
 import java.sql.SQLException;
-
-import static platform.server.logics.ServerResourceBundle.getString;
 
 public class LogOutActionProperty extends ScriptingActionProperty {
     public LogOutActionProperty(SecurityLogicsModule LM) {
