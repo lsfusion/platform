f42cf1d34f6972edff2c323aee40de7febee8fae
pavel-miniutka <asphalt.by@gmail.com>
2013-03-14 08:25:22 +0000

    [Web]: автозаполнение client.jnlp

---
+++ platform/web-api/src/main/java/platform/gwt/base/server/handlers/JNLPRequestHandler.java
@@ -0,0 +1,57 @@
+package platform.gwt.base.server.handlers;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.PropertyPlaceholderHelper;
+import org.springframework.web.HttpRequestHandler;
+import platform.base.IOUtils;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+
+import static platform.base.BaseUtils.nvl;
+
+public class JNLPRequestHandler implements HttpRequestHandler {
+    protected final static Logger logger = Logger.getLogger(JNLPRequestHandler.class);
+
+    private static final PropertyPlaceholderHelper stringResolver = new PropertyPlaceholderHelper("${", "}", ":", true);
+
+    @Autowired
+    private ServletContext servletContext;
+
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        logger.debug("Handling jnlp request");
+
+        try {
+            StringBuffer requestURL = request.getRequestURL();
+            Properties properties = new Properties();
+            properties.put("codebase.url", requestURL.substring(0, requestURL.lastIndexOf("/")));
+            properties.put("client.host", getLogicsHost());
+            properties.put("client.port", getLogicsPort());
+
+            String content = stringResolver.replacePlaceholders(
+                    IOUtils.readStreamToString(new FileInputStream(servletContext.getRealPath("client.jnlp"))), properties
+            );
+
+            response.setContentType("application/x-java-jnlp-file");
+            response.getOutputStream().write(content.getBytes());
+        } catch (Exception e) {
+            logger.debug("Error handling jnlp request: ", e);
+            response.sendError(HttpServletResponse.SC_NOT_FOUND, "Configuration can't be found.");
+        }
+    }
+
+    private String getLogicsHost() {
+        return nvl(servletContext.getInitParameter("serverHost"), "localhost");
+    }
+
+    private String getLogicsPort() {
+        return nvl(servletContext.getInitParameter("serverPort"), "7652");
+    }
+}
