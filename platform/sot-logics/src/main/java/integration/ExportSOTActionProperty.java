4e1fa0d2c47792e67e28443b58cf196af56282ce
Vandrouny <filipchyk.andrei@gmail.com>
2013-04-19 12:54:11 +0000

    [sot] Экспорт FTX

--- platform/sot-logics/src/main/java/integration/ExportSOTActionProperty.java
+++ platform/sot-logics/src/main/java/integration/ExportSOTActionProperty.java
@@ -1,7 +1,5 @@
 package integration;
 
-import fdk.integration.*;
-import org.apache.commons.lang.time.DateUtils;
 import org.xBaseJ.xBaseJException;
 import platform.base.col.MapFact;
 import platform.base.col.interfaces.immutable.ImMap;
@@ -10,10 +8,8 @@ import platform.base.col.interfaces.immutable.ImRevMap;
 import platform.interop.Compare;
 import platform.interop.action.MessageClientAction;
 import platform.server.classes.ConcreteClass;
-import platform.server.classes.ConcreteCustomClass;
 import platform.server.data.expr.KeyExpr;
 import platform.server.data.query.QueryBuilder;
-import platform.server.integration.*;
 import platform.server.logics.DataObject;
 import platform.server.logics.linear.LCP;
 import platform.server.logics.property.ClassPropertyInterface;
@@ -21,17 +17,12 @@ import platform.server.logics.property.ExecutionContext;
 import platform.server.logics.scripted.ScriptingActionProperty;
 import platform.server.logics.scripted.ScriptingErrorLog;
 import platform.server.logics.scripted.ScriptingLogicsModule;
-import platform.server.session.DataSession;
 
 import java.io.*;
 import java.sql.Date;
 import java.sql.SQLException;
-import java.sql.Timestamp;
-import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class ExportSOTActionProperty extends ScriptingActionProperty {
 
@@ -48,6 +39,12 @@ public class ExportSOTActionProperty extends ScriptingActionProperty {
 
                 if (getLCP("exportSOTReep").read(context) != null)
                     exportReep(context, path);
+
+                if (getLCP("exportSOTFtxp").read(context) != null)
+                    exportFtx(context, path, false);
+
+                if (getLCP("exportSOTFtxd").read(context) != null)
+                    exportFtx(context, path, true);
 
             }
         } catch (ScriptingErrorLog.SemanticErrorException e) {
@@ -115,7 +112,7 @@ public class ExportSOTActionProperty extends ScriptingActionProperty {
             Object supplierStockInvoice = userInvoiceValues.get("Sale.supplierStockInvoice");
             Object customerInvoice = userInvoiceValues.get("Sale.customerInvoice");
             Object customerStockInvoice = userInvoiceValues.get("Sale.customerStockInvoice");
-            String mag2CustomerStock = customerStockInvoice==null ? null : (String) LM.findLCPByCompoundName("mag2Stock").read(context, new DataObject(customerStockInvoice, (ConcreteClass) LM.findClassByCompoundName("warehouse")));
+            String mag2CustomerStock = customerStockInvoice == null ? null : (String) LM.findLCPByCompoundName("mag2Stock").read(context, new DataObject(customerStockInvoice, (ConcreteClass) LM.findClassByCompoundName("Warehouse")));
 
             Object exportSOTSupplier = LM.findLCPByCompoundName("exportSOTSupplier").read(context);
             Object exportSOTSupplierStock = LM.findLCPByCompoundName("exportSOTSupplierStock").read(context);
@@ -164,7 +161,7 @@ public class ExportSOTActionProperty extends ScriptingActionProperty {
                         String sotSIDOverrideUserInvoiceDetail = (String) uidValues.get("sotSIDOverrideUserInvoiceDetail");
                         String userInvoiceNumber = numberInvoice == null ? "" : String.valueOf(Integer.parseInt(numberInvoice.trim()));
                         reepList.add(new Reep(mag2CustomerStock == null ? "" : mag2CustomerStock.trim(),
-                                userInvoiceNumber, sotSIDOverrideUserInvoiceDetail== null ? "": sotSIDOverrideUserInvoiceDetail.trim(),
+                                userInvoiceNumber, sotSIDOverrideUserInvoiceDetail == null ? "" : sotSIDOverrideUserInvoiceDetail.trim(),
                                 nameSku == null ? "" : nameSku.trim(), uomID == null ? "" : uomID.trim().replaceFirst("UOMS", ""),
                                 parentGroupID == null ? "" : parentGroupID.trim().split("IG|:")[1],
                                 itemGroupID == null ? "" : itemGroupID.trim().split("IG|:")[2],
@@ -175,5 +172,148 @@ public class ExportSOTActionProperty extends ScriptingActionProperty {
             }
         }
         return reepList;
+    }
+
+    private void exportFtx(ExecutionContext context, String path, Boolean isReturn) throws IOException, xBaseJException, ScriptingErrorLog.SemanticErrorException, SQLException {
+
+        List<Ftx> ftxList = exportFtxToList(context, isReturn);
+        String prefix = isReturn ? "FTXD" : "FTXP";
+        if (ftxList.size() == 0) {
+            context.requestUserInteraction(new MessageClientAction("По заданным параметрам не найдено ни одной накладной", "Ошибка"));
+            return;
+        } else {
+
+            if (!new File(path).exists())
+                new File(path).mkdir();
+
+            FileOutputStream stream = new FileOutputStream(path + "//" + prefix);
+            OutputStreamWriter writer = new OutputStreamWriter(stream, "cp866");
+            String previousUNPLegalEntity1 = "";
+            String previousUNPLegalEntity2 = "";
+            Integer previousEntryNumber = 0;
+            for (Ftx ftx : ftxList) {
+                if (!previousUNPLegalEntity1.equals(ftx.unpLegalEntity1)) {
+                    writer.write(String.format("^" + prefix + "\r\n%s\r\n", ftx.unpLegalEntity1));
+                    previousUNPLegalEntity1 = ftx.unpLegalEntity1 == null ? "" : ftx.unpLegalEntity1;
+                }
+
+                if (!previousUNPLegalEntity2.equals(ftx.unpLegalEntity2)) {
+                    writer.write(String.format("^" + prefix + "(%d,412,%s,%s)\r\n%s:%s:%s:%s\r\n",
+                            ftx.monthNumber, ftx.storeID, ftx.numberDocument,
+                            new SimpleDateFormat("ddMM").format(ftx.dateDocument), ftx.sumDocument.intValue(), ftx.sumWareDocument.intValue(), ftx.unpLegalEntity2));
+                    previousUNPLegalEntity2 = ftx.unpLegalEntity2 == null ? "" : ftx.unpLegalEntity2;
+                }
+
+                if (!previousEntryNumber.equals(ftx.entryNumber)) {
+                    writer.write(String.format("^" + prefix + "(%d,412,%s,%s,%s)\r\n%s:%s:%s:%s:%s\r\n",
+                            ftx.monthNumber, ftx.storeID, ftx.numberDocument, ftx.entryNumber,
+                            ftx.operationCode, ftx.correspondingAccountCode, ftx.analyticalAccountCode, ftx.sumEntry.intValue(), ftx.sumWareEntry.intValue()));
+                    previousEntryNumber = ftx.entryNumber == null ? 0 : ftx.entryNumber;
+                }
+
+                if (ftx.financialSum != null)
+                    writer.write(String.format("^" + prefix + "(%d,412,%s,%s,%s,%s)\r\n183:%s:601:%s:%s\r\n",
+                            ftx.monthNumber, ftx.storeID, ftx.numberDocument, ftx.entryNumber, ftx.mainEntryAccountingInformationNumber,
+                            ftx.debitAnalytics, ftx.creditAnalytics, ftx.financialSum.intValue()));
+            }
+            writer.close();
+            context.requestUserInteraction(new MessageClientAction("Успешно экспортировано проводок: " + ftxList.size(), "Экспорт завершён"));
+
+        }
+    }
+
+    private List<Ftx> exportFtxToList(ExecutionContext context, Boolean isReturn) throws ScriptingErrorLog.SemanticErrorException, SQLException {
+
+        List<Ftx> ftxList = new ArrayList<Ftx>();
+
+        String namespace = isReturn ? "SaleReturn." : "Sale.";
+        String[] saleInvoiceProperties = new String[]{namespace + "supplierInvoice", namespace + "customerInvoice",
+                namespace + "dateUserInvoice", "numberObject", namespace + "customerStockInvoice"};
+        LCP<?> isSaleInvoice = LM.is(LM.findClassByCompoundName(namespace + "Invoice"));
+        ImRevMap<Object, KeyExpr> keys = (ImRevMap<Object, KeyExpr>) isSaleInvoice.getMapKeys();
+        KeyExpr key = keys.singleValue();
+        QueryBuilder<Object, Object> saleInvoiceQuery = new QueryBuilder<Object, Object>(keys);
+        for (String prop : saleInvoiceProperties)
+            saleInvoiceQuery.addProperty(prop, getLCP(prop).getExpr(context.getModifier(), key));
+        saleInvoiceQuery.and(isSaleInvoice.getExpr(key).getWhere());
+
+        ImOrderMap<ImMap<Object, Object>, ImMap<Object, Object>> saleInvoiceResult = saleInvoiceQuery.execute(context.getSession().sql);
+
+        int i = 0;
+        for (ImMap<Object, Object> saleInvoiceValues : saleInvoiceResult.valueIt()) {
+            DataObject userInvoiceObject = new DataObject(saleInvoiceResult.getKey(i).valueIt().iterator().next(), (ConcreteClass) LM.findClassByCompoundName("Sale.UserInvoice"));
+            i++;
+            Object operationObject = getLCP(namespace + "operationUserInvoice").read(context, userInvoiceObject);
+            String codeOperation = operationObject == null ? "" : (String) getLCP("codeOperation").read(context, new DataObject(operationObject, (ConcreteClass) LM.findClassByCompoundName("Sale.Operation")));
+            codeOperation = codeOperation==null ? "" : codeOperation.trim();
+            DataObject supplierInvoiceObject = new DataObject(saleInvoiceValues.get(namespace + "supplierInvoice"), (ConcreteClass) LM.findClassByCompoundName("LegalEntity"));
+            String unpLegalEntity1 = (String) getLCP("UNPLegalEntity").read(context, supplierInvoiceObject);
+            Object customerStockInvoice = saleInvoiceValues.get(namespace + "customerStockInvoice");
+            DataObject customerInvoiceObject = new DataObject(saleInvoiceValues.get(namespace + "customerInvoice"), (ConcreteClass) LM.findClassByCompoundName("LegalEntity"));
+            String unpLegalEntity2 = (String) getLCP("UNPLegalEntity").read(context, customerInvoiceObject);
+            String mag2Stock = customerStockInvoice == null ? null : (String) LM.findLCPByCompoundName("mag2Stock").read(context, new DataObject(customerStockInvoice, (ConcreteClass) LM.findClassByCompoundName("Warehouse")));
+            mag2Stock = mag2Stock == null ? "" : mag2Stock.trim();
+
+            Date dateInvoice = (Date) saleInvoiceValues.get(namespace + "dateUserInvoice");
+            Date dateFrom = (Date) LM.findLCPByCompoundName(isReturn ? "exportSOTFtxdDateFrom" : "exportSOTFtxpDateFrom").read(context);
+            Date dateTo = (Date) LM.findLCPByCompoundName(isReturn ? "exportSOTFtxdDateTo": "exportSOTFtxpDateTo").read(context);
+
+            if ((dateFrom == null || dateFrom.getTime() <= dateInvoice.getTime()) &&
+                    (dateTo == null || dateTo.getTime() >= dateInvoice.getTime())) {
+
+                Integer monthNumber = dateInvoice.getMonth() + 1;
+                String numberDocument = (String) saleInvoiceValues.get("numberObject");
+
+                KeyExpr userInvoiceDetailExpr = new KeyExpr("userInvoiceDetail");
+                ImRevMap<Object, KeyExpr> uidKeys = MapFact.singletonRev((Object) "userInvoiceDetail", userInvoiceDetailExpr);
+
+                String[] userInvoiceDetailProperties = new String[]{namespace + "valueVATUserInvoiceDetail", namespace + "sumUserInvoiceDetail",
+                        "purchaseRetailVATSumUserInvoiceDetail", namespace + "VATSumInvoiceDetail", "purchaseRetailMarkupSumUserInvoiceDetail"};
+                QueryBuilder<Object, Object> uidQuery = new QueryBuilder<Object, Object>(uidKeys);
+                for (String uidProperty : userInvoiceDetailProperties) {
+                    uidQuery.addProperty(uidProperty, getLCP(uidProperty).getExpr(context.getModifier(), userInvoiceDetailExpr));
+                }
+                uidQuery.and(getLCP(namespace + "userInvoiceUserInvoiceDetail").getExpr(context.getModifier(), uidQuery.getMapExprs().get("userInvoiceDetail")).compare(userInvoiceObject.getExpr(), Compare.EQUALS));
+                ImOrderMap<ImMap<Object, Object>, ImMap<Object, Object>> uidResult = uidQuery.execute(context.getSession().sql);
+
+                Double sum = 0.0;
+                Double vatSum = 0.0;
+                Double markupSum = 0.0;
+                Map<Double, Double> vatMap = new HashMap<Double, Double>();
+                for (ImMap<Object, Object> uidValues : uidResult.valueIt()) {
+                    Double vat = (Double) uidValues.get("purchaseRetailVATSumUserInvoiceDetail");
+                    Double valueVAT = (Double) uidValues.get(namespace + "valueVATUserInvoiceDetail");
+                    Double currentSum = vatMap.containsKey(valueVAT) ? vatMap.get(valueVAT) : 0;
+                    if (vat != null) {
+                        vatMap.put(valueVAT, currentSum + vat);
+                        vatSum += vat;
+                    }
+                    sum += (Double) uidValues.get(namespace + "sumUserInvoiceDetail");
+                    markupSum += (Double) uidValues.get("purchaseRetailMarkupSumUserInvoiceDetail");
+                }
+
+                unpLegalEntity1 = unpLegalEntity1 == null ? "" : unpLegalEntity1.trim();
+                unpLegalEntity2 = unpLegalEntity2 == null ? "" : unpLegalEntity2.trim();
+                numberDocument = numberDocument == null ? "" : numberDocument.trim();
+
+                Double sumDocument = sum + markupSum + vatSum;
+
+                int j = 1;
+                for (Map.Entry<Double, Double> entry : vatMap.entrySet()) {
+                    String vatCode = entry.getKey().equals(20.0) ? "55" : entry.getKey().equals(10.0) ? "11" : "00";
+                    ftxList.add(new Ftx(unpLegalEntity1, unpLegalEntity2, monthNumber, mag2Stock, numberDocument,
+                            dateInvoice, sumDocument, 0.0, 1, codeOperation, "601", "412", sum, 0.0, j, vatCode, "290387275", entry.getValue()));
+                    j++;
+                }
+
+                ftxList.add(new Ftx(unpLegalEntity1, unpLegalEntity2, monthNumber, mag2Stock, numberDocument,
+                        dateInvoice, sumDocument, 0.0, 2, codeOperation, "428", "412", vatSum, 0.0, null, null, null, null));
+
+                ftxList.add(new Ftx(unpLegalEntity1, unpLegalEntity2, monthNumber, mag2Stock, numberDocument,
+                        dateInvoice, sumDocument, 0.0, 3, codeOperation, "423", "412", markupSum, 0.0, null, null, null, null));
+
+            }
+        }
+        return ftxList;
     }
 }
