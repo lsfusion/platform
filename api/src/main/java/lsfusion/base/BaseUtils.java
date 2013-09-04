9f6e555882ba0a8234f7743fb9768429f587af83
Vandrouny <filipchyk.andrei@gmail.com>
2013-09-04 08:49:59 +0000

    [api] barcodeUtils

--- api/src/main/java/lsfusion/base/BaseUtils.java
+++ api/src/main/java/lsfusion/base/BaseUtils.java
@@ -2247,19 +2247,4 @@ public class BaseUtils {
             after++;
         }
     }
-
-    public static String convertBarcode12To13(String barcode) {
-        if (barcode!=null && barcode.length() == 12) {
-            int checkSum = 0;
-            for (int i = 0; i <= 10; i = i + 2) {
-                checkSum += Integer.valueOf(String.valueOf(barcode.charAt(i)));
-                checkSum += Integer.valueOf(String.valueOf(barcode.charAt(i + 1))) * 3;
-            }
-            checkSum %= 10;
-            if (checkSum != 0)
-                checkSum = 10 - checkSum;
-            return barcode.concat(String.valueOf(checkSum));
-        } else
-            return barcode;
-    }
 }
