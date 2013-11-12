7395396b0653270a1bdbc4fbea2f34023e0c648b
Vandrouny <filipchyk.andrei@gmail.com>
2013-11-12 10:01:45 +0000

    [server] в резервное копирование добавлена опция отключения таблиц

--- src/main/java/lsfusion/erp/utils/backup/BackupActionProperty.java
+++ src/main/java/lsfusion/erp/utils/backup/BackupActionProperty.java
@@ -1,10 +1,16 @@
 package lsfusion.erp.utils.backup;
 
 import com.google.common.base.Throwables;
+import lsfusion.base.col.MapFact;
 import lsfusion.base.col.interfaces.immutable.ImMap;
+import lsfusion.base.col.interfaces.immutable.ImOrderMap;
+import lsfusion.base.col.interfaces.immutable.ImRevMap;
 import lsfusion.server.classes.ConcreteCustomClass;
 import lsfusion.server.classes.ValueClass;
+import lsfusion.server.data.expr.KeyExpr;
+import lsfusion.server.data.query.QueryBuilder;
 import lsfusion.server.logics.DataObject;
+import lsfusion.server.logics.ObjectValue;
 import lsfusion.server.logics.property.CalcProperty;
 import lsfusion.server.logics.property.ClassPropertyInterface;
 import lsfusion.server.logics.property.ExecutionContext;
@@ -14,8 +20,10 @@ import lsfusion.server.logics.scripted.ScriptingLogicsModule;
 import lsfusion.server.session.DataSession;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.List;
 
 import static lsfusion.base.IOUtils.readFileToString;
 
@@ -33,7 +41,25 @@ public class BackupActionProperty extends ScriptingActionProperty {
             long currentTime = currentDate.getTime();
             String backupFileName = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss").format(currentDate);
 
-            String backupFilePath = context.getDbManager().backupDB(context, backupFileName);
+            KeyExpr tableExpr = new KeyExpr("Table");
+            ImRevMap<Object, KeyExpr> tableKeys = MapFact.<Object, KeyExpr>singletonRev("Table", tableExpr);
+
+            QueryBuilder<Object, Object> tableQuery = new QueryBuilder<Object, Object>(tableKeys);
+            tableQuery.addProperty("sidTable", LM.findLCPByCompoundName("sidTable").getExpr(context.getModifier(), tableExpr));
+
+            tableQuery.and(LM.findLCPByCompoundName("excludeTable").getExpr(context.getModifier(), tableExpr).getWhere());
+
+            ImOrderMap<ImMap<Object, Object>, ImMap<Object, Object>> tableResult = tableQuery.execute(context.getSession());
+
+            List<String> excludeTables = new ArrayList<String>();
+            for (ImMap<Object, Object> entry : tableResult.values()) {
+
+                String sidTable = (String) entry.get("sidTable");
+                if (sidTable != null)
+                    excludeTables.add(sidTable.trim());
+            }
+
+            String backupFilePath = context.getDbManager().backupDB(context, backupFileName, excludeTables);
             if (backupFilePath != null) {
                 String backupFileLogPath = backupFilePath + ".log";
                 String backupFileExtension = backupFilePath.substring(backupFilePath.lastIndexOf("."), backupFilePath.length());
@@ -46,6 +72,12 @@ public class BackupActionProperty extends ScriptingActionProperty {
                 LM.findLCPByCompoundName("fileLogBackup").change(backupFileLogPath, session, backupObject);
 
                 LM.findLCPByCompoundName("logBackup").change(readFileToString(backupFileLogPath), session, backupObject);
+
+                for (String excludeTable : excludeTables) {
+                    ObjectValue tableObject = LM.findLCPByCompoundName("tableSID").readClasses(session, new DataObject(excludeTable));
+                    if (tableObject instanceof DataObject)
+                        LM.findLCPByCompoundName("excludeBackupTable").change(true, session, backupObject, (DataObject) tableObject);
+                }
 
                 session.apply(context.getBL());
 
