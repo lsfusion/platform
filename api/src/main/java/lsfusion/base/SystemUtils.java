447495efde92729be952798348fdc23929bec293
Fantast <fantasteg@gmail.com>
2013-12-17 09:37:05 +0000

    OS detection (c/p from org.apache.commons.lang.SystemUtils)

--- api/src/main/java/lsfusion/base/SystemUtils.java
+++ api/src/main/java/lsfusion/base/SystemUtils.java
@@ -13,6 +13,342 @@ import java.util.jar.Manifest;
 import java.util.prefs.Preferences;
 
 public class SystemUtils {
+    /**
+     * The prefix String for all Windows OS.
+     */
+    private static final String OS_NAME_WINDOWS_PREFIX = "Windows";
+
+    /**
+     * <p>The <code>line.separator</code> System Property. Line separator
+     * (<code>&quot;\n&quot;</code> on UNIX).</p>
+     *
+     * <p>Defaults to <code>null</code> if the runtime does not have
+     * security access to read this property or the property does not exist.</p>
+     *
+     * <p>
+     * This value is initialized when the class is loaded. If {@link System#setProperty(String,String)}
+     * or {@link System#setProperties(java.util.Properties)} is called after this class is loaded, the value
+     * will be out of sync with that System property.
+     * </p>
+     *
+     * @since Java 1.1
+     */
+    public static final String LINE_SEPARATOR = getSystemProperty("line.separator");
+
+    /**
+     * <p>The <code>os.arch</code> System Property. Operating system architecture.</p>
+     *
+     * <p>Defaults to <code>null</code> if the runtime does not have
+     * security access to read this property or the property does not exist.</p>
+     *
+     * <p>
+     * This value is initialized when the class is loaded. If {@link System#setProperty(String,String)}
+     * or {@link System#setProperties(java.util.Properties)} is called after this class is loaded, the value
+     * will be out of sync with that System property.
+     * </p>
+     *
+     * @since Java 1.1
+     */
+    public static final String OS_ARCH = getSystemProperty("os.arch");
+
+    /**
+     * <p>The <code>os.name</code> System Property. Operating system name.</p>
+     *
+     * <p>Defaults to <code>null</code> if the runtime does not have
+     * security access to read this property or the property does not exist.</p>
+     *
+     * <p>
+     * This value is initialized when the class is loaded. If {@link System#setProperty(String,String)}
+     * or {@link System#setProperties(java.util.Properties)} is called after this class is loaded, the value
+     * will be out of sync with that System property.
+     * </p>
+     *
+     * @since Java 1.1
+     */
+    public static final String OS_NAME = getSystemProperty("os.name");
+
+    /**
+     * <p>The <code>os.version</code> System Property. Operating system version.</p>
+     *
+     * <p>Defaults to <code>null</code> if the runtime does not have
+     * security access to read this property or the property does not exist.</p>
+     *
+     * <p>
+     * This value is initialized when the class is loaded. If {@link System#setProperty(String,String)}
+     * or {@link System#setProperties(java.util.Properties)} is called after this class is loaded, the value
+     * will be out of sync with that System property.
+     * </p>
+     *
+     * @since Java 1.1
+     */
+    public static final String OS_VERSION = getSystemProperty("os.version");
+
+    /**
+     * <p>The <code>path.separator</code> System Property. Path separator
+     * (<code>&quot;:&quot;</code> on UNIX).</p>
+     *
+     * <p>Defaults to <code>null</code> if the runtime does not have
+     * security access to read this property or the property does not exist.</p>
+     *
+     * <p>
+     * This value is initialized when the class is loaded. If {@link System#setProperty(String,String)}
+     * or {@link System#setProperties(java.util.Properties)} is called after this class is loaded, the value
+     * will be out of sync with that System property.
+     * </p>
+     *
+     * @since Java 1.1
+     */
+    public static final String PATH_SEPARATOR = getSystemProperty("path.separator");
+
+    /**
+     * <p>Is <code>true</code> if this is AIX.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.0
+     */
+    public static final boolean IS_OS_AIX = getOSMatches("AIX");
+
+    /**
+     * <p>Is <code>true</code> if this is HP-UX.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.0
+     */
+    public static final boolean IS_OS_HP_UX = getOSMatches("HP-UX");
+
+    /**
+     * <p>Is <code>true</code> if this is Irix.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.0
+     */
+    public static final boolean IS_OS_IRIX = getOSMatches("Irix");
+
+    /**
+     * <p>Is <code>true</code> if this is Linux.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.0
+     */
+    public static final boolean IS_OS_LINUX = getOSMatches("Linux") || getOSMatches("LINUX");
+
+    /**
+     * <p>Is <code>true</code> if this is Mac.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.0
+     */
+    public static final boolean IS_OS_MAC = getOSMatches("Mac");
+
+    /**
+     * <p>Is <code>true</code> if this is Mac.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.0
+     */
+    public static final boolean IS_OS_MAC_OSX = getOSMatches("Mac OS X");
+
+    /**
+     * <p>Is <code>true</code> if this is OS/2.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.0
+     */
+    public static final boolean IS_OS_OS2 = getOSMatches("OS/2");
+
+    /**
+     * <p>Is <code>true</code> if this is Solaris.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.0
+     */
+    public static final boolean IS_OS_SOLARIS = getOSMatches("Solaris");
+
+    /**
+     * <p>Is <code>true</code> if this is SunOS.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.0
+     */
+    public static final boolean IS_OS_SUN_OS = getOSMatches("SunOS");
+
+    /**
+     * <p>Is <code>true</code> if this is a POSIX compilant system,
+     * as in any of AIX, HP-UX, Irix, Linux, MacOSX, Solaris or SUN OS.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.1
+     */
+    public static final boolean IS_OS_UNIX =
+            IS_OS_AIX || IS_OS_HP_UX || IS_OS_IRIX || IS_OS_LINUX ||
+            IS_OS_MAC_OSX || IS_OS_SOLARIS || IS_OS_SUN_OS;
+
+    /**
+     * <p>Is <code>true</code> if this is Windows.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.0
+     */
+    public static final boolean IS_OS_WINDOWS = getOSMatches(OS_NAME_WINDOWS_PREFIX);
+
+    /**
+     * <p>Is <code>true</code> if this is Windows 2000.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.0
+     */
+    public static final boolean IS_OS_WINDOWS_2000 = getOSMatches(OS_NAME_WINDOWS_PREFIX, "5.0");
+
+    /**
+     * <p>Is <code>true</code> if this is Windows 95.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.0
+     */
+    public static final boolean IS_OS_WINDOWS_95 = getOSMatches(OS_NAME_WINDOWS_PREFIX + " 9", "4.0");
+    // JDK 1.2 running on Windows98 returns 'Windows 95', hence the above
+
+    /**
+     * <p>Is <code>true</code> if this is Windows 98.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.0
+     */
+    public static final boolean IS_OS_WINDOWS_98 = getOSMatches(OS_NAME_WINDOWS_PREFIX + " 9", "4.1");
+    // JDK 1.2 running on Windows98 returns 'Windows 95', hence the above
+
+    /**
+     * <p>Is <code>true</code> if this is Windows ME.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.0
+     */
+    public static final boolean IS_OS_WINDOWS_ME = getOSMatches(OS_NAME_WINDOWS_PREFIX, "4.9");
+    // JDK 1.2 running on WindowsME may return 'Windows 95', hence the above
+
+    /**
+     * <p>Is <code>true</code> if this is Windows NT.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.0
+     */
+    public static final boolean IS_OS_WINDOWS_NT = getOSMatches(OS_NAME_WINDOWS_PREFIX + " NT");
+    // Windows 2000 returns 'Windows 2000' but may suffer from same JDK1.2 problem
+
+    /**
+     * <p>Is <code>true</code> if this is Windows XP.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.0
+     */
+    public static final boolean IS_OS_WINDOWS_XP = getOSMatches(OS_NAME_WINDOWS_PREFIX, "5.1");
+
+    //-----------------------------------------------------------------------
+    /**
+     * <p>Is <code>true</code> if this is Windows Vista.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.4
+     */
+    public static final boolean IS_OS_WINDOWS_VISTA = getOSMatches(OS_NAME_WINDOWS_PREFIX, "6.0");
+
+    /**
+     * <p>Is <code>true</code> if this is Windows 7.</p>
+     *
+     * <p>The field will return <code>false</code> if <code>OS_NAME</code> is
+     * <code>null</code>.</p>
+     *
+     * @since 2.5
+     */
+    public static final boolean IS_OS_WINDOWS_7 = getOSMatches(OS_NAME_WINDOWS_PREFIX, "6.1");
+
+    /**
+     * <p>Decides if the operating system matches.</p>
+     *
+     * @param osNamePrefix  the prefix for the os name
+     * @return true if matches, or false if not or can't determine
+     */
+    private static boolean getOSMatches(String osNamePrefix) {
+        if (OS_NAME == null) {
+            return false;
+        }
+        return OS_NAME.startsWith(osNamePrefix);
+    }
+
+    /**
+     * <p>Decides if the operating system matches.</p>
+     *
+     * @param osNamePrefix  the prefix for the os name
+     * @param osVersionPrefix  the prefix for the version
+     * @return true if matches, or false if not or can't determine
+     */
+    private static boolean getOSMatches(String osNamePrefix, String osVersionPrefix) {
+        if (OS_NAME == null || OS_VERSION == null) {
+            return false;
+        }
+        return OS_NAME.startsWith(osNamePrefix) && OS_VERSION.startsWith(osVersionPrefix);
+    }
+
+    //-----------------------------------------------------------------------
+    /**
+     * <p>Gets a System property, defaulting to <code>null</code> if the property
+     * cannot be read.</p>
+     *
+     * <p>If a <code>SecurityException</code> is caught, the return
+     * value is <code>null</code> and a message is written to <code>System.err</code>.</p>
+     *
+     * @param property the system property name
+     * @return the system property value or <code>null</code> if a security problem occurs
+     */
+    private static String getSystemProperty(String property) {
+        try {
+            return System.getProperty(property);
+        } catch (SecurityException ex) {
+            // we are not allowed to look at this property
+            System.err.println(
+                    "Caught a SecurityException reading the system property '" + property
+                    + "'; the SystemUtils property value will default to null."
+            );
+            return null;
+        }
+    }
+
     public static String getResourcePath(String resource, String path, Class<?> cls, boolean overwrite, boolean appendPath) throws IOException {
 
         File file = getUserFile(appendPath ? resolveName(cls, path + resource) : resource);
@@ -36,14 +372,14 @@ public class SystemUtils {
 
     public static String getLibraryPath(String libName, String path, Class<?> cls) throws IOException {
 
-        String system = System.getProperty("os.name");
-        String libExtension =
-                "Linux".equals(system) ? ".so" : ".dll";
-        String libPath =
-                "Linux".equals(system) ? "ux" : "win";
-        libPath += is64Arch() ? "64" : "32";
+        String libExtension = IS_OS_WINDOWS ? "*.dll" : ".so";
+        String libPath = (IS_OS_WINDOWS ? "win" : "ux") + (is64Arch() ? "64" : "32");
 
         return getResourcePath(libName + libExtension, path + libPath + '/', cls, false, false); // будем считать, что в library зашифрована вер
+    }
+
+    public static void loadLibrary(String libName, String path, Class<?> cls) throws IOException {
+        System.load(getLibraryPath(libName, path, cls));
     }
 
     public static void saveCurrentDirectory(File lastFolder) {
@@ -69,10 +405,6 @@ public class SystemUtils {
 
     public static void loadClass(String className, String path, Class<?> cls) throws IOException {
         getResourcePath(className + ".class", path, cls, true, true); // для класса обновляем, поскольку иначе изменения не будут обновляться
-    }
-
-    public static void loadLibrary(String libName, String path, Class<?> cls) throws IOException {
-        System.load(getLibraryPath(libName, path, cls));
     }
 
     public static File getUserDir() {
