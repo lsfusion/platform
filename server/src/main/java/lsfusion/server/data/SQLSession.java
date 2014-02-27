03c3f20ff803034e7b90273429da763b04c4f523
AlexKirkouski <alex.kirkouski@gmail.com>
2014-02-27 13:26:59 +0000

    REVERT REVERT'а

--- server/src/main/java/lsfusion/server/data/SQLSession.java
+++ server/src/main/java/lsfusion/server/data/SQLSession.java
@@ -494,7 +494,7 @@ public class SQLSession extends MutableObject {
     }
 
     public void vacuumAnalyzeSessionTable(String table) throws SQLException {
-        executeDDL((isInTransaction()? "" :"") + "ANALYZE " + table, ExecuteEnvironment.NOREADONLY);
+        executeDDL((isInTransaction()? "" :"VACUUM ") + "ANALYZE " + table, ExecuteEnvironment.NOREADONLY);
     }
 
     private int noReadOnly = 0;
