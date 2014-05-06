d73467c0a76028ded282257e30365c2788da3499
Vandrouny <filipchyk.andrei@gmail.com>
2014-05-06 13:54:22 +0000

    [erp] перевод по словарю: ищем термины из нескольких слов

--- src/main/java/lsfusion/erp/utils/i18n/TranslateDictionaryActionProperty.java
+++ src/main/java/lsfusion/erp/utils/i18n/TranslateDictionaryActionProperty.java
@@ -3,6 +3,7 @@ package lsfusion.erp.utils.i18n;
 import lsfusion.server.classes.StringClass;
 import lsfusion.server.data.SQLHandledException;
 import lsfusion.server.logics.DataObject;
+import lsfusion.server.logics.linear.LCP;
 import lsfusion.server.logics.property.ClassPropertyInterface;
 import lsfusion.server.logics.property.ExecutionContext;
 import lsfusion.server.logics.scripted.ScriptingActionProperty;
@@ -35,33 +36,66 @@ public class TranslateDictionaryActionProperty extends ScriptingActionProperty {
             if (dictionaryObject != null && termObject != null) {
 
                 Boolean insensitive = getLCP("insensitiveDictionary").read(context.getSession(), dictionaryObject) != null;
+                LCP insensitiveLP = getLCP("insensitiveTranslationDictionaryEntryDictionaryTerm");
+                LCP sensitiveLP = getLCP("translationDictionaryEntryDictionaryTerm");
 
                 String source = (String) termObject.object;
                 if (insensitive)
                     source = source.toUpperCase();
                 if (source != null) {
                     String delim = ", .:;%#$@/\\|<>=+-_)(*&?^!~{}[]\"1234567890'";
-                    StringTokenizer st = new StringTokenizer(source, delim, true);
                     String result = "";
-                    String fullLineTranslation = (String) (getLCP(insensitive ? "insensitiveTranslationDictionaryEntryDictionaryTerm" : "translationDictionaryEntryDictionaryTerm").read(context, dictionaryObject, new DataObject(source, StringClass.text)));
+                    String fullLineTranslation = (String) (insensitive ? insensitiveLP : sensitiveLP).read(context, dictionaryObject, new DataObject(source, StringClass.text));
                     if (fullLineTranslation != null) {
                         result = fullLineTranslation.trim();
                     } else {
+                        StringTokenizer st = new StringTokenizer(source, delim, true);
+                        String[] phrase = new String[st.countTokens()];
+                        int count = 0;
                         while (st.hasMoreTokens()) {
                             String token = st.nextToken();
-                            if (!delim.contains(token.subSequence(0, token.length()))) {
-                                String translation = (String) (getLCP(insensitive ? "insensitiveTranslationDictionaryEntryDictionaryTerm" : "translationDictionaryEntryDictionaryTerm").read(context, dictionaryObject, new DataObject(token, StringClass.text)));
+                            phrase[count] = token;
+                            count++;
+                        }
+
+                        int start = 0;
+                        int finish = phrase.length;
+                        while (start < finish) {
+                            int tStart = start;
+                            int tFinish = finish;
+
+                            while (tStart < tFinish) {
+
+                                String token = getToken(phrase, tStart, tFinish);
+
+                                String translation = (String) (insensitive ? insensitiveLP : sensitiveLP).read(context, dictionaryObject, new DataObject(token, StringClass.text));
                                 if (translation != null) {
-                                    token = translation.trim();
+                                    result += translation.trim();
+                                    start = tFinish;
+                                    tStart = tFinish;
+                                } else {
+                                    tFinish--;
+                                    if(tStart == tFinish) { //не нашли перевод
+                                        result += token;
+                                        start++;
+                                    }
                                 }
                             }
-                            result += token;
                         }
                     }
                     getLCP("translationResult").change(result, context);
                 }
             }
-        } catch (ScriptingErrorLog.SemanticErrorException e) {
+        } catch (ScriptingErrorLog.SemanticErrorException ignored) {
         }
+    }
+
+    private String getToken(String[] source, int start, int finish) {
+        String token = "";
+        while (start < finish) {
+            token += source[start];
+            start++;
+        }
+        return token;
     }
 }
