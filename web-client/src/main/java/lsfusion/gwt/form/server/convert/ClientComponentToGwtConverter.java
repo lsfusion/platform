05f579791b60fd341561e742870f19c2421c3efc
pavel-miniutka <asphalt.by@gmail.com>
2014-09-11 09:39:47 +0000

    [Web]: use default font size if not specified

--- web-client/src/main/java/lsfusion/gwt/form/server/convert/ClientComponentToGwtConverter.java
+++ web-client/src/main/java/lsfusion/gwt/form/server/convert/ClientComponentToGwtConverter.java
@@ -108,6 +108,9 @@ public class ClientComponentToGwtConverter extends CachedObjectConverter {
         if (font == null) {
             return null;
         }
+        if (font.size <= 0) {
+            font.size = GFont.DEFAULT_FONT_SIZE;
+        }
         form.addFont(font);
         return font;
     }
