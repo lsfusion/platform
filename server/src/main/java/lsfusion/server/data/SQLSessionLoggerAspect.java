6022fe568ed26a0e082e932138828a4b1f0ccaf6
AlexKirkouski <alex.kirkouski@gmail.com>
2014-10-07 14:04:31 +0000

    Расширенное логгирование

--- server/src/main/java/lsfusion/server/data/SQLSessionLoggerAspect.java
+++ server/src/main/java/lsfusion/server/data/SQLSessionLoggerAspect.java
@@ -13,28 +13,28 @@ import static lsfusion.server.ServerLoggers.sqlLogger;
 @Aspect
 public class SQLSessionLoggerAspect {
 
-    @Around("execution(* lsfusion.server.data.SQLSession.executeDDL(java.lang.String)) && args(queryString)")
-    public Object executeSQL(ProceedingJoinPoint thisJoinPoint, String queryString) throws Throwable {
-        return executeMethodAndLogTime(thisJoinPoint, queryString);
+    @Around("execution(* lsfusion.server.data.SQLSession.executeDDL(java.lang.String)) && target(session) && args(queryString)")
+    public Object executeSQL(ProceedingJoinPoint thisJoinPoint, SQLSession session, String queryString) throws Throwable {
+        return executeMethodAndLogTime(thisJoinPoint, session, queryString);
     }
 
-    @Around("execution(* lsfusion.server.data.SQLSession.executeDML(java.lang.String, ..)) && args(queryString, ..)")
-    public Object executeDML(ProceedingJoinPoint thisJoinPoint, String queryString) throws Throwable {
-        return executeMethodAndLogTime(thisJoinPoint, queryString);
+    @Around("execution(* lsfusion.server.data.SQLSession.executeDML(java.lang.String, ..)) && target(session) && args(queryString, ..)")
+    public Object executeDML(ProceedingJoinPoint thisJoinPoint, SQLSession session, String queryString) throws Throwable {
+        return executeMethodAndLogTime(thisJoinPoint, session, queryString);
     }
 
-    @Around("execution(* lsfusion.server.data.SQLSession.executeSelect(java.lang.String, ..)) && args(select, ..)")
-    public Object executeSelect(ProceedingJoinPoint thisJoinPoint, String select) throws Throwable {
-        return executeMethodAndLogTime(thisJoinPoint, select);
+    @Around("execution(* lsfusion.server.data.SQLSession.executeSelect(java.lang.String, ..)) && target(session) && args(select, ..)")
+    public Object executeSelect(ProceedingJoinPoint thisJoinPoint, SQLSession session, String select) throws Throwable {
+        return executeMethodAndLogTime(thisJoinPoint, session, select);
     }
 
-    @Around("execution(* lsfusion.server.data.SQLSession.insertBatchRecords(java.lang.String, lsfusion.base.col.interfaces.immutable.ImOrderSet, lsfusion.base.col.interfaces.immutable.ImMap, ..)) && args(table, keys, rows, ..)")
-    public Object executeInsertBatch(ProceedingJoinPoint thisJoinPoint, String table, ImOrderSet keys, ImMap rows) throws Throwable {
-        return executeMethodAndLogTime(thisJoinPoint, "INSERT BATCH INTO " + table + " ROWS " + rows.size());
+    @Around("execution(* lsfusion.server.data.SQLSession.insertBatchRecords(java.lang.String, lsfusion.base.col.interfaces.immutable.ImOrderSet, lsfusion.base.col.interfaces.immutable.ImMap, ..)) && target(session) && args(table, keys, rows, ..)")
+    public Object executeInsertBatch(ProceedingJoinPoint thisJoinPoint, SQLSession session, String table, ImOrderSet keys, ImMap rows) throws Throwable {
+        return executeMethodAndLogTime(thisJoinPoint, session, "INSERT BATCH INTO " + table + " ROWS " + rows.size());
     }
-    @Around("execution(* lsfusion.server.data.SQLSession.readSingleValues(lsfusion.server.data.SessionTable, ..)) && args(table, ..)")
-    public Object executeReadSingleValues(ProceedingJoinPoint thisJoinPoint, SessionTable table) throws Throwable {
-        return executeMethodAndLogTime(thisJoinPoint, "READ SINGLE VALUES " + table);
+    @Around("execution(* lsfusion.server.data.SQLSession.readSingleValues(lsfusion.server.data.SessionTable, ..)) && target(session) && args(table, ..)")
+    public Object executeReadSingleValues(ProceedingJoinPoint thisJoinPoint, SQLSession session, SessionTable table) throws Throwable {
+        return executeMethodAndLogTime(thisJoinPoint, session, "READ SINGLE VALUES " + table);
     }
 
     private static long runningTotal = 0;
@@ -43,7 +43,7 @@ public class SQLSessionLoggerAspect {
     public static int breakPointTime = 60;
     private static int breakPointLength = 10000;
 
-    public Object executeMethodAndLogTime(ProceedingJoinPoint thisJoinPoint, String queryString) throws Throwable {
+    public Object executeMethodAndLogTime(ProceedingJoinPoint thisJoinPoint, SQLSession session, String queryString) throws Throwable {
         boolean loggingEnabled = sqlLogger.isDebugEnabled();
 
         long startTime = 0;
@@ -67,7 +67,7 @@ public class SQLSessionLoggerAspect {
                     queryString = "[rows " + ((ImOrderMap) result).size() + "] " + queryString;
                 if (result instanceof Integer) // cheat, но чисто для логинга
                     queryString = "[rows " + result + "] " + queryString;
-                sqlLogger.info(String.format("Executed query (time: %1$d ms., running total: %3$d, running warn: %4$d, running count: %5$d): %2$s", runTime / 1000000, queryString, runningTotal / 1000000, runningWarningTotal / 1000000, runningCount));
+                sqlLogger.info(String.format("Executed query (time: %1$d ms., running total: %3$d, running warn: %4$d, running count: %5$d): %2$s, volatile : %6$b", runTime / 1000000, queryString, runningTotal / 1000000, runningWarningTotal / 1000000, runningCount, session.isVolatileStats()));
             }
         }
 
