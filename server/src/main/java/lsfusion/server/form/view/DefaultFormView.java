6e09314987619eaa2cb7a3a7c5aedc9a8aaf91c9
pavel-miniutka <asphalt.by@gmail.com>
2015-01-20 11:35:23 +0000

    fixed creating of container for property group

--- server/src/main/java/lsfusion/server/form/view/DefaultFormView.java
+++ server/src/main/java/lsfusion/server/form/view/DefaultFormView.java
@@ -437,7 +437,7 @@ public class DefaultFormView extends FormView {
                 currentGroupContainer.setType(ContainerType.COLUMNS);
                 currentGroupContainer.columns = 4;
 
-                panelContainers.get(groupObject).add(currentGroupContainer, version);
+                panelContainers.get(groupObject).add(currentGroupContainer, Version.GLOBAL);
             }
             
             putGroupPropertyContainer(groupObject, currentGroup, currentGroupContainer);
