1e899428bfa33a0c888ebed6eeaac039b7e372c3
AlexKirkouski <alex.kirkouski@gmail.com>
2015-05-21 09:31:57 +0000

    Багфикс с setOrDropped

--- server/src/main/java/lsfusion/server/logics/property/ChangedProperty.java
+++ server/src/main/java/lsfusion/server/logics/property/ChangedProperty.java
@@ -9,6 +9,7 @@ import lsfusion.base.col.interfaces.immutable.ImRevMap;
 import lsfusion.base.col.interfaces.immutable.ImSet;
 import lsfusion.base.col.interfaces.mutable.MSet;
 import lsfusion.server.ServerLoggers;
+import lsfusion.server.Settings;
 import lsfusion.server.classes.LogicalClass;
 import lsfusion.server.data.SQLHandledException;
 import lsfusion.server.data.expr.Expr;
@@ -65,21 +66,35 @@ public class ChangedProperty<T extends PropertyInterface> extends SessionCalcPro
     @Override
     public ImSet<CalcProperty> calculateUsedChanges(StructChanges propChanges) {
         Boolean setOrDropped = getSetOrDropped();
-        if(setOrDropped != null) {
-            ChangeType type = propChanges.getUsedChange(property);
-            Boolean changeSetOrDropped;
-            if(type != null && (changeSetOrDropped = type.getSetOrDropped())!=null) {
-                if(!setOrDropped.equals(changeSetOrDropped)) { // если SET, а изменение на NULL, или DROPPED и наоборот, считаем что изменение не используется так как все равно всегда NULL будет
-                    boolean isFinal = type.isFinal();
-                    ImSet<CalcProperty> usedChanges = null;
-                    if(isFinal || (usedChanges = property.getUsedChanges(propChanges)).size() == 1) {
-                        ServerLoggers.assertLog(isFinal || usedChanges.contains(property), "SHOULD NOT BE");
-                        return SetFact.EMPTY();
-                    }
-                }
-            }
+        if(setOrDropped != null && !Settings.get().isDisableSetDroppedOptimization()) {
+            if (isFakeChange(propChanges, setOrDropped))
+                return SetFact.EMPTY();
         }
         return super.calculateUsedChanges(propChanges);
+    }
+
+    // должно быть синхронизировано с аналогичным методом в StructChanges
+    private boolean isFakeChange(StructChanges propChanges, Boolean setOrDropped) {
+        return isSingleFakeChange(propChanges, property, setOrDropped) && isSingleFakeChange(propChanges, property.getOld(scope), !setOrDropped);
+    }
+
+    private static boolean isSingleFakeChange(StructChanges propChanges, CalcProperty<?> property, boolean setOrDropped) {
+        ImSet<CalcProperty> usedChanges = property.getUsedChanges(propChanges);
+        if(usedChanges.isEmpty()) // нет изменений
+            return true;
+
+        ChangeType type = propChanges.getUsedChange(property);
+
+        if(usedChanges.size() > 1 || !BaseUtils.hashEquals(usedChanges.single(), property)) { // есть изменения, кроме считанного usedChange
+            ServerLoggers.assertLog(type == null || !type.isFinal(), "SHOULD NOT BE");
+            return false;
+        }
+
+        assert BaseUtils.hashEquals(usedChanges.single(), property);
+        ServerLoggers.assertLog(type != null, "SHOULD NOT BE");
+
+        Boolean changeSetOrDropped = type.getSetOrDropped();
+        return changeSetOrDropped != null && !changeSetOrDropped.equals(setOrDropped); // если SET, а изменение на NULL, или DROPPED и наоборот, считаем что изменение не используется так как все равно всегда NULL будет
     }
 
     protected Expr calculateExpr(ImMap<T, ? extends Expr> joinImplement, CalcType calcType, PropertyChanges propChanges, WhereBuilder changedWhere) {
