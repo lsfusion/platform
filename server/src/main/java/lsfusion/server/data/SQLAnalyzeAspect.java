32cfc8225441e1200d7648a9fcc2b43fb2ce2b49
AlexKirkouski <alex.kirkouski@gmail.com>
2015-06-27 16:16:54 +0000

    Изменения EXPLAIN ANALYZE SELECT

--- server/src/main/java/lsfusion/server/data/SQLAnalyzeAspect.java
+++ server/src/main/java/lsfusion/server/data/SQLAnalyzeAspect.java
@@ -3,6 +3,7 @@ package lsfusion.server.data;
 import lsfusion.base.Result;
 import lsfusion.base.col.interfaces.immutable.ImMap;
 import lsfusion.server.ParamMessage;
+import lsfusion.server.Settings;
 import lsfusion.server.data.query.DynamicExecEnvSnapshot;
 import lsfusion.server.data.query.DynamicExecuteEnvironment;
 import lsfusion.server.data.query.StaticExecuteEnvironment;
@@ -19,17 +20,21 @@ public class SQLAnalyzeAspect {
     @Around("execution(* lsfusion.server.data.SQLSession.executeCommand(lsfusion.server.data.SQLCommand, lsfusion.server.data.query.DynamicExecEnvSnapshot, lsfusion.server.data.OperationOwner, lsfusion.base.col.interfaces.immutable.ImMap, " +
             "java.lang.Object)) && target(sql) && args(command, queryExecEnv, owner, paramObjects, handler)")
     public Object executeCommand(final ProceedingJoinPoint thisJoinPoint, final SQLSession sql, final SQLCommand command, DynamicExecEnvSnapshot queryExecEnv, final OperationOwner owner, final ImMap<String, ParseInterface> paramObjects, final Object handler) throws Throwable {
-        if(!(command instanceof SQLAnalyze)) {
-            boolean explain = sql.explainAnalyze();
-            boolean noAnalyze = sql.explainNoAnalyze();
-            if (command instanceof SQLDML && explain && !noAnalyze)
-                return thisJoinPoint.proceed(new Object[]{sql, new SQLAnalyze(command, false), queryExecEnv, owner, paramObjects, handler});
+        if (command instanceof SQLAnalyze)
+            return thisJoinPoint.proceed();
 
-            if (explain) {
-                thisJoinPoint.proceed(new Object[]{sql, new SQLAnalyze(command, noAnalyze), queryExecEnv, owner, paramObjects, new Result<Integer>()});
-            }
+        boolean explain = sql.explainAnalyze();
+        boolean noAnalyze = sql.explainNoAnalyze();
+        if (command instanceof SQLDML && explain && !noAnalyze)
+            return thisJoinPoint.proceed(new Object[]{sql, new SQLAnalyze(command, false), queryExecEnv, owner, paramObjects, handler});
+
+        long started = System.currentTimeMillis();
+        Object result = thisJoinPoint.proceed();
+
+        if (explain && (started - System.currentTimeMillis()) > Settings.get().getExplainThreshold()) {
+            thisJoinPoint.proceed(new Object[]{sql, new SQLAnalyze(command, noAnalyze), queryExecEnv, owner, paramObjects, new Result<Integer>()});
         }
 
-        return thisJoinPoint.proceed();
+        return result;
     }
 }
