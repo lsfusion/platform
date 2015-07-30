b9b10cc09d4d6c8a1c2eb65718f812cec6faff34
Vandrouny <filipchyk.andrei@gmail.com>
2015-07-30 06:36:29 +0000

    [server] обслуживание БД многопоточно

--- server/src/main/java/lsfusion/server/logics/service/ServiceDBMultiThreadActionProperty.java
+++ server/src/main/java/lsfusion/server/logics/service/ServiceDBMultiThreadActionProperty.java
@@ -1,36 +1,45 @@
 package lsfusion.server.logics.service;
 
 import lsfusion.interop.action.MessageClientAction;
+import lsfusion.server.classes.ValueClass;
 import lsfusion.server.data.SQLHandledException;
 import lsfusion.server.data.SQLSession;
 import lsfusion.server.logics.DBManager;
+import lsfusion.server.logics.ObjectValue;
 import lsfusion.server.logics.ServiceLogicsModule;
 import lsfusion.server.logics.property.ClassPropertyInterface;
 import lsfusion.server.logics.property.ExecutionContext;
 import lsfusion.server.logics.scripted.ScriptingActionProperty;
 
 import java.sql.SQLException;
+import java.util.Iterator;
 
 import static lsfusion.server.logics.ServerResourceBundle.getString;
 
 public class ServiceDBMultiThreadActionProperty extends ScriptingActionProperty {
+    private ClassPropertyInterface threadCountInterface;
     ServiceLogicsModule serviceLM;
-    public ServiceDBMultiThreadActionProperty(ServiceLogicsModule LM) {
-        super(LM);
+    public ServiceDBMultiThreadActionProperty(ServiceLogicsModule LM, ValueClass... classes) {
+        super(LM, classes);
         this.serviceLM = LM;
+
+        Iterator<ClassPropertyInterface> i = interfaces.iterator();
+        threadCountInterface = i.next();
     }
 
     @Override
     public void executeCustom(final ExecutionContext<ClassPropertyInterface> context) throws SQLException, SQLHandledException {
 
+        final ObjectValue threadCountObject = context.getKeyValue(threadCountInterface);
+
         run(context, new RunService() {
             public void run(SQLSession session, boolean isolatedTransaction) throws SQLException, SQLHandledException {
-                serviceLM.recalculateMultiThreadAction.execute(context);
+                serviceLM.recalculateMultiThreadAction.execute(context, threadCountObject);
             }});
 
         run(context, new RunService() {
             public void run(SQLSession session, boolean isolatedTransaction) throws SQLException, SQLHandledException {
-                serviceLM.recalculateClassesMultiThreadAction.execute(context);
+                serviceLM.recalculateClassesMultiThreadAction.execute(context, threadCountObject);
             }
         });
 
@@ -38,13 +47,13 @@ public class ServiceDBMultiThreadActionProperty extends ScriptingActionProperty 
 
         run(context, new RunService() {
             public void run(SQLSession session, boolean isolatedTransaction) throws SQLException, SQLHandledException {
-                serviceLM.recalculateFollowsMultiThreadAction.execute(context);
+                serviceLM.recalculateFollowsMultiThreadAction.execute(context, threadCountObject);
             }
         });
 
         run(context, new RunService() {
             public void run(SQLSession session, boolean isolatedTransaction) throws SQLException, SQLHandledException {
-                serviceLM.recalculateStatsMultiThreadAction.execute(context);
+                serviceLM.recalculateStatsMultiThreadAction.execute(context, threadCountObject);
             }
         });
 
