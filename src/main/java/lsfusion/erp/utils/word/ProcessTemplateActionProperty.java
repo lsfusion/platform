c5a5293d36dcc42deaa526c294eb5b4ed8df8dca
Vandrouny <filipchyk.andrei@gmail.com>
2015-08-11 06:48:17 +0000

    [fsl] word templates: поддержка таблиц в docx

--- src/main/java/lsfusion/erp/utils/word/ProcessTemplateActionProperty.java
+++ src/main/java/lsfusion/erp/utils/word/ProcessTemplateActionProperty.java
@@ -3,7 +3,7 @@ package lsfusion.erp.utils.word;
 import com.google.common.base.Throwables;
 import lsfusion.server.data.SQLHandledException;
 import org.apache.poi.hwpf.HWPFDocument;
-import org.apache.poi.hwpf.usermodel.Range;
+import org.apache.poi.hwpf.usermodel.*;
 import org.apache.poi.poifs.filesystem.POIFSFileSystem;
 import org.apache.poi.xwpf.usermodel.*;
 import lsfusion.base.col.MapFact;
@@ -24,10 +24,9 @@ import lsfusion.server.logics.scripted.ScriptingLogicsModule;
 
 import java.io.*;
 import java.sql.SQLException;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+
+import static org.apache.commons.lang.StringUtils.trimToNull;
 
 public class ProcessTemplateActionProperty extends ScriptingActionProperty {
     public final ClassPropertyInterface templateInterface;
@@ -52,7 +51,7 @@ public class ProcessTemplateActionProperty extends ScriptingActionProperty {
                 if (fileObject != null) {
 
                     DataObject wordObject = new DataObject(fileObject, WordClass.get(false, false));
-                    Map<String, String> templateEntriesMap = new HashMap<>();
+                    List<List<Object>> templateEntriesList = new ArrayList<>();
 
                     KeyExpr templateEntryExpr = new KeyExpr("TemplateEntry");
                     ImRevMap<Object, KeyExpr> templateEntryKeys = MapFact.singletonRev((Object) "TemplateEntry", templateEntryExpr);
@@ -60,6 +59,7 @@ public class ProcessTemplateActionProperty extends ScriptingActionProperty {
                     QueryBuilder<Object, Object> templateEntryQuery = new QueryBuilder<>(templateEntryKeys);
                     templateEntryQuery.addProperty("keyTemplateEntry", findProperty("keyTemplateEntry").getExpr(context.getModifier(), templateEntryExpr));
                     templateEntryQuery.addProperty("valueTemplateEntry", findProperty("valueTemplateEntry").getExpr(context.getModifier(), templateEntryExpr));
+                    templateEntryQuery.addProperty("isTableTemplateEntry", findProperty("isTableTemplateEntry").getExpr(context.getModifier(), templateEntryExpr));
 
                     templateEntryQuery.and(findProperty("templateTemplateEntry").getExpr(context.getModifier(), templateEntryQuery.getMapExprs().get("TemplateEntry")).compare(templateObject.getExpr(), Compare.EQUALS));
 
@@ -67,11 +67,12 @@ public class ProcessTemplateActionProperty extends ScriptingActionProperty {
 
                     for (ImMap<Object, Object> templateEntry : templateEntryResult.values()) {
 
-                        String keyTemplateEntry = (String) templateEntry.get("keyTemplateEntry");
-                        String valueTemplateEntry = (String) templateEntry.get("valueTemplateEntry");
+                        String keyTemplateEntry = trimToNull((String) templateEntry.get("keyTemplateEntry"));
+                        String valueTemplateEntry = trimToNull((String) templateEntry.get("valueTemplateEntry"));
+                        boolean isTableTemplateEntry = templateEntry.get("isTableTemplateEntry") != null;
 
                         if (keyTemplateEntry != null && valueTemplateEntry != null)
-                            templateEntriesMap.put(keyTemplateEntry.trim(), valueTemplateEntry.trim().replace('\n', '\r'));
+                            templateEntriesList.add(Arrays.asList((Object) keyTemplateEntry, valueTemplateEntry.replace('\n', '\r'), isTableTemplateEntry));
                     }
 
                     boolean isDocx = ((byte[]) fileObject).length > 2 && ((byte[]) fileObject)[0] == 80 && ((byte[]) fileObject)[1] == 75;
@@ -81,47 +82,22 @@ public class ProcessTemplateActionProperty extends ScriptingActionProperty {
                     if (isDocx) {
                         XWPFDocument document = new XWPFDocument(new ByteArrayInputStream((byte[]) wordObject.object));
                         List<XWPFParagraph> docParagraphs = document.getParagraphs();
-
-                        for (Map.Entry<String, String> entry : templateEntriesMap.entrySet()) {
-                            for (XWPFParagraph p : docParagraphs) {
-                                List<XWPFRun> runs = p.getRuns();
-                                for (int i = runs.size() - 1; i >= 0; i--) {
-                                    String text = runs.get(i).getText(0);
-                                    if (text != null && text.contains(entry.getKey()))
-                                        text = text.replace(entry.getKey(), entry.getValue());
-                                    runs.get(i).setText(text, 0);
-                                }
-                            }
-
+                        for (List<Object> entry : templateEntriesList) {
+                            String key = (String) entry.get(0);
+                            String value = (String) entry.get(1);
+                            boolean isTable = (boolean) entry.get(2);
                             for (XWPFTable tbl : document.getTables()) {
-                                for (XWPFTableRow row : tbl.getRows()) {
-                                    for (XWPFTableCell cell : row.getTableCells()) {
-                                        for (XWPFParagraph p : cell.getParagraphs()) {
-                                            for (XWPFRun r : p.getRuns()) {
-                                                String text = r.getText(0);
-                                                if (text != null && text.contains(entry.getKey())) {
-                                                    text = text.replace(entry.getKey(), entry.getValue());
-                                                    r.setText(text, 0);
-                                                }
-                                            }
-                                        }
-                                    }
-                                }
+                                replaceTableDataDocx(tbl, key, value, isTable);
                             }
+                            replaceRegularDataDocx(docParagraphs, key, value);
                         }
-
-
-
                         document.write(outputStream);
-
                     } else {
                         HWPFDocument document = new HWPFDocument(new POIFSFileSystem(new ByteArrayInputStream((byte[]) wordObject.object)));
                         Range range = document.getRange();
-
-                        for (Map.Entry<String, String> entry : templateEntriesMap.entrySet()) {
-                            range.replaceText(entry.getKey(), entry.getValue());
+                        for (List<Object> entry : templateEntriesList) {
+                            range.replaceText((String) entry.get(0), (String) entry.get(1));
                         }
-
                         document.write(outputStream);
                     }
 
@@ -130,6 +106,68 @@ public class ProcessTemplateActionProperty extends ScriptingActionProperty {
             }
         } catch (IOException | ScriptingErrorLog.SemanticErrorException e) {
             throw Throwables.propagate(e);
+        }
+    }
+
+    private void replaceTableDataDocx(XWPFTable tbl, String key, String value, boolean isTable) {
+        if(isTable) {
+            XWPFTableRow row = tbl.getRow(0);
+            XWPFTableCell cell = row.getCell(0);
+            String text = cell.getText();
+            if (text != null && text.contains(key)) {
+                String[] tableRows = value.split("\r");
+                int i = 0;
+                for (String tableRow : tableRows) {
+                    if (i == 0) {
+                        XWPFTableRow newRow = tbl.getRow(0);
+                        int j = 0;
+                        for (String tableCell : tableRow.split(";")) {
+                            XWPFTableCell newCell = newRow.getTableICells().size() > j ? newRow.getCell(j) : newRow.createCell();
+                            if (newCell.getText().isEmpty())
+                                newCell.setText(tableCell);
+                            else {
+                                newCell.getParagraphs().get(0).getRuns().get(0).setText(tableCell, 0);
+                            }
+                            j++;
+                        }
+                    } else {
+                        XWPFTableRow newRow = tbl.createRow();
+                        int j = 0;
+                        for (String tableCell : tableRow.split(";")) {
+                            XWPFTableCell newCell = newRow.getTableICells().size() > j ? newRow.getCell(j) : newRow.createCell();
+                            newCell.setText(tableCell);
+                            j++;
+                        }
+                    }
+                    i++;
+                }
+            }
+        } else {
+            for (XWPFTableRow row : tbl.getRows()) {
+                for (XWPFTableCell cell : row.getTableCells()) {
+                    for (XWPFParagraph p : cell.getParagraphs()) {
+                        for (XWPFRun r : p.getRuns()) {
+                            String text = r.getText(0);
+                            if (text != null && text.contains(key)) {
+                                text = text.replace(key, value);
+                                r.setText(text, 0);
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    private void replaceRegularDataDocx(List<XWPFParagraph> docParagraphs, String key, String value) {
+        for (XWPFParagraph p : docParagraphs) {
+            List<XWPFRun> runs = p.getRuns();
+            for (int i = runs.size() - 1; i >= 0; i--) {
+                String text = runs.get(i).getText(0);
+                if (text != null && text.contains(key))
+                    text = text.replace(key, value);
+                runs.get(i).setText(text, 0);
+            }
         }
     }
 }
