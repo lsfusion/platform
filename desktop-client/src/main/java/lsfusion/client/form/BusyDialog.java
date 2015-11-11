b694087518ecf5cbcaebb55e2ada48e1c0214fab
Vandrouny <filipchyk.andrei@gmail.com>
2015-11-11 12:37:32 +0000

    [client] BusyDialog - 60 секунд вместо 5

--- desktop-client/src/main/java/lsfusion/client/form/BusyDialog.java
+++ desktop-client/src/main/java/lsfusion/client/form/BusyDialog.java
@@ -151,7 +151,7 @@ class BusyDialog extends JDialog {
             }
         });
 
-        longActionTimer = new Timer(5000, new ActionListener() {
+        longActionTimer = new Timer(60000, new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
                 btnExit.setEnabled(true);
