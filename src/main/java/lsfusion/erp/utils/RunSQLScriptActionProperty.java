c9d64ec5de37947da759d0c59a931771f1787aeb
Vandrouny <filipchyk.andrei@gmail.com>
2016-01-26 11:44:49 +0000

    [server] перенос потабличной упаковки в SQLUtils

---
+++ src/main/java/lsfusion/erp/utils/RunSQLScriptActionProperty.java
@@ -0,0 +1,37 @@
+package lsfusion.erp.utils;
+
+import com.google.common.base.Throwables;
+import lsfusion.server.ServerLoggers;
+import lsfusion.server.classes.ValueClass;
+import lsfusion.server.data.SQLHandledException;
+import lsfusion.server.logics.property.ClassPropertyInterface;
+import lsfusion.server.logics.property.ExecutionContext;
+import lsfusion.server.logics.scripted.ScriptingActionProperty;
+import lsfusion.server.logics.scripted.ScriptingLogicsModule;
+
+import java.sql.SQLException;
+import java.util.Iterator;
+
+public class RunSQLScriptActionProperty extends ScriptingActionProperty {
+    private final ClassPropertyInterface stringInterface;
+
+    public RunSQLScriptActionProperty(ScriptingLogicsModule LM, ValueClass... classes) {
+        super(LM, classes);
+
+        Iterator<ClassPropertyInterface> i = interfaces.iterator();
+        stringInterface = i.next();
+    }
+
+    @Override
+    protected void executeCustom(ExecutionContext<ClassPropertyInterface> context) throws SQLException, SQLHandledException {
+        try {
+            String script = (String) context.getDataKeyValue(stringInterface).object;
+            if(script != null) {
+                ServerLoggers.systemLogger.info("Executing SQL: " + script);
+                context.getSession().sql.executeDDL(script);
+            }
+        } catch (Exception e) {
+            throw Throwables.propagate(e);
+        }
+    }
+}
