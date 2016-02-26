52410ae4270f923e2cfb489ac2bb060f894aa098
pavel-miniutka <asphalt.by@gmail.com>
2016-02-26 07:18:27 +0000

    статистика кэшей

--- src/main/java/lsfusion/erp/utils/UpdateThreadAllocatedBytesActionProperty.java
+++ src/main/java/lsfusion/erp/utils/UpdateThreadAllocatedBytesActionProperty.java
@@ -1,8 +1,11 @@
 package lsfusion.erp.utils;
 
 import com.google.common.base.Throwables;
+import lsfusion.base.col.MapFact;
 import lsfusion.server.ServerLoggers;
 import lsfusion.server.Settings;
+import lsfusion.server.caches.CacheStats;
+import lsfusion.server.caches.CacheStats.CacheType;
 import lsfusion.server.context.ThreadLocalContext;
 import lsfusion.server.data.SQLHandledException;
 import lsfusion.server.data.SQLSession;
@@ -18,9 +21,13 @@ import lsfusion.server.logics.scripted.ScriptingLogicsModule;
 import java.lang.management.ManagementFactory;
 import java.lang.management.ThreadMXBean;
 import java.sql.SQLException;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
+import java.util.concurrent.ConcurrentHashMap;
+
+import static lsfusion.base.BaseUtils.nullToZero;
 
 public class UpdateThreadAllocatedBytesActionProperty extends ScriptingActionProperty {
     private static Timer timer;
@@ -40,6 +47,7 @@ public class UpdateThreadAllocatedBytesActionProperty extends ScriptingActionPro
                     } else {
                         final long period = Settings.get().getThreadAllocatedMemoryPeriod();
                         final long maxAllocatedBytes = Settings.get().getMaxThreadAllocatedBytes();
+                        final int cacheMissesStatsLimit = Settings.get().getCacheMissesStatsLimit();
 
                         final LogicsInstance logicsInstance = context.getLogicsInstance();
                         timer = new Timer("ReadAllocatedBytes", true);
@@ -47,8 +55,7 @@ public class UpdateThreadAllocatedBytesActionProperty extends ScriptingActionPro
                             @Override
                             public void run() {
                                 ThreadLocalContext.set(logicsInstance.getContext());
-                                ThreadMXBean tBean = ManagementFactory.getThreadMXBean();
-                                updateThreadAllocatedBytesMap(tBean, maxAllocatedBytes);
+                                updateThreadAllocatedBytesMap(maxAllocatedBytes, cacheMissesStatsLimit);
                             }
                         }, 0, (period <= 0 ? 1800000 : period) / 2);
                     }
@@ -59,39 +66,89 @@ public class UpdateThreadAllocatedBytesActionProperty extends ScriptingActionPro
         }
     }
 
-    private void updateThreadAllocatedBytesMap(ThreadMXBean tBean, long maxAllocatedBytes) {
+    private void updateThreadAllocatedBytesMap(long maxAllocatedBytes, int cacheMissesStatsLimit) {
+        ThreadMXBean tBean = ManagementFactory.getThreadMXBean();
         if (tBean instanceof com.sun.management.ThreadMXBean && ((com.sun.management.ThreadMXBean) tBean).isThreadAllocatedMemorySupported()) {
             long time = System.currentTimeMillis();
-            long sum = 0;
-            long totalSum = 0;
+            long bytesSum = 0;
+            long totalBytesSum = 0;
             SQLSession.updateThreadAllocatedBytesMap();
             Map<Long, Thread> threadMap = ThreadUtils.getThreadMap();
 
+            ConcurrentHashMap<Long, HashMap<CacheType, Long>> hitStats = MapFact.getGlobalConcurrentHashMap(CacheStats.getCacheHitStats());
+            ConcurrentHashMap<Long, HashMap<CacheType, Long>> missedStats = MapFact.getGlobalConcurrentHashMap(CacheStats.getCacheMissedStats());
+            CacheStats.resetStats();
+
+            long totalHit = 0;
+            long totalMissed = 0;
+            HashMap<CacheType, Long> totalHitMap = new HashMap<>();
+            HashMap<CacheType, Long> totalMissedMap = new HashMap<>();
+            long exceededMisses = 0;
+            long exceededMissesHits = 0;
+            HashMap<CacheType, Long> exceededHitMap = new HashMap<>();
+            HashMap<CacheType, Long> exceededMissedMap = new HashMap<>();
+
+            boolean logTotal = false;
+            
             for (Map.Entry<Long, Long> bEntry : SQLSession.threadAllocatedBytesBMap.entrySet()) {
                 Long id = bEntry.getKey();
                 if (id != null) {
                     Long bBytes = bEntry.getValue();
                     Long aBytes = SQLSession.threadAllocatedBytesAMap.get(bEntry.getKey());
 
-                    Long delta = bBytes != null && aBytes != null ? (bBytes - aBytes) : 0;
-                    totalSum += delta;
-                    if (delta > maxAllocatedBytes) {
-                        sum += delta;
+                    Long deltaBytes = bBytes != null && aBytes != null ? (bBytes - aBytes) : 0;
+                    totalBytesSum += deltaBytes;
+
+                    long userMissed = 0;
+                    long userHit = 0;
+
+                    HashMap<CacheType, Long> userHitMap = hitStats.get(id) != null ? hitStats.get(id) : new HashMap<CacheType, Long>();
+                    HashMap<CacheType, Long> userMissedMap = missedStats.get(id) != null ? missedStats.get(id) : new HashMap<CacheType, Long>();
+                    for (CacheType cacheType : CacheType.values()) {
+                        Long hit = nullToZero(userHitMap.get(cacheType));
+                        Long missed = nullToZero(userMissedMap.get(cacheType));
+                        userHit += hit;
+                        userMissed += missed;
+                    }
+                    totalHit += userHit;
+                    totalMissed += userMissed;
+                    sumMap(totalHitMap, userHitMap);
+                    sumMap(totalMissedMap, userMissedMap);
+                    
+                    if (deltaBytes > maxAllocatedBytes || userMissed > cacheMissesStatsLimit) {
+                        logTotal = true;
+                        
+                        bytesSum += deltaBytes;
+
+                        exceededMisses += userMissed;
+                        exceededMissesHits += userHit;
+                        sumMap(exceededHitMap, userHitMap);
+                        sumMap(exceededMissedMap, userMissedMap);
+                        
                         Thread thread = threadMap.get(id);
                         LogInfo logInfo = thread == null ? null : ThreadLocalContext.logInfoMap.get(thread);
                         String computer = logInfo == null ? null : logInfo.hostnameComputer;
                         String user = logInfo == null ? null : logInfo.userName;
-                        if (user == null)
-                            ServerLoggers.allocatedBytesLogger.info(String.format("Process ID %s: %s", bEntry.getKey(), humanReadableByteCount(delta)));
-                        else
-                            ServerLoggers.allocatedBytesLogger.info(String.format("Process ID %s, Computer %s, User %s: %s", bEntry.getKey(),
-                                    computer == null ? "unknown" : computer, user, humanReadableByteCount(delta)));
+                        
+                        String userMessage;
+                        if (user == null) {
+                            userMessage = String.format("PID %s: %s", bEntry.getKey(), humanReadableByteCount(deltaBytes));
+                        } else {
+                            userMessage = String.format("PID %s, %s, Comp. %s, User %s", bEntry.getKey(),
+                                    humanReadableByteCount(deltaBytes), computer == null ? "unknown" : computer, user);
+                        }
+                        userMessage += String.format(", missed-hit: All: %s-%s, %s", userMissed, userHit, getString(userHitMap, userMissedMap));
+                        
+                        ServerLoggers.allocatedBytesLogger.info(userMessage);
                     }
                 }
             }
-            if (sum > 0)
-                ServerLoggers.allocatedBytesLogger.info(String.format("Reading allocated bytes: elapsed %sms, sum: %s, totalSum: %s",
-                        System.currentTimeMillis() - time, humanReadableByteCount(sum), humanReadableByteCount(totalSum)));
+            if (logTotal) {
+                ServerLoggers.allocatedBytesLogger.info(String.format("Exceeded: sum: %s, \t\t\tmissed-hit: All: %s-%s, %s", 
+                        humanReadableByteCount(bytesSum), exceededMisses, exceededMissesHits, getString(exceededHitMap, exceededMissedMap)));
+                ServerLoggers.allocatedBytesLogger.info(String.format("Total: sum: %s, elapsed %sms, missed-hit: All: %s-%s, %s",
+                        humanReadableByteCount(totalBytesSum), System.currentTimeMillis() - time, totalMissed, totalHit, getString(totalHitMap, totalMissedMap)));
+            }
         }
     }
 
@@ -101,5 +158,23 @@ public class UpdateThreadAllocatedBytesActionProperty extends ScriptingActionPro
         int exp = (int) (Math.log(bytes) / Math.log(unit));
         char pre = "KMGTPE".charAt(exp - 1);
         return String.format("%.1f %sB", bytes / Math.pow(unit, exp), pre);
+    }
+    
+    private String getString(HashMap<CacheType, Long> hitStats, HashMap<CacheType, Long> missedStats) {
+        String result = "";
+        for (int i = 0; i < CacheType.values().length; i++) {
+            CacheType type = CacheType.values()[i];
+            result += type + ": " + nullToZero(missedStats.get(type)) + "-" + nullToZero(hitStats.get(type));
+            if (i < CacheType.values().length - 1) {
+                result += "; ";
+            }
+        }
+        return result;
+    }
+    
+    private void sumMap(HashMap<CacheType, Long> target, HashMap<CacheType, Long> source) {
+        for (CacheType type : CacheType.values()) {
+            target.put(type, nullToZero(target.get(type)) + nullToZero(source.get(type)));
+        }
     }
 }
