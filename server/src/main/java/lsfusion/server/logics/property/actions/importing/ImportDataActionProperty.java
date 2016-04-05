0f5d39be995f3958e1cd5666bcd38b23c724af13
danchanka <danchenko@gmail.com>
2016-04-05 08:20:42 +0000

    Указание колонок в экселе до 256-ой при импорте

--- server/src/main/java/lsfusion/server/logics/property/actions/importing/ImportDataActionProperty.java
+++ server/src/main/java/lsfusion/server/logics/property/actions/importing/ImportDataActionProperty.java
@@ -47,15 +47,36 @@ public abstract class ImportDataActionProperty extends ScriptingActionProperty {
     protected final List<String> ids;
     protected final List<LCP> properties;
 
-    public final static Map<String, Integer> XLSColumnsMapping = new HashMap<String, Integer>() {{
-        put("A", 0); put("B", 1); put("C", 2); put("D", 3); put("E", 4); put("F", 5); put("G", 6); put("H", 7); put("I", 8);
-        put("J", 9); put("K", 10); put("L", 11); put("M", 12); put("N", 13); put("O", 14); put("P", 15); put("Q", 16);
-        put("R", 17); put("S", 18); put("T", 19); put("U", 20); put("V", 21); put("W", 22); put("X", 23); put("Y", 24);
-        put("Z", 25); put("AA", 26); put("AB", 27); put("AC", 28); put("AD", 29); put("AE", 30); put("AF", 31); put("AG", 32);
-        put("AH", 33); put("AI", 34); put("AJ", 35); put("AK", 36); put("AL", 37); put("AM", 38); put("AN", 39); put("AO", 40);
-        put("AP", 41); put("AQ", 42); put("AR", 43); put("AS", 44); put("AT", 45); put("BA", 52); put("BB", 53); put("BC", 54);
-    }};
+    public final static Map<String, Integer> XLSColumnsMapping = new HashMap<>();
+    static {
+        fillXLSCollumsMapping();
+    }
 
+    private static String XLSColumnByIndex(int index) {
+        String columnName = "";
+        int resultLen = 1;
+        final int LETTERS_CNT = 26;
+        int sameLenCnt = LETTERS_CNT;
+        while (sameLenCnt <= index) {
+            ++resultLen;
+            index -= sameLenCnt;
+            sameLenCnt *= LETTERS_CNT;
+        }
+        
+        for (int i = 0; i < resultLen; ++i) {
+            columnName = (char)('A' + (index % LETTERS_CNT)) + columnName; 
+            index /= LETTERS_CNT;
+        }
+        return columnName;
+    }
+    
+    private static void fillXLSCollumsMapping() {
+        final int MAX_COLUMN = 256;
+        for (int i = 0; i < MAX_COLUMN; ++i) {
+            XLSColumnsMapping.put(XLSColumnByIndex(i), i);
+        }
+    }
+    
     public static ImportDataActionProperty createExcelProperty(ValueClass valueClass, ImportSourceFormat format, ScriptingLogicsModule LM, List<String> ids, List<LCP> properties, ValueClass sheetIndex) {
         if (format == ImportSourceFormat.XLS) {
             return new ImportXLSDataActionProperty(sheetIndex == null ? new ValueClass[] {valueClass} : new ValueClass[] {valueClass, sheetIndex} , LM, ids, properties);
