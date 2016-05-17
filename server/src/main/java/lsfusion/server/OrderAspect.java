3a7b306fed5a3face37bc3eb0dc195eb5bbdcfd2
AlexKirkouski <alex.kirkouski@gmail.com>
2016-05-17 09:26:44 +0000

    Оптимизация кэширования

--- server/src/main/java/lsfusion/server/OrderAspect.java
+++ server/src/main/java/lsfusion/server/OrderAspect.java
@@ -3,8 +3,8 @@ package lsfusion.server;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.DeclarePrecedence;
 
-
+// Pack делаем до кэширования, чтобы не зависали ссылки на ключи в LRU кэшах
 @Aspect
-@DeclarePrecedence("lsfusion.server.remote.RemoteContextAspect, lsfusion.server.remote.RemoteLoggerAspect, lsfusion.server.caches.CacheAspect, lsfusion.server.data.translator.AfterTranslateAspect, lsfusion.server.caches.AutoHintsAspect, lsfusion.server.caches.QueryCacheAspect, lsfusion.server.data.query.MapCacheAspect, lsfusion.server.caches.WrapComplexityAspect, lsfusion.server.MessageAspect, lsfusion.server.caches.PackComplexityAspect")
+@DeclarePrecedence("lsfusion.server.remote.RemoteContextAspect, lsfusion.server.remote.RemoteLoggerAspect, lsfusion.server.caches.PackComplexityAspect, lsfusion.server.caches.CacheAspect, lsfusion.server.data.translator.AfterTranslateAspect, lsfusion.server.caches.AutoHintsAspect, lsfusion.server.caches.QueryCacheAspect, lsfusion.server.data.query.MapCacheAspect, lsfusion.server.caches.WrapComplexityAspect, lsfusion.server.MessageAspect")
 public class OrderAspect {
 }
