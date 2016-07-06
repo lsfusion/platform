574c3fc86885ed93924ed0840b15663f2bb6ad7b
Vandrouny <filipchyk.andrei@gmail.com>
2016-07-06 07:35:52 +0000

    к предыдущему: версия db опущена, а версия api поднята

--- server/src/main/java/lsfusion/server/remote/RemoteLogics.java
+++ server/src/main/java/lsfusion/server/remote/RemoteLogics.java
@@ -188,7 +188,7 @@ public class RemoteLogics<T extends BusinessLogics> extends ContextAwarePendingR
 
     @Override
     public Integer getApiVersion() throws RemoteException {
-        return 11;
+        return 12;
     }
 
     public GUIPreferences getGUIPreferences() throws RemoteException {
