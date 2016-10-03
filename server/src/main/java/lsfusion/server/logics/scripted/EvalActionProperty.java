9b0c50cdf1a3efafd3ec08b4527d1fc7413f446a
Vandrouny <filipchyk.andrei@gmail.com>
2016-10-03 08:58:35 +0000

    [server] EVAL: сохранить как - в новой сессии, при выполнении старого скрипта обновляем дату запуска

--- server/src/main/java/lsfusion/server/logics/scripted/EvalActionProperty.java
+++ server/src/main/java/lsfusion/server/logics/scripted/EvalActionProperty.java
@@ -6,6 +6,7 @@ import lsfusion.server.classes.ConcreteCustomClass;
 import lsfusion.server.data.SQLHandledException;
 import lsfusion.server.logics.BusinessLogics;
 import lsfusion.server.logics.DataObject;
+import lsfusion.server.logics.NullValue;
 import lsfusion.server.logics.ObjectValue;
 import lsfusion.server.logics.i18n.LocalizedString;
 import lsfusion.server.logics.linear.LAP;
@@ -63,13 +64,14 @@ public class EvalActionProperty<P extends PropertyInterface> extends SystemExpli
             LAP<?> runAction = module.findAction(runName);
             if (runAction != null) {
                 String textScript = (String) evalLM.findProperty("scriptStorage[]").read(context);
-                if (evalLM.findProperty("countTextScript[]").read(context) == null) {
-                    try (DataSession session = context.createSession()) {
-                        DataObject scriptObject = session.addObject((ConcreteCustomClass) evalLM.findClass("Script"));
-                        evalLM.findProperty("text[Script]").change(textScript, session, scriptObject);
-                        evalLM.findProperty("dateTime[Script]").change(new Timestamp(Calendar.getInstance().getTime().getTime()), session, scriptObject);
-                        session.apply(context);
+                try (DataSession session = context.createSession()) {
+                    ObjectValue scriptObject = evalLM.findProperty("textScript[]").readClasses(session);
+                    if (scriptObject instanceof NullValue) {
+                        scriptObject = session.addObject((ConcreteCustomClass) evalLM.findClass("Script"));
+                        evalLM.findProperty("text[Script]").change(textScript, session, (DataObject) scriptObject);
                     }
+                    evalLM.findProperty("dateTime[Script]").change(new Timestamp(Calendar.getInstance().getTime().getTime()), session, (DataObject) scriptObject);
+                    session.apply(context);
                 }
                 runAction.execute(context);
             }
