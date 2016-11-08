92175131bfd7921add4760e3ec4e757d3cdce735
pavel-miniutka <asphalt.by@gmail.com>
2016-11-08 11:17:12 +0000

    [Desktop]: изменён порядок обработки результатов запросов - соблюдаем очередь

--- desktop-client/src/main/java/lsfusion/client/form/RmiQueue.java
+++ desktop-client/src/main/java/lsfusion/client/form/RmiQueue.java
@@ -190,7 +190,7 @@ public class RmiQueue {
                     rmiFuture = execRmiRequestInternal(request);
                 }
 
-                while (!rmiFuture.isDone()) {
+                while ((direct && !rmiFuture.isDone()) || (!direct && !rmiFutures.isEmpty())) {
                     waitOnEdtSyncBlocker();
 
                     ConnectionLostManager.blockIfHasFailed();
@@ -236,8 +236,7 @@ public class RmiQueue {
                 } else {
                     rmiFuture = execRmiRequestInternal(request);
                 }
-
-                while (!rmiFuture.isDone()) {
+                while ((direct && !rmiFuture.isDone()) || (!direct && !rmiFutures.isEmpty())) { // дожидаемся выполнения всех запросов в очереди
                     long timeout = 1000 - (System.currentTimeMillis() - start);
 
                     boolean flush = !direct;
@@ -253,7 +252,7 @@ public class RmiQueue {
                         busyDisplayer.show(new Runnable() {
                             @Override
                             public void run() {
-                                while (!rmiFuture.isDone() && !(!direct && isRmiFutureDone())) {
+                                while ((direct && !rmiFuture.isDone()) || !isRmiFutureDone()) {
                                     try {
                                         waitOnEdtSyncBlocker();
                                     } catch (InterruptedException e) {
