365e5b2720efedc531ede2d4c0a650439fd40da9
Vandrouny <filipchyk.andrei@gmail.com>
2017-02-15 09:07:03 +0000

    [server, desktop, fsl, api, web] Копирование SQL запроса в буфер обмена (desktop), показ на форме (web) (apiVersion 23 -> 24)

--- fsl-server/src/main/java/lsfusion/erp/utils/CopyFullQuerySQLProcessActionProperty.java
+++ fsl-server/src/main/java/lsfusion/erp/utils/CopyFullQuerySQLProcessActionProperty.java
@@ -1,16 +1,14 @@
 package lsfusion.erp.utils;
 
 import com.google.common.base.Throwables;
+import lsfusion.interop.action.CopyToClipboardClientAction;
 import lsfusion.server.classes.ValueClass;
 import lsfusion.server.logics.DataObject;
-import lsfusion.server.logics.ThreadUtils;
 import lsfusion.server.logics.property.ClassPropertyInterface;
 import lsfusion.server.logics.property.ExecutionContext;
 import lsfusion.server.logics.scripted.ScriptingActionProperty;
 import lsfusion.server.logics.scripted.ScriptingLogicsModule;
 
-import java.awt.*;
-import java.awt.datatransfer.StringSelection;
 import java.sql.SQLException;
 import java.util.Iterator;
 
@@ -29,8 +27,10 @@ public class CopyFullQuerySQLProcessActionProperty extends ScriptingActionProper
         try {
             DataObject currentObject = context.getDataKeyValue(integerInterface);
             String fullQuery = (String) findProperty("fullQuerySQLProcess[VARSTRING[10]]").read(context, currentObject);
-            if(fullQuery != null && !fullQuery.isEmpty())
-            Toolkit.getDefaultToolkit().getSystemClipboard().setContents(new StringSelection(fullQuery.trim()), null);
+            if(fullQuery != null && !fullQuery.isEmpty()) {
+                if (!(boolean) context.requestUserInteraction(new CopyToClipboardClientAction(fullQuery)))
+                    findProperty("copiedFullQuerySQLProcess[VARSTRING[10]]").change(fullQuery, context, currentObject);
+            }
         } catch (Exception e) {
             throw Throwables.propagate(e);
         }
