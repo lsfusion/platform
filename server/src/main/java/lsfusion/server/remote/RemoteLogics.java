be1b1f62a120f6cb5f04075d0565eede5490cccf
danchanka <danchenko@gmail.com>
2017-02-21 13:03:26 +0000

    Api version

--- server/src/main/java/lsfusion/server/remote/RemoteLogics.java
+++ server/src/main/java/lsfusion/server/remote/RemoteLogics.java
@@ -169,7 +169,7 @@ public class RemoteLogics<T extends BusinessLogics> extends ContextAwarePendingR
 
     @Override
     public Integer getApiVersion() throws RemoteException {
-        return 24;
+        return 25;
     }
 
     public GUIPreferences getGUIPreferences() throws RemoteException {
