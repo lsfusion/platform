661dd7773ea5a826d7f1f40db37989e459238d81
pavel-miniutka <asphalt.by@gmail.com>
2017-03-29 13:13:57 +0000

    api version вдогонку

--- server/src/main/java/lsfusion/server/remote/RemoteLogics.java
+++ server/src/main/java/lsfusion/server/remote/RemoteLogics.java
@@ -176,7 +176,7 @@ public class RemoteLogics<T extends BusinessLogics> extends ContextAwarePendingR
 
     @Override
     public Integer getApiVersion() throws RemoteException {
-        return 30;
+        return 31;
     }
 
     public GUIPreferences getGUIPreferences() throws RemoteException {
