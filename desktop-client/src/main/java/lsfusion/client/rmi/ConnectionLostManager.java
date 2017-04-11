c2a3be7120f743aa323df0711cdc6100b5f53873
Vandrouny <filipchyk.andrei@gmail.com>
2017-04-11 12:07:08 +0000

    [client] ConnectionLost - выходим по крестику

--- desktop-client/src/main/java/lsfusion/client/rmi/ConnectionLostManager.java
+++ desktop-client/src/main/java/lsfusion/client/rmi/ConnectionLostManager.java
@@ -13,10 +13,7 @@ import lsfusion.interop.remote.ClientCallBackInterface;
 
 import javax.swing.*;
 import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.WindowAdapter;
-import java.awt.event.WindowEvent;
+import java.awt.event.*;
 import java.rmi.RemoteException;
 import java.util.ArrayList;
 import java.util.Collection;
@@ -254,7 +251,7 @@ public class ConnectionLostManager {
 
             this.message = message;
 
-            setDefaultCloseOperation(JDialog.DO_NOTHING_ON_CLOSE);
+            setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
 
             btnExit = new JButton(getString("rmi.connectionlost.exit"));
             btnExit.setEnabled(false);
@@ -302,6 +299,13 @@ public class ConnectionLostManager {
             setFocusableWindowState(false);
             
             setLocationRelativeTo(owner);
+
+            addWindowListener(new WindowAdapter() {
+                @Override
+                public void windowClosing(WindowEvent e) {
+                    Main.shutdown();
+                }
+            });
         }
 
         public void showDialog() {
