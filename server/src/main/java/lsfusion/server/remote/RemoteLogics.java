3f5331496faa2efd41b6d3f9b617f1288d926b0c
Vandrouny <filipchyk.andrei@gmail.com>
2017-04-21 09:25:19 +0000

    [server] apiVersion 32->33 (из-за того, что некоторые старые изменения пошли в RC)

--- server/src/main/java/lsfusion/server/remote/RemoteLogics.java
+++ server/src/main/java/lsfusion/server/remote/RemoteLogics.java
@@ -176,7 +176,7 @@ public class RemoteLogics<T extends BusinessLogics> extends ContextAwarePendingR
 
     @Override
     public Integer getApiVersion() throws RemoteException {
-        return 32;
+        return 33;
     }
 
     public GUIPreferences getGUIPreferences() throws RemoteException {
