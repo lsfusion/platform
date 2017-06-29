dae4afecdf590b24c965287e72300d92290b30e2
AlexKirkouski <alex.kirkouski@gmail.com>
2017-06-29 15:05:00 +0000

    Изменил эвристику MANAGESESSION

--- server/src/main/java/lsfusion/server/form/instance/FormInstance.java
+++ server/src/main/java/lsfusion/server/form/instance/FormInstance.java
@@ -345,7 +345,7 @@ public class FormInstance<T extends BusinessLogics<T>> extends ExecutionEnvironm
         } else {
             int prevOwners = updateSessionOwner(true, stack);
      
-            boolean heurManageSession = heuristicManageSession(prevOwners) && (!heurReadOnly || session.isStoredDataChanged());
+            boolean heurManageSession = heuristicManageSession(prevOwners) && !heurReadOnly;
             if(manageSession == ManageSessionType.AUTO) {
                 if(heurManageSession) { // если нет owner'ов
                     assert !heurReadOnly;
