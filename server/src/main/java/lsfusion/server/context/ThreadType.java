b5b108cc3d06c06a95bb417a0707319a3c625044
AlexKirkouski <alex.kirkouski@gmail.com>
2017-10-06 14:43:11 +0000

    Рефакторинг с потоками небольшой (temporary commit)

---
+++ server/src/main/java/lsfusion/server/context/ThreadType.java
@@ -0,0 +1,7 @@
+package lsfusion.server.context;
+
+// temporary
+public class ThreadType {
+    public static final ThreadInfo HTTP = new EventThreadInfo("noevent");
+    public static final ThreadInfo RMI = new EventThreadInfo("noevent");
+}
