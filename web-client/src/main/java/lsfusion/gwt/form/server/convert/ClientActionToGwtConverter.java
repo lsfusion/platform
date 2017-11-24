50c10e14d6600e5debece64884a858ddae2c6c99
Vandrouny <filipchyk.andrei@gmail.com>
2017-11-24 08:52:26 +0000

    [web-client] расширение для displayName для OPEN файлов

--- web-client/src/main/java/lsfusion/gwt/form/server/convert/ClientActionToGwtConverter.java
+++ web-client/src/main/java/lsfusion/gwt/form/server/convert/ClientActionToGwtConverter.java
@@ -157,7 +157,7 @@ public class ClientActionToGwtConverter extends ObjectConverter {
 
     @Converter(from = OpenFileClientAction.class)
     public GOpenFileAction convertAction(OpenFileClientAction action) {
-        return new GOpenFileAction(FileUtils.saveFile(action.file, action.name, action.extension), action.name);
+        return new GOpenFileAction(FileUtils.saveFile(action.file, action.name, action.extension), action.name + (action.extension != null ? "." + action.extension : ""));
     }
 
     @Converter(from = SaveFileClientAction.class)
