e3b9fdb5d659fef969825601a578d73d20507f11
Vandrouny <filipchyk.andrei@gmail.com>
2018-05-14 11:52:29 +0000

    [fsl] moveFile refactoring

--- fsl-server/src/main/java/lsfusion/erp/utils/utils/FileUtils.java
+++ fsl-server/src/main/java/lsfusion/erp/utils/utils/FileUtils.java
@@ -3,16 +3,55 @@ package lsfusion.erp.utils.utils;
 import com.google.common.base.Throwables;
 import com.jcraft.jsch.JSchException;
 import com.jcraft.jsch.SftpException;
+import lsfusion.server.ServerLoggers;
 import lsfusion.server.logics.property.ExecutionContext;
 import lsfusion.server.logics.property.actions.ReadUtils;
+import lsfusion.server.logics.property.actions.WriteActionProperty;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.FileCopyUtils;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class FileUtils {
+
+    public static void moveFile(ExecutionContext context, String sourcePath, String destinationPath, boolean isClient) throws SQLException, JSchException, SftpException, IOException {
+        Path srcPath = parsePath(sourcePath, isClient);
+        Path destPath = parsePath(destinationPath, isClient);
+
+        if(isClient) {
+            boolean result = (boolean) context.requestUserInteraction(new FileClientAction(2, srcPath.path, destPath.path));
+            if (!result)
+                throw new RuntimeException(String.format("Failed to move file from %s to %s", sourcePath, destinationPath));
+        } else {
+            ReadUtils.ReadResult readResult = ReadUtils.readFile(sourcePath, false, false, false);
+            if (readResult.errorCode == 0) {
+                File sourceFile = new File(readResult.filePath);
+                try {
+                    switch (destPath.type) {
+                        case "ftp":
+                            WriteActionProperty.storeFileToFTP(destinationPath, sourceFile);
+                            break;
+                        case "sftp":
+                            WriteActionProperty.storeFileToSFTP(destinationPath, sourceFile);
+                            break;
+                        default:
+                            ServerLoggers.importLogger.info(String.format("Writing file to %s", destinationPath));
+                            FileCopyUtils.copy(sourceFile, new File(destPath.path));
+                            break;
+                    }
+                } finally {
+                    deleteFile(srcPath, sourcePath);
+                }
+
+            } else if (readResult.error != null) {
+                throw new RuntimeException(readResult.error);
+            }
+        }
+    }
 
     public static void deleteFile(ExecutionContext context, String sourcePath, boolean isClient) throws SftpException, JSchException, IOException {
         Path path = parsePath(sourcePath, isClient);
@@ -21,20 +60,21 @@ public class FileUtils {
             if (!result)
                 throw new RuntimeException(String.format("Failed to delete file '%s'", sourcePath));
         } else {
-            switch (path.type) {
-                case "ftp":
-                    ReadUtils.deleteFTPFile(path.path);
-                    break;
-                case "sftp":
-                    ReadUtils.deleteSFTPFile(path.path);
-                    break;
-                default:
-                    File sourceFile = new File(path.path);
-                    if (!sourceFile.delete()) {
-                        throw new RuntimeException(String.format("Failed to delete file '%s'", sourceFile));
-                    }
-                    break;
-            }
+            deleteFile(path, sourcePath);
+        }
+    }
+
+    private static void deleteFile(Path path, String sourcePath) throws IOException, SftpException, JSchException {
+        File sourceFile = new File(path.path);
+        if (!sourceFile.delete()) {
+            throw new RuntimeException(String.format("Failed to delete file '%s'", sourceFile));
+        }
+        if (path.type.equals("ftp")) {
+            //todo: parseFTPPath может принимать путь без ftp://
+            ReadUtils.deleteFTPFile(sourcePath);
+        } else if (path.type.equals("sftp")) {
+            //todo: parseFTPPath может принимать путь без ftp://
+            ReadUtils.deleteSFTPFile(sourcePath);
         }
     }
 
@@ -62,5 +102,4 @@ public class FileUtils {
             this.path = path;
         }
     }
-
 }
--- fsl-server/src/main/java/lsfusion/erp/utils/utils/MoveFileActionProperty.java
+++ fsl-server/src/main/java/lsfusion/erp/utils/utils/MoveFileActionProperty.java
@@ -5,17 +5,11 @@ import lsfusion.server.classes.ValueClass;
 import lsfusion.server.data.SQLHandledException;
 import lsfusion.server.logics.property.ClassPropertyInterface;
 import lsfusion.server.logics.property.ExecutionContext;
-import lsfusion.server.logics.property.actions.ReadUtils;
-import lsfusion.server.logics.property.actions.WriteActionProperty;
 import lsfusion.server.logics.scripted.ScriptingActionProperty;
 import lsfusion.server.logics.scripted.ScriptingLogicsModule;
-import org.springframework.util.FileCopyUtils;
 
-import java.io.File;
 import java.sql.SQLException;
 import java.util.Iterator;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class MoveFileActionProperty extends ScriptingActionProperty {
     private final ClassPropertyInterface sourceInterface;
@@ -37,48 +31,7 @@ public class MoveFileActionProperty extends ScriptingActionProperty {
             String destinationPath = (String) context.getKeyValue(destinationInterface).getValue();
             boolean isClient = context.getKeyValue(isClientInterface).getValue() != null;
             if (sourcePath != null && destinationPath != null) {
-
-                if(isClient) {
-
-                    Pattern p = Pattern.compile("(?:file:(?://)?)?(.*)");
-                    Matcher m = p.matcher(sourcePath);
-                    if (m.matches()) {
-                        boolean result = (boolean) context.requestUserInteraction(new FileClientAction(2, sourcePath.replace("file://", ""), destinationPath.replace("file://", "")));
-                        if (!result)
-                            throw Throwables.propagate(new RuntimeException(String.format("Failed to move file from %s to %s", sourcePath, destinationPath)));
-                    } else {
-                        throw Throwables.propagate(new RuntimeException("Unsupported move source: " + sourcePath + ". Please use format: file://path"));
-                    }
-
-                } else {
-
-                    Pattern p = Pattern.compile("(?:(file|ftp|sftp):(?://)?)?(.*)");
-                    Matcher m = p.matcher(sourcePath);
-                    if (m.matches()) {
-                        String type = m.group(1) == null ? "file" : m.group(1).toLowerCase();
-
-                        ReadUtils.ReadResult readResult = ReadUtils.readFile(sourcePath, false, false, false);
-                        if (readResult.errorCode == 0) {
-                            File sourceFile = new File(readResult.filePath);
-                            try {
-                                if (destinationPath.startsWith("ftp://")) {
-                                    WriteActionProperty.storeFileToFTP(destinationPath, sourceFile);
-                                } else if (destinationPath.startsWith("sftp://")) {
-                                    WriteActionProperty.storeFileToSFTP(destinationPath, sourceFile);
-                                } else {
-                                    FileCopyUtils.copy(sourceFile, new File(destinationPath.replace("file://", "")));
-                                }
-                            } finally {
-                                ReadUtils.deleteFile(type, sourcePath);
-                            }
-
-                        } else if (readResult.error != null) {
-                            throw Throwables.propagate(new RuntimeException(readResult.error));
-                        }
-                    } else {
-                        throw Throwables.propagate(new RuntimeException("Unsupported move source: " + sourcePath + ". Please use format: file://path or ftp|sftp://username:password;charset@host:port/path"));
-                    }
-                }
+                FileUtils.moveFile(context, sourcePath, destinationPath, isClient);
             }
         } catch (Exception e) {
             throw Throwables.propagate(e);
