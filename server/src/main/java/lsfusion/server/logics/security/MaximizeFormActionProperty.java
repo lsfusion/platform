34650ee954c3837d2c8cad3d4f008236527b154f
Vandrouny <filipchyk.andrei@gmail.com>
2018-05-18 12:06:10 +0000

    [server, client, api] Maximize не требует formCanonicalName

--- server/src/main/java/lsfusion/server/logics/security/MaximizeFormActionProperty.java
+++ server/src/main/java/lsfusion/server/logics/security/MaximizeFormActionProperty.java
@@ -1,7 +1,6 @@
 package lsfusion.server.logics.security;
 
 import lsfusion.interop.action.MaximizeFormClientAction;
-import lsfusion.server.classes.ValueClass;
 import lsfusion.server.data.SQLHandledException;
 import lsfusion.server.logics.SecurityLogicsModule;
 import lsfusion.server.logics.property.ClassPropertyInterface;
@@ -9,21 +8,15 @@ import lsfusion.server.logics.property.ExecutionContext;
 import lsfusion.server.logics.scripted.ScriptingActionProperty;
 
 import java.sql.SQLException;
-import java.util.Iterator;
 
 public class MaximizeFormActionProperty extends ScriptingActionProperty {
-    private final ClassPropertyInterface formCanonicalNameInterface;
 
-    public MaximizeFormActionProperty(SecurityLogicsModule LM, ValueClass... valueClasses) {
-        super(LM, valueClasses);
-
-        Iterator<ClassPropertyInterface> i = interfaces.iterator();
-        formCanonicalNameInterface = i.next();
+    public MaximizeFormActionProperty(SecurityLogicsModule LM) {
+        super(LM);
     }
 
     @Override
     protected void executeCustom(ExecutionContext<ClassPropertyInterface> context) throws SQLException, SQLHandledException {
-        String formCanonicalName = (String) context.getKeyValue(formCanonicalNameInterface).getValue();
-        context.delayUserInteraction(new MaximizeFormClientAction(formCanonicalName));
+        context.delayUserInteraction(new MaximizeFormClientAction());
     }
 }
