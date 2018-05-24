a63bfb79dae22993be313ffdc77281e86283df4c
AlexKirkouski <alex.kirkouski@gmail.com>
2018-05-24 06:49:13 +0000

    Migration navigatorForm -> navigatorAction

--- server/src/main/java/lsfusion/server/form/navigator/NavigatorAction.java
+++ server/src/main/java/lsfusion/server/form/navigator/NavigatorAction.java
@@ -1,16 +1,19 @@
 package lsfusion.server.form.navigator;
 
+import lsfusion.server.form.entity.FormEntity;
 import lsfusion.server.logics.i18n.LocalizedString;
 import lsfusion.server.logics.property.ActionProperty;
 
 public class NavigatorAction extends NavigatorElement {
     private final ActionProperty action;
+    private final FormEntity form;
 
-    public NavigatorAction(ActionProperty action, String canonicalName, LocalizedString caption) {
+    public NavigatorAction(ActionProperty action, String canonicalName, LocalizedString caption, FormEntity form, String icon, DefaultIcon defaultIcon) {
         super(canonicalName, caption);
         
         this.action = action;
-        setImage("/images/action.png", DefaultIcon.ACTION);
+        this.form = form;
+        setImage(icon, defaultIcon);
     }
 
     @Override
@@ -21,6 +24,10 @@ public class NavigatorAction extends NavigatorElement {
     @Override
     public byte getTypeID() {
         return 2;
+    }
+
+    public FormEntity getForm() {
+        return form;
     }
 
     public ActionProperty getAction() {
