17c69d5692d64e4d6ed62dc6f0723cdb5043a555
AFilipchyk <filipchyk.andrei@gmail.com>
2018-06-27 10:05:24 +0300

    [fsl] fsl open link

---
+++ fsl-server/src/main/java/lsfusion/erp/utils/utils/OpenRawLinkActionProperty.java
@@ -0,0 +1,40 @@
+package lsfusion.erp.utils.utils;
+
+import com.google.common.base.Throwables;
+import lsfusion.interop.action.OpenUriClientAction;
+import lsfusion.server.classes.ValueClass;
+import lsfusion.server.classes.link.LinkClass;
+import lsfusion.server.data.SQLHandledException;
+import lsfusion.server.logics.DataObject;
+import lsfusion.server.logics.ObjectValue;
+import lsfusion.server.logics.property.ClassPropertyInterface;
+import lsfusion.server.logics.property.ExecutionContext;
+import lsfusion.server.logics.scripted.ScriptingActionProperty;
+import lsfusion.server.logics.scripted.ScriptingLogicsModule;
+import org.apache.commons.httpclient.util.URIUtil;
+
+import java.net.URI;
+import java.sql.SQLException;
+import java.util.Iterator;
+
+public class OpenRawLinkActionProperty extends ScriptingActionProperty {
+    private final ClassPropertyInterface sourceInterface;
+
+    public OpenRawLinkActionProperty(ScriptingLogicsModule LM, ValueClass... classes) {
+        super(LM, classes);
+
+        Iterator<ClassPropertyInterface> i = interfaces.iterator();
+        sourceInterface = i.next();
+    }
+
+    public void executeCustom(ExecutionContext<ClassPropertyInterface> context) throws SQLException, SQLHandledException {
+        try {
+            ObjectValue sourceObject = context.getDataKeyValue(sourceInterface);
+            for (URI file : ((LinkClass) ((DataObject) sourceObject).getType()).getFiles(sourceObject.getValue())) {
+                context.delayUserInteraction(new OpenUriClientAction(new URI(URIUtil.decode(file.toString()))));
+            }
+        } catch (Exception e) {
+            throw Throwables.propagate(e);
+        }
+    }
+}
