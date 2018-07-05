abe36630f2be57d9f211655948fe78d839f619b0
AlexKirkouski <alex.kirkouski@gmail.com>
2018-07-05 09:49:44 +0300

    Improved concurrency when working with Strategy

--- api/src/main/java/lsfusion/base/col/lru/LRUUtil.java
+++ api/src/main/java/lsfusion/base/col/lru/LRUUtil.java
@@ -187,8 +187,8 @@ public class LRUUtil {
     public static final int DEFAULT_CONCURRENCY_LEVEL = SystemUtils.getAvailableProcessors() * 8;
 
     public static class Strategy {
-        public int baseTime;
-        public int maxTime;
+        public final int baseTime;
+        public final int maxTime;
 
         public Strategy(int baseTime, int maxTime) {
             this.baseTime = baseTime;
