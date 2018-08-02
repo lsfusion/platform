002647e488265889d014d608db3aa35ea6524be9
pavel-miniutka <asphalt.by@gmail.com>
2018-08-02 14:58:20 +0300

    renamed system navigator elements

--- server/src/main/java/lsfusion/server/logics/BaseLogicsModule.java
+++ server/src/main/java/lsfusion/server/logics/BaseLogicsModule.java
@@ -500,9 +500,9 @@ public class BaseLogicsModule<T extends BusinessLogics<T>> extends ScriptingLogi
 
         application = findNavigatorElement("application");
 
-        system = findNavigatorElement("configuration");
-
-        logs = findNavigatorElement("systemEvents");
+        system = findNavigatorElement("system");
+                                                                            
+        logs = findNavigatorElement("logs");
     }
 
 
