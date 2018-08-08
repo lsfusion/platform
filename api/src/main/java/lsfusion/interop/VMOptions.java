d71ce01186be371fd89a361b248c53457b5cc05f
AFilipchyk <filipchyk.andrei@gmail.com>
2018-08-08 14:46:47 +0300

    minHeapFreeRatio

--- api/src/main/java/lsfusion/interop/VMOptions.java
+++ api/src/main/java/lsfusion/interop/VMOptions.java
@@ -5,12 +5,14 @@ import java.io.Serializable;
 public class VMOptions implements Serializable {
     private String initHeapSize;
     private String maxHeapSize;
+    private String minHeapFreeRatio;
     private String maxHeapFreeRatio;
     private String vmargs;
 
-    public VMOptions(String initHeapSize, String maxHeapSize, String maxHeapFreeRatio, String vmargs) {
+    public VMOptions(String initHeapSize, String maxHeapSize, String minHeapFreeRatio, String maxHeapFreeRatio, String vmargs) {
         this.initHeapSize = initHeapSize;
         this.maxHeapSize = maxHeapSize;
+        this.minHeapFreeRatio = minHeapFreeRatio;
         this.maxHeapFreeRatio = maxHeapFreeRatio;
         this.vmargs = vmargs;
     }
@@ -29,6 +31,14 @@ public class VMOptions implements Serializable {
 
     public void setMaxHeapSize(String maxHeapSize) {
         this.maxHeapSize = maxHeapSize;
+    }
+
+    public String getMinHeapFreeRatio() {
+        return minHeapFreeRatio;
+    }
+
+    public void setMinHeapFreeRatio(String minHeapFreeRatio) {
+        this.minHeapFreeRatio = minHeapFreeRatio;
     }
 
     public String getMaxHeapFreeRatio() {
