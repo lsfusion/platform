34250e2580dc05ed2f255b45dcd91b11de70ce62
AFilipchyk <filipchyk.andrei@gmail.com>
2018-09-05 15:41:35 +0300

    [client] if check api failed, close navigator before shutdown

--- desktop-client/src/main/java/lsfusion/client/Main.java
+++ desktop-client/src/main/java/lsfusion/client/Main.java
@@ -202,6 +202,7 @@ public class Main {
                     }
 
                     remoteLogics = loginAction.getRemoteLogics();
+                    remoteNavigator = loginAction.getRemoteNavigator();
 
                     String serverVersion = null;
                     String clientVersion = null;
@@ -220,6 +221,7 @@ public class Main {
                     }
                     if(serverVersion != null) {
                         JOptionPane.showMessageDialog(Main.frame, getString("client.error.need.restart", serverVersion, clientVersion), "LSFusion", JOptionPane.WARNING_MESSAGE);
+                        remoteNavigator.close();
                         Main.shutdown();
                         return;
                     }
@@ -230,8 +232,6 @@ public class Main {
                     logicsMainIcon = prefs.logicsMainIcon;
                     logicsLogo = prefs.logicsLogo;
                     hideMenu = prefs.hideMenu;
-
-                    remoteNavigator = loginAction.getRemoteNavigator();
 
                     LocalePreferences userPreferences = remoteNavigator.getLocalePreferences();
 
@@ -289,7 +289,7 @@ public class Main {
                     }
 
                     busyDialog = remoteNavigator.isBusyDialog();
-                    
+
                     useRequestTimeout = remoteNavigator.isUseRequestTimeout();
 
                     ((DockableMainFrame) frame).executeNavigatorAction("SystemEvents.onClientStarted[]", 0, null, null);
