3f4cff7e56830230a670c97f724d403b4e3184b7
AFilipchyk <filipchyk.andrei@gmail.com>
2018-10-10 14:07:57 +0300

    useStreamingReader option for import huge xlsx files

--- server/src/main/java/lsfusion/server/logics/property/actions/integration/importing/plain/xls/ImportXLSIterator.java
+++ server/src/main/java/lsfusion/server/logics/property/actions/integration/importing/plain/xls/ImportXLSIterator.java
@@ -1,12 +1,15 @@
 package lsfusion.server.logics.property.actions.integration.importing.plain.xls;
 
+import com.monitorjbl.xlsx.StreamingReader;
 import lsfusion.base.col.ListFact;
 import lsfusion.base.col.interfaces.immutable.ImMap;
 import lsfusion.base.col.interfaces.immutable.ImOrderMap;
 import lsfusion.base.col.interfaces.immutable.ImOrderSet;
 import lsfusion.base.col.interfaces.mutable.mapvalue.GetValue;
+import lsfusion.server.Settings;
 import lsfusion.server.data.type.Type;
 import lsfusion.server.logics.property.actions.integration.importing.plain.ImportPlainIterator;
+import org.apache.commons.io.FileUtils;
 import org.apache.poi.hssf.usermodel.HSSFFormulaEvaluator;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.*;
@@ -14,12 +17,17 @@ import org.apache.poi.xssf.usermodel.XSSFFormulaEvaluator;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
-import java.text.ParseException;
+import java.util.Iterator;
+import java.util.Map;
 
 public class ImportXLSIterator extends ImportPlainIterator {
 
-    private final Workbook wb; 
+    private final boolean useStreamingReader;
+
+    private final Workbook wb;
+    private File wbFile;
     private FormulaEvaluator formulaEvaluator;
     private int rowsCount;
     private Integer lastSheet;
@@ -27,15 +35,27 @@ public class ImportXLSIterator extends ImportPlainIterator {
     public ImportXLSIterator(ImOrderMap<String, Type> fieldTypes, byte[] file, boolean xlsx, Integer singleSheetIndex) throws IOException {
         super(fieldTypes);
 
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(file);
-        if(xlsx) {
-            wb = new XSSFWorkbook(inputStream);
-            formulaEvaluator = new XSSFFormulaEvaluator((XSSFWorkbook) wb);
+        int minSize = Settings.get().getMinSizeForExcelStreamingReader();
+        useStreamingReader = xlsx && minSize >= 0 && file.length >= minSize;
+        if(useStreamingReader) {
+            wbFile = File.createTempFile("import", "xlsx");
+            FileUtils.writeByteArrayToFile(wbFile, file);
+            wb = StreamingReader.builder().rowCacheSize(100)    // number of rows to keep in memory (defaults to 10)
+                    .bufferSize(4096)     // buffer size to use when reading InputStream to file (defaults to 1024)
+                    .open(wbFile);            // InputStream or File for XLSX file (required)
+            formulaEvaluator = new XLSXHugeFormulaEvaluator();
         } else {
-            wb = new HSSFWorkbook(inputStream);
-            formulaEvaluator = new HSSFFormulaEvaluator((HSSFWorkbook) wb);
+            ByteArrayInputStream inputStream = new ByteArrayInputStream(file);
+            if (xlsx) {
+                wb = new XSSFWorkbook(inputStream);
+                formulaEvaluator = new XSSFFormulaEvaluator((XSSFWorkbook) wb);
+            } else {
+                wb = new HSSFWorkbook(inputStream);
+                formulaEvaluator = new HSSFFormulaEvaluator((HSSFWorkbook) wb);
+            }
         }
-        if(singleSheetIndex != null) {
+
+        if (singleSheetIndex != null) {
             currentSheet = singleSheetIndex;
             lastSheet = singleSheetIndex;
         } else {
@@ -57,33 +77,58 @@ public class ImportXLSIterator extends ImportPlainIterator {
 
     private int currentSheet = 0;
     private Sheet sheet = null;
+    private Iterator<Row> rowIterator = null;
+    private boolean firstRow;
+
     private boolean nextSheet() {
-        if(currentSheet > lastSheet)
+        if (currentSheet > lastSheet) {
             return false;
-        
-        sheet = wb.getSheetAt(currentSheet++);
-        rowsCount = sheet.getLastRowNum() + 1;
+        }
+        if (useStreamingReader) {
+            rowIterator = wb.getSheetAt(currentSheet++).iterator();
+            firstRow = true;
+        } else {
+            sheet = wb.getSheetAt(currentSheet++);
+            rowsCount = sheet.getLastRowNum() + 1;
+        }
         return true;
     }
     
     private int currentRow = 0;
     private Row row;
     @Override
-    protected boolean nextRow() throws IOException {
-        if (sheet == null || currentRow >= rowsCount) {
-            if(!nextSheet())
-                return false;
-            currentRow = 0;
+    protected boolean nextRow() {
+        if(useStreamingReader) {
+            if (rowIterator != null && rowIterator.hasNext()) {
+                row = rowIterator.next();
+                if (row.getRowNum() == 0 && !firstRow) { //skip all rows with rowNum 0 (except first) - they are incorrect
+                    return nextRow();
+                } else {
+                    firstRow = false;
+                }
+            } else {
+                if (nextSheet()) {
+                    return nextRow();
+                } else {
+                    return false;
+                }
+            }
+        } else {
+            if (sheet == null || currentRow >= rowsCount) {
+                if (!nextSheet())
+                    return false;
+                currentRow = 0;
+            }
+
+            row = sheet.getRow(currentRow++);
+            if (row == null)
+                return nextRow();
         }
-        
-        row = sheet.getRow(currentRow++);
-        if(row == null)
-            return nextRow();
         return true;
     }
 
     @Override
-    protected Object getPropValue(String name, Type type) throws lsfusion.server.data.type.ParseException, ParseException {
+    protected Object getPropValue(String name, Type type) {
         Cell cell = row.getCell(fieldIndexes.get(name));
         if(cell == null)
             return null;
@@ -101,6 +146,11 @@ public class ImportXLSIterator extends ImportPlainIterator {
     public void release() throws IOException {
         if(wb != null)
             wb.close();
+        if(useStreamingReader) {
+            if (wbFile != null && !wbFile.delete()) {
+                wbFile.deleteOnExit();
+            }
+        }
     }
 
     public final static ImOrderMap<String, Integer> nameToIndexColumnsMapping = ListFact.consecutiveList(256, 0).mapOrderKeys(new GetValue<String, Integer>() {
@@ -125,5 +175,83 @@ public class ImportXLSIterator extends ImportPlainIterator {
             index /= LETTERS_CNT;
         }
         return columnName;
+    }
+
+    private class XLSXHugeFormulaEvaluator implements FormulaEvaluator {
+        @Override
+        public void clearAllCachedResultValues() {
+        }
+
+        @Override
+        public void notifySetFormula(Cell cell) {
+        }
+
+        @Override
+        public void notifyDeleteCell(Cell cell) {
+        }
+
+        @Override
+        public void notifyUpdateCell(Cell cell) {
+        }
+
+        @Override
+        public void evaluateAll() {
+        }
+
+        //from BaseFormulaEvaluator
+        @Override
+        public CellValue evaluate(Cell cell) {
+            if (cell == null) {
+                return null;
+            }
+            switch (cell.getCellTypeEnum()) {
+                case BOOLEAN:
+                    return CellValue.valueOf(cell.getBooleanCellValue());
+                case ERROR:
+                    return CellValue.getError(cell.getErrorCellValue());
+                case FORMULA:
+                    switch (cell.getCachedFormulaResultTypeEnum()) {
+                        case NUMERIC:
+                            return new CellValue(cell.getNumericCellValue());
+                        default:
+                            return new CellValue(cell.getRichStringCellValue().getString());
+                    }
+                case NUMERIC:
+                    return new CellValue(cell.getNumericCellValue());
+                case STRING:
+                    return new CellValue(cell.getRichStringCellValue().getString());
+                case BLANK:
+                    return null;
+                default:
+                    throw new IllegalStateException("Bad cell type (" + cell.getCellTypeEnum() + ")");
+            }
+        }
+
+        @Override
+        public int evaluateFormulaCell(Cell cell) {
+            return 0;
+        }
+
+        @Override
+        public CellType evaluateFormulaCellEnum(Cell cell) {
+            return null;
+        }
+
+        @Override
+        public Cell evaluateInCell(Cell cell) {
+            return null;
+        }
+
+        @Override
+        public void setupReferencedWorkbooks(Map<String, FormulaEvaluator> workbooks) {
+        }
+
+        @Override
+        public void setIgnoreMissingWorkbooks(boolean ignore) {
+        }
+
+        @Override
+        public void setDebugEvaluationOutputForNextEval(boolean value) {
+        }
     }
 }
