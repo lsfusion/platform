54394ad8bf63977245870c5e4796ea1d529a45de
AFilipchyk <filipchyk.andrei@gmail.com>
2018-10-22 09:17:03 +0300

    Revert "THROW action"
    This reverts commit 1578ed5

--- server/src/main/java/lsfusion/server/logics/property/actions/flow/LSFException.java
+++
@@ -1,8 +0,0 @@
-package lsfusion.server.logics.property.actions.flow;
-
-public class LSFException extends RuntimeException {
-
-    public LSFException(String message) {
-        super(message);
-    }
-}
