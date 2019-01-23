b154334bb000e8f22848bd41c17dec5b59838b69
AFilipchyk <filipchyk.andrei@gmail.com>
2019-01-23 10:52:39 +0300

    [server] SaveFileDialogClientAction removed

--- server/src/main/java/lsfusion/server/logics/property/actions/file/WriteClientAction.java
+++ server/src/main/java/lsfusion/server/logics/property/actions/file/WriteClientAction.java
@@ -1,6 +1,7 @@
 package lsfusion.server.logics.property.actions.file;
 
 import com.google.common.base.Throwables;
+import lsfusion.base.FileDialogUtils;
 import lsfusion.base.RawFileData;
 import lsfusion.interop.action.ClientActionDispatcher;
 import lsfusion.interop.action.ExecuteClientAction;
@@ -12,18 +13,24 @@ public class WriteClientAction extends ExecuteClientAction {
     String path;
     String extension;
     boolean append;
+    boolean isDialog;
 
-    public WriteClientAction(RawFileData file, String path, String extension, boolean append) {
+    public WriteClientAction(RawFileData file, String path, String extension, boolean append, boolean isDialog) {
         this.file = file;
         this.path = path;
         this.extension = extension;
         this.append = append;
+        this.isDialog = isDialog;
     }
 
     @Override
     public void execute(ClientActionDispatcher dispatcher) throws IOException {
         try {
-            WriteUtils.write(file, path, extension, append);
+            if(isDialog) {
+                FileDialogUtils.showSaveFileDialog(WriteUtils.appendExtension(path, extension), file);
+            } else {
+                WriteUtils.write(file, path, extension, append);
+            }
         } catch (Exception e) {
             throw Throwables.propagate(e);
         }
