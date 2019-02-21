88d50e6b8eaa643368a465917edb744fed98e0cd
AlexKirkouski <alex.kirkouski@gmail.com>
2019-02-21 16:30:37 +0300

    Made Utils system module

--- server/src/main/java/lsfusion/utils/utils/GeneratePasswordActionProperty.java
+++ server/src/main/java/lsfusion/utils/utils/GeneratePasswordActionProperty.java
@@ -3,6 +3,7 @@ package lsfusion.utils.utils;
 import com.google.common.base.Throwables;
 import lsfusion.server.classes.ValueClass;
 import lsfusion.server.data.SQLHandledException;
+import lsfusion.server.logics.UtilsLogicsModule;
 import lsfusion.server.logics.property.ClassPropertyInterface;
 import lsfusion.server.logics.property.ExecutionContext;
 import lsfusion.server.logics.scripted.ScriptingActionProperty;
@@ -20,7 +21,7 @@ public class GeneratePasswordActionProperty extends ScriptingActionProperty {
 
     private static final String CHARACTERS = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 
-    public GeneratePasswordActionProperty(ScriptingLogicsModule LM, ValueClass... classes) {
+    public GeneratePasswordActionProperty(UtilsLogicsModule LM, ValueClass... classes) {
         super(LM, classes);
 
         Iterator<ClassPropertyInterface> i = interfaces.iterator();
