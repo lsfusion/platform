50eb4674c8c2a8a665b4f3853067b15d01aca1cf
AFilipchyk <filipchyk.andrei@gmail.com>
2019-03-20 14:31:52 +0300

    [client, web] HTMLLINK renderer

--- web-client/src/main/java/lsfusion/gwt/shared/view/classes/link/GHTMLLinkType.java
+++ web-client/src/main/java/lsfusion/gwt/shared/view/classes/link/GHTMLLinkType.java
@@ -1,10 +1,18 @@
 package lsfusion.gwt.shared.view.classes.link;
 
 import lsfusion.gwt.client.ClientMessages;
+import lsfusion.gwt.client.form.ui.grid.renderer.GridCellRenderer;
+import lsfusion.gwt.client.form.ui.grid.renderer.HTMLGridCellRenderer;
+import lsfusion.gwt.shared.view.GPropertyDraw;
 
 public class GHTMLLinkType extends GLinkType {
     @Override
     public String toString() {
         return ClientMessages.Instance.get().typeHTMLFileLinkCaption();
+    }
+
+    @Override
+    public GridCellRenderer createGridCellRenderer(GPropertyDraw property) {
+        return new HTMLGridCellRenderer();
     }
 }
