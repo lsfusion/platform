fb65dbe2f33b3f68e690ad4da76bdd881279be62
AlexKirkouski <alex.kirkouski@gmail.com>
2019-06-14 16:10:36 +0300

    Moved installer to jenkins repository

--- server/src/installer/Installer-x64-dev.nsi
+++
@@ -1,4 +0,0 @@
-!define DEV 1
-!define OUT_FILE "x64-dev.exe"
-
-!include "Installer-x64.nsi"
--- server/src/installer/install-config/configure.bat
+++
@@ -1,2 +0,0 @@
-set JAVA_HOME=JAVA
-call "..\install-bin\apache-ant-1.9.2\bin\ant" -verbose -f configure.xml %* >>configure.log 2>&1
