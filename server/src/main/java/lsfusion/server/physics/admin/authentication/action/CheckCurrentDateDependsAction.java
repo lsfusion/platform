6a8c562c6ac82369ae638ebdf94b7f7cb2ba5692
AFilipchyk <filipchyk.andrei@gmail.com>
2019-06-20 11:43:00 +0300

    i18n

--- server/src/main/java/lsfusion/server/physics/admin/authentication/action/CheckCurrentDateDependsAction.java
+++ server/src/main/java/lsfusion/server/physics/admin/authentication/action/CheckCurrentDateDependsAction.java
@@ -3,19 +3,20 @@ package lsfusion.server.physics.admin.authentication.action;
 import lsfusion.interop.action.ConfirmClientAction;
 import lsfusion.server.data.sql.exception.SQLHandledException;
 import lsfusion.server.data.value.DataObject;
-import lsfusion.server.language.property.LP;
 import lsfusion.server.logics.BusinessLogics;
 import lsfusion.server.logics.action.controller.context.ExecutionContext;
 import lsfusion.server.logics.classes.ValueClass;
 import lsfusion.server.logics.property.Property;
 import lsfusion.server.logics.property.classes.ClassPropertyInterface;
-import lsfusion.server.logics.property.oraction.ActionOrProperty;
 import lsfusion.server.physics.admin.authentication.security.SecurityLogicsModule;
+import lsfusion.server.physics.dev.i18n.LocalizedString;
 import lsfusion.server.physics.dev.integration.internal.to.InternalAction;
 
 import javax.swing.*;
 import java.sql.SQLException;
 import java.util.Iterator;
+
+import static lsfusion.server.base.controller.thread.ThreadLocalContext.localize;
 
 public class CheckCurrentDateDependsAction extends InternalAction {
 
@@ -38,8 +39,8 @@ public class CheckCurrentDateDependsAction extends InternalAction {
         Property<?> property = BL.findProperty((String) BL.reflectionLM.canonicalNameProperty.read(context, propertyObject)).property;
         if (Property.depends(property, BL.timeLM.currentDate.property)) {
             allow = JOptionPane.YES_OPTION == (Integer) context.requestUserInteraction(
-                    new ConfirmClientAction("Свойство зависит от текущей даты",
-                            String.format("Свойство %s зависит от текущей даты. Вы уверены, что хотите его залогировать?", property.toString())));
+                    new ConfirmClientAction(localize("{security.property.depends.on.current.date}"),
+                            localize(LocalizedString.createFormatted("{security.property.depends.on.current.date.are.you.sure}", property.toString()))));
         }
         if (!allow) {
             try(ExecutionContext.NewSession<ClassPropertyInterface> newContext = context.newSession()) {
