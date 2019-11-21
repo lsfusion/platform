0186de3aea30396fa95338dc1e949738170bef01
AlexKirkouski <alex.kirkouski@gmail.com>
2019-11-21 18:10:59 +0300

    NPE when changing filter makes tree empty (closes #199)

--- web-client/src/main/java/lsfusion/gwt/client/form/object/table/tree/view/GTreeTable.java
+++ web-client/src/main/java/lsfusion/gwt/client/form/object/table/tree/view/GTreeTable.java
@@ -439,7 +439,8 @@ public class GTreeTable extends GGridPropertyTable<GTreeGridRecord> {
     }
 
     public boolean isCurrentPathExpanded() {
-        return selectedRecord != null && tree.getNodeByRecord(selectedRecord).isOpen();
+        GTreeTableNode node;
+        return selectedRecord != null && (node = tree.getNodeByRecord(selectedRecord)) != null && node.isOpen();
     }
 
     private void fireCollapseNode(GTreeTableNode node) {
@@ -592,7 +593,7 @@ public class GTreeTable extends GGridPropertyTable<GTreeGridRecord> {
         if (event.getTypeInt() == Event.ONDBLCLICK) {
             if (treeGroupController.isExpandOnClick() && !isEditable(getCurrentCellContext()) && getTableBodyElement().isOrHasChild(Node.as(event.getEventTarget()))) {
                 GTreeTableNode node = tree.getNodeByRecord(getSelectedRecord());
-                if (node.isExpandable()) {
+                if (node != null && node.isExpandable()) {
                     GwtClientUtils.stopPropagation(event);
                     if (!node.isOpen()) {
                         fireExpandNode(node);
