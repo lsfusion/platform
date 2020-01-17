10fff7329ba2404600263436919b6a59d353c1d8
AFilipchyk <filipchyk.andrei@gmail.com>
2020-01-17 10:25:59 +0300

    [server] PostgreDataAdapter: extra log in ensureDB

--- server/src/main/java/lsfusion/server/data/sql/adapter/PostgreDataAdapter.java
+++ server/src/main/java/lsfusion/server/data/sql/adapter/PostgreDataAdapter.java
@@ -73,6 +73,7 @@ public class PostgreDataAdapter extends DataAdapter {
             try {
                 connect = DriverManager.getConnection("jdbc:postgresql://" + server + "/postgres?user=" + userID + "&password=" + password);
             } catch (PSQLException e) {
+                ServerLoggers.startLogger.error(e.getMessage());
                 logger.error("EnsureDB error: ", e);
                 //08001 = connection refused (database is not started), 57P03 = the database system is starting up
                 if (e.getSQLState() != null && (e.getSQLState().equals("08001") || e.getSQLState().equals("57P03"))) {
