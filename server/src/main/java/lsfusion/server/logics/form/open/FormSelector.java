d557aa1e4241436fe5d218902e1be2b9b03949dd
AlexKirkouski <alex.kirkouski@gmail.com>
2020-02-20 16:38:56 +0300

    Bugfix with NPE

--- server/src/main/java/lsfusion/server/logics/form/open/FormSelector.java
+++ server/src/main/java/lsfusion/server/logics/form/open/FormSelector.java
@@ -2,6 +2,7 @@ package lsfusion.server.logics.form.open;
 
 import com.google.common.base.Throwables;
 import lsfusion.base.Pair;
+import lsfusion.base.col.MapFact;
 import lsfusion.base.col.interfaces.immutable.ImMap;
 import lsfusion.base.col.interfaces.immutable.ImRevMap;
 import lsfusion.server.data.sql.exception.SQLHandledException;
@@ -11,7 +12,6 @@ import lsfusion.server.logics.action.session.DataSession;
 import lsfusion.server.logics.classes.ValueClass;
 import lsfusion.server.logics.form.struct.FormEntity;
 import lsfusion.server.logics.form.struct.object.ObjectEntity;
-import lsfusion.server.logics.property.Property;
 
 import java.sql.SQLException;
 
@@ -23,7 +23,7 @@ public interface FormSelector<O extends ObjectSelector> {
     FormEntity getNFStaticForm();
     default FormEntity getStaticForm(BaseLogicsModule LM) {
         try {
-            return getForm(LM, null, null).first; // always not null since session is null
+            return getForm(LM, null, MapFact.EMPTY()).first; // always not null since session is null
         } catch (SQLException | SQLHandledException e) { // can't be since session is null
             throw Throwables.propagate(e);
         }
