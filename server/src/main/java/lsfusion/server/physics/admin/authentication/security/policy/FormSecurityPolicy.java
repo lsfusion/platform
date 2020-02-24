bad4808b69fcd022f78290a091b2f4a1f4575f10
AFilipchyk <filipchyk.andrei@gmail.com>
2020-02-24 10:01:01 +0300

    Удалены ClassSecurityPolicy и FormSecurityPolicy

--- server/src/main/java/lsfusion/server/physics/admin/authentication/security/policy/FormSecurityPolicy.java
+++
@@ -1,7 +0,0 @@
-package lsfusion.server.physics.admin.authentication.security.policy;
-
-import lsfusion.server.logics.form.struct.FormEntity;
-
-public class FormSecurityPolicy extends AbstractSecurityPolicy<FormEntity> {
-    
-}
