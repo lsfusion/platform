1ef987da85064af84bbce777d16bcff45f884462
pavel-miniutka <asphalt.by@gmail.com>
2020-03-27 10:19:53 +0300

    apiVersion up

--- api/src/main/java/lsfusion/base/BaseUtils.java
+++ api/src/main/java/lsfusion/base/BaseUtils.java
@@ -54,7 +54,7 @@ public class BaseUtils {
     private static final int STRING_SERIALIZATION_CHUNK_SIZE = 65535/3;
 
     public static Integer getApiVersion() {
-        return 107;
+        return 108;
     }
     
     public static String getPlatformVersion() {
