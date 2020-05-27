98e2f737717e79447c0af7b3a522da16433e6731
AFilipchyk <filipchyk.andrei@gmail.com>
2020-05-27 09:13:25 +0300

    PrintFile: trayName, duplex options

--- server/src/main/java/lsfusion/server/physics/dev/integration/external/to/equ/printer/PrintFileAction.java
+++ server/src/main/java/lsfusion/server/physics/dev/integration/external/to/equ/printer/PrintFileAction.java
@@ -2,7 +2,6 @@ package lsfusion.server.physics.dev.integration.external.to.equ.printer;
 
 import com.google.common.base.Throwables;
 import lsfusion.base.file.RawFileData;
-import lsfusion.server.data.sql.exception.SQLHandledException;
 import lsfusion.server.language.ScriptingLogicsModule;
 import lsfusion.server.logics.action.controller.context.ExecutionContext;
 import lsfusion.server.logics.classes.ValueClass;
@@ -10,19 +9,22 @@ import lsfusion.server.logics.property.classes.ClassPropertyInterface;
 import lsfusion.server.physics.dev.integration.external.to.equ.printer.client.PrintFileClientAction;
 import lsfusion.server.physics.dev.integration.internal.to.InternalAction;
 
-import java.sql.SQLException;
 import java.util.Iterator;
 
 public class PrintFileAction extends InternalAction {
     private final ClassPropertyInterface fileInterface; //PDFFILE or STRING
     private final ClassPropertyInterface printerNameInterface;
+    private final ClassPropertyInterface trayNameInterface;
+    private final ClassPropertyInterface duplexInterface;
 
     public PrintFileAction(ScriptingLogicsModule LM, ValueClass... classes) {
         super(LM, classes);
 
-        Iterator<ClassPropertyInterface> i = interfaces.iterator();
+        Iterator<ClassPropertyInterface> i = getOrderInterfaces().iterator();
         fileInterface = i.next();
         printerNameInterface = i.next();
+        trayNameInterface = i.next();
+        duplexInterface = i.next();
     }
 
     public void executeInternal(ExecutionContext<ClassPropertyInterface> context) {
@@ -30,11 +32,13 @@ public class PrintFileAction extends InternalAction {
             Object file = context.getKeyValue(fileInterface).getValue();
             if(file != null) {
                 String printerName = (String) context.getKeyValue(printerNameInterface).getValue();
+                String trayName = (String) context.getKeyValue(trayNameInterface).getValue();
+                boolean duplex = context.getKeyValue(duplexInterface).getValue() != null;
                 if(file instanceof RawFileData) {
-                    context.delayUserInteraction(new PrintFileClientAction((RawFileData) file, null, printerName));
+                    context.delayUserInteraction(new PrintFileClientAction((RawFileData) file, null, printerName, trayName, duplex));
                 } else {
                     assert file instanceof String;
-                    context.delayUserInteraction(new PrintFileClientAction(null, (String) file, printerName));
+                    context.delayUserInteraction(new PrintFileClientAction(null, (String) file, printerName, trayName, duplex));
                 }
             }
         } catch (Exception e) {
