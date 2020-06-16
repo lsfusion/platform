8d53be963aa3c45c0c28bd3c91a970221d7bfa73
AFilipchyk <filipchyk.andrei@gmail.com>
2020-06-16 17:20:15 +0300

    stringToFile: write null to resultFile if inputValue == null

--- server/src/main/java/lsfusion/server/physics/dev/integration/external/to/file/StringToFileAction.java
+++ server/src/main/java/lsfusion/server/physics/dev/integration/external/to/file/StringToFileAction.java
@@ -3,14 +3,12 @@ package lsfusion.server.physics.dev.integration.external.to.file;
 import com.google.common.base.Throwables;
 import lsfusion.base.file.FileData;
 import lsfusion.base.file.RawFileData;
-import lsfusion.server.data.sql.exception.SQLHandledException;
 import lsfusion.server.logics.UtilsLogicsModule;
 import lsfusion.server.logics.action.controller.context.ExecutionContext;
 import lsfusion.server.logics.classes.ValueClass;
 import lsfusion.server.logics.property.classes.ClassPropertyInterface;
 import lsfusion.server.physics.dev.integration.internal.to.InternalAction;
 
-import java.sql.SQLException;
 import java.util.Iterator;
 
 public class StringToFileAction extends InternalAction {
@@ -34,9 +32,8 @@ public class StringToFileAction extends InternalAction {
         String extension = (String) context.getKeyValue(extensionInterface).getValue();
 
         try {
-            if (inputValue != null && extension != null) {
-                findProperty("resultFile[]").change(new FileData(new RawFileData(inputValue.getBytes(charset)), extension), context);
-            }
+            FileData fileData = inputValue != null && extension != null ? new FileData(new RawFileData(inputValue.getBytes(charset)), extension) : null;
+            findProperty("resultFile[]").change(fileData, context);
         } catch (Exception e) {
             throw Throwables.propagate(e);
         }
