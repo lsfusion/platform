c3fbdb742513449499119c4fc44955fb94b3985c
Andrei Filipchyk <40162373+AFilipchyk@users.noreply.github.com>
2020-06-16 14:42:22 +0300

    Numbers - i18n of Words (#401)
    Accepts BigDecimal, Double, Long, Integer
    Languages: russian, belarussian, ukrainian

---
+++ api/src/main/java/lsfusion/base/NumbersRu.java
@@ -0,0 +1,53 @@
+package lsfusion.base;
+
+public class NumbersRu {
+
+    private static String lang = "ru";
+
+    public static String toString(Object number) {
+        return Numbers.toString(lang, number);
+    }
+
+    public static String toString(Object number, Integer numOfDigits) {
+        return Numbers.toString(lang, number, numOfDigits);
+    }
+
+    public static String toString(Object number, boolean female) {
+        return Numbers.toString(lang, number, female);
+    }
+
+    //main method without type
+    public static String toString(Object number, Integer numOfDigits, boolean female, boolean upcase) {
+        return Numbers.toString(lang, number, numOfDigits, female, upcase);
+    }
+
+    public static String toString(Object number, String type) {
+        return Numbers.toString(lang, number, type);
+    }
+
+    public static String toString(Object number, String type, boolean upcase) {
+        return Numbers.toString(lang, number, type, upcase);
+    }
+
+    public static String toString(Object number, String type, boolean numericFraction, boolean upcase) {
+        return Numbers.toString(lang, number, type, numericFraction, upcase);
+    }
+
+    //main method with type
+    public static String toString(Object number, String type, boolean numericFraction, Integer forceNumOfDigits, boolean upcase) {
+        return Numbers.toString(lang, number, type, numericFraction, forceNumOfDigits, upcase);
+    }
+
+    public static String toStringCustom(Object numObject, String decPostfix, String fractPostfix) {
+        return Numbers.toStringCustom(lang, numObject, decPostfix, fractPostfix);
+    }
+
+    public static String toStringCustom(Object numObject, String decPostfix, String fractPostfix, boolean numericFraction, boolean upcase) {
+        return Numbers.toStringCustom(lang, numObject, decPostfix, fractPostfix, numericFraction, upcase);
+    }
+
+    //main method with custom postfixes
+    public static String toStringCustom(Object numObject, String decPostfix, String fractPostfix, int numOfFractDigits, Integer forceNumOfFractDigits, boolean numericFraction, boolean upcase) {
+        return Numbers.toStringCustom(lang, numObject, decPostfix, fractPostfix, numOfFractDigits, forceNumOfFractDigits, numericFraction, upcase);
+    }
+}
