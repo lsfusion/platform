7d6fd78215278ff34fd29d25f1523d41c833b835
AFilipchyk <filipchyk.andrei@gmail.com>
2020-08-04 16:06:33 +0300

    Timeout for EXTERNAL HTTP (with executor)

--- api/src/main/java/lsfusion/interop/session/ExternalHttpUtils.java
+++ api/src/main/java/lsfusion/interop/session/ExternalHttpUtils.java
@@ -1,5 +1,6 @@
 package lsfusion.interop.session;
 
+import lsfusion.base.BaseUtils;
 import lsfusion.base.file.IOUtils;
 import org.apache.http.Header;
 import org.apache.http.HttpEntity;
@@ -58,7 +59,7 @@ public class ExternalHttpUtils {
             requestBuilder.setDefaultRequestConfig(configBuilder.build());
         }
 
-        HttpResponse response = requestBuilder.build().execute(httpRequest);
+        HttpResponse response = (HttpResponse) BaseUtils.executeWithTimeout(() -> requestBuilder.build().execute(httpRequest), timeout);
         HttpEntity responseEntity = response.getEntity();
         ContentType responseContentType = ContentType.get(responseEntity);
         byte[] responseBytes = responseEntity != null ? IOUtils.readBytesFromStream(responseEntity.getContent()) : null;
