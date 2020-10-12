0147f47d4a260c4931b686cda72c43d9c778246e
pavel-miniutka <asphalt.by@gmail.com>
2020-10-12 15:46:23 +0300

    possible split container children count limited to 2

--- server/src/main/java/lsfusion/server/logics/form/struct/FormEntity.java
+++ server/src/main/java/lsfusion/server/logics/form/struct/FormEntity.java
@@ -25,6 +25,7 @@ import lsfusion.server.base.version.NFFact;
 import lsfusion.server.base.version.Version;
 import lsfusion.server.base.version.interfaces.*;
 import lsfusion.server.data.value.ObjectValue;
+import lsfusion.server.language.ScriptParsingException;
 import lsfusion.server.language.action.LA;
 import lsfusion.server.language.property.LP;
 import lsfusion.server.language.property.oraction.LAP;
@@ -1111,8 +1112,12 @@ public class FormEntity implements FormSelector<ObjectEntity> {
         
         for(RegularFilterGroupEntity regularFilterGroup : getRegularFilterGroupsIt())
             regularFilterGroup.finalizeAroundInit();
-        
-        getRichDesign().finalizeAroundInit();
+
+        try {
+            getRichDesign().finalizeAroundInit();
+        } catch (ScriptParsingException e) {
+            throw new ScriptParsingException("error finalizing form " + this + ":\n" + e.getMessage());
+        }
 
         proceedAllEventActions((action, drawAction) -> {
         }); // need this to generate default event actions (which will generate auto forms, and for example fill GroupObjectEntity.FILTER props, what is important to do before form is used)
