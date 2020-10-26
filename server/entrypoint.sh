dc42fce342a97d13965a7df54fbdadd790b0c06e
alex-byi <alexeybyimistrov@gmail.com>
2020-10-26 17:46:13 +0300

    some fixes

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,35 +1,35 @@
 #!/bin/bash -e
 
+command="java -cp .:./*:../server.jar"
+
 if ! [ -e conf/settings.properties ]; then
   mkdir conf
   cat > conf/settings.properties <<EOF
-db.server=localhost
-db.name=lsfusion
-db.user=postgres
-db.password=11111
+db.server=${DB_SERVER:-localhost}
+db.name=${DB_NAME:-lsfusion}
+db.user=${DB_USER:-postgres}
+db.password=${DB_PASSWORD:-postgres}
 EOF
-fi
-
-command="java -cp .:./*:../server.jar"
-
-if ! [ -z "$DB_USER" ]; then
+  else
+    if ! [ -z "$DB_USER" ]; then
     command+=" -Ddb.user=$DB_USER"
+    fi
+
+    if ! [ -z "$DB_NAME" ]; then
+        command+=" -Ddb.name=$DB_NAME"
+    fi
+
+    if ! [ -z "$DB_SERVER" ]; then
+        command+=" -Ddb.server=$DB_SERVER"
+    fi
+
+    if ! [ -z "$DB_PASSWORD" ]; then
+        command+=" -Ddb.password=$DB_PASSWORD"
+    fi
 fi
 
-if ! [ -z "$DB_NAME" ]; then
-    command+=" -Ddb.name=$DB_NAME"
-fi
-
-if ! [ -z "$DB_SERVER" ]; then
-    command+=" -Ddb.server=$DB_SERVER"
-fi
-
-if ! [ -z "$DB_PASSWORD" ]; then
-    command+=" -Ddb.password=$DB_PASSWORD"
-fi
-
-if ! [ -z "$JAVA_OPTIONS" ]; then
-    export JAVA_OPTS="$JAVA_OPTIONS"
+if ! [ -z "$JAVA_OPTS" ]; then
+    export JAVA_OPTS="$JAVA_OPTS"
 fi
 
 command+=" lsfusion.server.logics.BusinessLogicsBootstrap"
--- web-client/entrypoint.sh
+++ web-client/entrypoint.sh
@@ -1,16 +1,12 @@
 #!/bin/bash -e
 
 if ! [ -e conf/Catalina/localhost/ROOT.xml ]; then
-
-  SERVER_HOST=${SERVER_HOST:-localhost}
-  SERVER_PORT=${SERVER_PORT:-7652}
-
   mkdir -p conf/Catalina/localhost
   cat > conf/Catalina/localhost/ROOT.xml <<EOF
 <?xml version='1.0' encoding='utf-8'?>
 <Context>
-  <Parameter name="host" value="$SERVER_HOST" override="false"/>
-  <Parameter name="port" value="$SERVER_PORT" override="false"/>
+  <Parameter name="host" value="${SERVER_HOST:-localhost}" override="false"/>
+  <Parameter name="port" value="${SERVER_PORT:-7652}" override="false"/>
 </Context>
 EOF
   elif ! [ -z "$SERVER_HOST" ] && ! [ -z "$SERVER_PORT" ]; then
@@ -21,8 +17,8 @@ EOF
     </Context>" conf/server.xml
 fi
 
-if ! [ -z "$JAVA_OPTIONS" ]; then
-    export JAVA_OPTS="$JAVA_OPTIONS"
+if ! [ -z "$JAVA_OPTS" ]; then
+    export JAVA_OPTS="$JAVA_OPTS"
 fi
 
 exec bin/catalina.sh run
