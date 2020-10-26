5c3fb7780f86af79a00e406d0fee488a17b597a8
alex-byi <alexeybyimistrov@gmail.com>
2020-10-26 15:25:09 +0300

    parameters in environment or in config-files

--- server/entrypoint.sh
+++ server/entrypoint.sh
@@ -1,29 +1,33 @@
 #!/bin/bash -e
 
 if ! [ -e conf/settings.properties ]; then
-  echo >&2 "lsFusion server is not configured in $(pwd) - configuring..."
-
-  LSFUSION_DB_USER=${LSFUSION_DB_USER:-postgres}
-  LSFUSION_DB_NAME=${LSFUSION_DB_NAME:-lsfusion}
-
-  if [ -z "$LSFUSION_DB_PASSWORD" ]; then
-  	echo >&2 'error: missing required LSFUSION_DB_PASSWORD environment variable'
-  	exit 1
-  fi
-
-  if [ -z "$LSFUSION_DB_HOST" ]; then
-  	echo >&2 'error: missing required LSFUSION_DB_HOST environment variable'
-  	exit 1
-  fi
-
   mkdir -p conf
-
   cat > conf/settings.properties <<EOF
-db.server=$LSFUSION_DB_HOST
-db.name=$LSFUSION_DB_NAME
-db.user=$LSFUSION_DB_USER
-db.password=$LSFUSION_DB_PASSWORD
+db.server=localhost
+db.name=lsfusion
+db.user=postgres
+db.password=11111
 EOF
 fi
 
-exec java -cp ".:server.jar" lsfusion.server.logics.BusinessLogicsBootstrap
+command="java -cp .:./*:../server.jar"
+
+if ! [ -z "$DB_USER" ]; then
+    command+=" -Ddb.user=$DB_USER"
+fi
+
+if ! [ -z "$DB_NAME" ]; then
+    command+=" -Ddb.name=$DB_NAME"
+fi
+
+if ! [ -z "$DB_SERVER" ]; then
+    command+=" -Ddb.server=$DB_SERVER"
+fi
+
+if ! [ -z "$DB_PASSWORD" ]; then
+    command+=" -Ddb.password=$DB_PASSWORD"
+fi
+
+command+=" lsfusion.server.logics.BusinessLogicsBootstrap"
+
+exec $command
--- web-client/entrypoint.sh
+++ web-client/entrypoint.sh
@@ -1,27 +1,22 @@
 #!/bin/bash -e
 
-if ! [ -e conf/Catalina/ROOT.xml ]; then
-  echo >&2 "lsFusion client is not configured - configuring..."
-
-    if [ -z "$LSFUSION_SERVER_HOST" ]; then
-  	echo >&2 'error: missing required LSFUSION_SERVER_HOST environment variable'
-  	exit 1
-    fi
-
-    if [ -z "$LSFUSION_SERVER_PORT" ]; then
-  	echo >&2 'error: missing required LSFUSION_SERVER_PORT environment variable'
-  	exit 1
-    fi
-
-  mkdir -p conf/Catalina
-
-  cat > conf/Catalina/ROOT.xml <<EOF
+if ! [ -e conf/Catalina/localhost/ROOT.xml ]; then
+  mkdir -p conf/Catalina/localhost
+  cat > conf/Catalina/localhost/ROOT.xml <<EOF
 <?xml version='1.0' encoding='utf-8'?>
 <Context>
-  <Parameter name="host" value="$LSFUSION_SERVER_HOST" override="false"/>
-  <Parameter name="port" value="$LSFUSION_SERVER_PORT" override="false"/>
+  <Parameter name="host" value="localhost" override="false"/>
+  <Parameter name="port" value="7652" override="false"/>
 </Context>
 EOF
+fi
+
+if ! [ -z "$SERVER_HOST" ] && ! [ -z "$SERVER_PORT" ]; then
+  sed -i -e "/unpackWARs=\"true\" autoDeploy=\"true\">/a\
+  <Context path=\"\"> \
+    <Parameter name=\"host\" value=\"$SERVER_HOST\" override=\"false\"/> \
+    <Parameter name=\"port\" value=\"$SERVER_PORT\" override=\"false\"/> \
+  </Context>" conf/server.xml
 fi
 
 exec bin/catalina.sh run
