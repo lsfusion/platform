8f0de02eb2c0d49ac515122e9c3f350200ff21b8
AFilipchyk <filipchyk.andrei@gmail.com>
2021-02-02 12:34:47 +0300

    IMPORT XLS: cast double to BigDecimal with stripTrailingZeros

--- server/src/main/java/lsfusion/server/data/type/AbstractType.java
+++ server/src/main/java/lsfusion/server/data/type/AbstractType.java
@@ -160,7 +160,7 @@ public abstract class AbstractType<T> extends AbstractReader<T> implements Type<
                 if(DateUtil.isCellDateFormatted(cell)) {
                     cellValue = DateClass.instance.formatString(cell.getLocalDateTimeCellValue().toLocalDate());
                 } else {
-                    cellValue = BigDecimal.valueOf(formulaValue.getNumberValue()).toPlainString();
+                    cellValue = BigDecimal.valueOf(formulaValue.getNumberValue()).stripTrailingZeros().toPlainString();
                 }
                 break;        
             default:
