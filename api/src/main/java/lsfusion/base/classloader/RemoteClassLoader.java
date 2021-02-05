76828136bf88939c96aa78e65f2162486e47a21c
alex-byi <alexeybyimistrov@gmail.com>
2021-02-05 10:02:09 +0300

    catch IllegalArgumentException in RemoteClassLoader
    (cherry picked from commit 5b4c4520e3115ec6d327203799a6c6ccfc926949)

--- api/src/main/java/lsfusion/base/classloader/RemoteClassLoader.java
+++ api/src/main/java/lsfusion/base/classloader/RemoteClassLoader.java
@@ -37,12 +37,11 @@ public class RemoteClassLoader extends RMIClassLoaderSpi {
                 Class<?> aClass = findLoadedClass(name);
                 byte[] classBytes;
                 if (aClass == null) {
-
                     classBytes = remoteLogics.findClass(name);
                     aClass = defineClass(name, classBytes, 0, classBytes.length);
                 }
                 return aClass;
-            } catch (RemoteException remote) {
+            } catch (RemoteException | IllegalArgumentException remote) { // catch IllegalArgumentException to the JRClassloader work correctly. Because remoteLogics.findClass() throws IllegalArgumentException from getResource, but JRClassloader catches only ClassNotFoundException
                 throw new ClassNotFoundException(getString("errors.error.loading.class.on.the.client"), remote);
             }
         }
