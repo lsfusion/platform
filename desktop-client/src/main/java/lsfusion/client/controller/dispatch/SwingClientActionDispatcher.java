4c2c70d006ce919937bf4500a591ed6e3cfa6e69
AFilipchyk <filipchyk.andrei@gmail.com>
2021-05-27 15:40:45 +0300

    LoadNetworkPath regexp

--- desktop-client/src/main/java/lsfusion/client/controller/dispatch/SwingClientActionDispatcher.java
+++ desktop-client/src/main/java/lsfusion/client/controller/dispatch/SwingClientActionDispatcher.java
@@ -47,6 +47,8 @@ import java.text.DecimalFormat;
 import java.util.EventObject;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import static lsfusion.client.ClientResourceBundle.getString;
 
@@ -535,8 +537,11 @@ public abstract class SwingClientActionDispatcher implements ClientActionDispatc
         if (!canceled) {
             File file = fileChooser.getSelectedFile();
             result = file.toURI().toString();
-            if(result.startsWith("file:/")) {
-                result = result.replaceFirst("file:/", "file://");
+            //replace "file:/" for "file://"
+            Pattern p = Pattern.compile("file:/([^/].*)");
+            Matcher m = p.matcher(result);
+            if(m.matches()) {
+                result = "file://" + m.group(1);
             }
             SystemUtils.saveCurrentDirectory(file.getParentFile());
         }
