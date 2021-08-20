9d9d546061bc0ec1f930f8dc1955c7e67232d751
alex-byi <alexeybyimistrov@gmail.com>
2021-08-20 12:52:01 +0300

    add googleAutocompleteCountry to lsfParams

--- server/src/main/resources/web/customEditText/googleAutocomplete.js
+++ server/src/main/resources/web/customEditText/googleAutocomplete.js
@@ -1,17 +1,19 @@
-let mapApiKeyGoogle = lsfParams.get('mapApiKey_Google');
-if (mapApiKeyGoogle != null) {
+let mapApiKeyGoogle = lsfParams.mapApiKey_Google;
+
+if (mapApiKeyGoogle != null)
     $.getScript('https://maps.googleapis.com/maps/api/js?key=' + mapApiKeyGoogle + '&libraries=places');
-} else {
+else
     console.error("google key does not exist");
-}
 
 function customGoogleAutocomplete() {
+    if (mapApiKeyGoogle == null)
+        window.alert("google key does not exist");
     return {
         render: (element, editor) => {
             let autocompleteOptions = {
                 types: ['address'],
                 componentRestrictions: {
-                    country: "BLR"
+                country: lsfParams.googleMapAutocompleteCountry
                 }
             };
 
