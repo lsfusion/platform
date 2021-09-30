e2e89444c9809548cca17dc8d2c89532819a83e6
AFilipchyk <filipchyk.andrei@gmail.com>
2021-09-30 16:56:35 +0300

    GetAvailablePrinters: show trays

--- server/src/main/java/lsfusion/server/physics/dev/integration/external/to/equ/printer/client/GetAvailablePrintersClientAction.java
+++ server/src/main/java/lsfusion/server/physics/dev/integration/external/to/equ/printer/client/GetAvailablePrintersClientAction.java
@@ -8,7 +8,10 @@ import javax.print.PrintService;
 import javax.print.PrintServiceLookup;
 import javax.print.attribute.HashPrintRequestAttributeSet;
 import javax.print.attribute.PrintRequestAttributeSet;
-import java.io.IOException;
+import javax.print.attribute.standard.Media;
+import javax.print.attribute.standard.MediaTray;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class GetAvailablePrintersClientAction implements ClientAction {
@@ -22,7 +25,18 @@ public class GetAvailablePrintersClientAction implements ClientAction {
         PrintService[] printServices = PrintServiceLookup.lookupPrintServices(flavor, attributeSet);
         String printerNames = "";
         for (PrintService printService : printServices) {
-            printerNames += printService.getName() + '\n';
+
+            List<String> trays = new ArrayList<>();
+            Object o = printService.getSupportedAttributeValues(Media.class, DocFlavor.SERVICE_FORMATTED.PAGEABLE, null);
+            if (o instanceof Media[]) {
+                for (Media media : (Media[]) o) {
+                    if(media instanceof MediaTray) {
+                        trays.add("'" + media + "'");
+                    }
+                }
+            }
+
+            printerNames += String.format("%s, trays: %s\n", printService.getName(), String.join(", ", trays));
         }
         return printerNames;
     }
