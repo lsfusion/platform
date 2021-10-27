c45f7370a6a5d8bfa978a4e814bd9eca1f107a2f
Alexey Buymistrov <alexeybyimistrov@gmail.com>
2021-10-27 11:39:19 +0300

    Added support Quill editor/renderer for rich text type (closes #671)

--- web-client/src/main/java/lsfusion/gwt/client/form/property/cell/classes/controller/RequestReplaceCellEditor.java
+++ web-client/src/main/java/lsfusion/gwt/client/form/property/cell/classes/controller/RequestReplaceCellEditor.java
@@ -1,45 +1,11 @@
 package lsfusion.gwt.client.form.property.cell.classes.controller;
 
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.event.dom.client.KeyCodes;
-import com.google.gwt.user.client.Event;
 import lsfusion.gwt.client.base.GwtClientUtils;
-import lsfusion.gwt.client.base.view.EventHandler;
-import lsfusion.gwt.client.base.view.grid.DataGrid;
-import lsfusion.gwt.client.form.event.GKeyStroke;
-import lsfusion.gwt.client.form.property.cell.controller.CommitReason;
 import lsfusion.gwt.client.form.property.cell.controller.ReplaceCellEditor;
 import lsfusion.gwt.client.form.property.cell.view.RenderContext;
 
-import static com.google.gwt.dom.client.BrowserEvents.BLUR;
-import static com.google.gwt.dom.client.BrowserEvents.KEYDOWN;
-
-public interface RequestReplaceCellEditor extends RequestCellEditor, ReplaceCellEditor {
-
-    default void onBrowserEvent(Element parent, EventHandler handler) {
-        Event event = handler.event;
-        String type = event.getType();
-        if (KEYDOWN.equals(type)) {
-            int keyCode = event.getKeyCode();
-            if (keyCode == KeyCodes.KEY_ENTER && checkEnterEvent(handler.event)) {
-                handler.consume();
-                commit(parent,  CommitReason.ENTERPRESSED);
-            } else if (keyCode == KeyCodes.KEY_ESCAPE && GKeyStroke.isPlainKeyEvent(handler.event)) {
-                handler.consume();
-                cancel(parent);
-            }
-        } else if (BLUR.equals(type)) {
-            // Cancel the change. Ensure that we are blurring the input element and
-            // not the parent element itself.
-            if (!DataGrid.isFakeBlur(event, parent)) {
-                commit(parent, CommitReason.BLURRED);
-            }
-        }
-    }
-
-    default boolean checkEnterEvent(Event event) {
-        return GKeyStroke.isPlainKeyEvent(event);
-    }
+public interface RequestReplaceCellEditor extends RequestEmbeddedCellEditor, ReplaceCellEditor {
 
     @Override
     default void clearRender(Element cellParent, RenderContext renderContext, boolean cancel) {
--- web-client/src/main/java/lsfusion/gwt/client/form/property/cell/classes/view/TextCellRenderer.java
+++ web-client/src/main/java/lsfusion/gwt/client/form/property/cell/classes/view/TextCellRenderer.java
@@ -1,19 +1,12 @@
 package lsfusion.gwt.client.form.property.cell.classes.view;
 
-import com.google.gwt.dom.client.Document;
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.dom.client.Style;
-import lsfusion.gwt.client.base.EscapeUtils;
-import lsfusion.gwt.client.base.GwtClientUtils;
 import lsfusion.gwt.client.form.property.GPropertyDraw;
-import lsfusion.gwt.client.form.property.cell.view.RenderContext;
 
 public class TextCellRenderer extends StringBasedCellRenderer {
-    private final boolean rich;
 
-    public TextCellRenderer(GPropertyDraw property, boolean rich) {
+    public TextCellRenderer(GPropertyDraw property) {
         super(property);
-        this.rich = rich;
     }
 
     @Override
@@ -22,17 +15,8 @@ public class TextCellRenderer extends StringBasedCellRenderer {
     }
 
     @Override
-    protected boolean isWordWrap() {
-        return !rich;
-    }
-
-    @Override
     protected void setInnerContent(Element element, String innerText) {
-        if (rich) {
-            element.setInnerHTML("<div>"+EscapeUtils.sanitizeHtml(innerText)+"</div>"); // need to wrap in div, since there can be display:flex in element
-        } else {
-            super.setInnerContent(element, innerText);
-        }
+        super.setInnerContent(element, innerText);
     }
 
     @Override
