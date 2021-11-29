d95402c00dc9f88523e7a9b5b5006a03bd065580
Aliaksei Kirkouski <crushby@gmail.com>
2021-11-29 16:50:25 +0300

    Web chat refactoring

---
+++ server/src/main/resources/web/chat/chat.js
@@ -0,0 +1,205 @@
+function chatMessageRender() {
+    return {
+        render: function (element) {
+            element.style.removeProperty("max-height");
+            element.parentElement.style.removeProperty("height");
+
+            var message = document.createElement("div")
+            message.classList.add("chat-message");
+            message.classList.add("ql-bubble");
+
+            var header = document.createElement("div");
+            header.classList.add("chat-header");
+
+            var author = document.createElement("div");
+            author.classList.add("chat-author");
+
+            element.author = author;
+            header.appendChild(author);
+
+            var replyAction = document.createElement("a");
+            replyAction.classList.add("chat-reply-action");
+
+            var replyCaption = document.createTextNode("Reply");
+            replyAction.appendChild(replyCaption);
+
+            element.replyAction = replyAction;
+            header.appendChild(replyAction);
+
+            message.appendChild(header);
+
+            var replyContent = document.createElement("div");
+            replyContent.classList.add("chat-reply-content");
+
+            var replyAuthor = document.createElement("div");
+            replyAuthor.classList.add("chat-reply-author");
+
+            element.replyAuthor = replyAuthor;
+            replyContent.appendChild(replyAuthor);
+
+            var replyText = document.createElement("div");
+            replyText.classList.add("chat-reply-text");
+
+            element.replyText = replyText;
+            replyContent.appendChild(replyText);
+
+            element.replyContent = replyContent;
+            message.appendChild(replyContent);
+
+            var text = document.createElement("div");
+            text.classList.add("chat-text");
+            text.classList.add("ql-editor");
+
+            element.text = text;
+            message.appendChild(text);
+
+            var attachments = document.createElement("div");
+            attachments.classList.add("chat-attachments");
+
+            element.attachments = attachments;
+            message.appendChild(attachments);
+
+            var time = document.createElement("div");
+            time.classList.add("chat-time");
+
+            element.time = time;
+            message.appendChild(time);
+
+            element.message = message;
+            element.appendChild(message);
+        },
+        update: function (element, controller, value) {
+            var obj = JSON.parse(value);
+            element.author.innerHTML = obj.author;
+
+            element.replyAction.onclick = function(event) {
+                controller.changeValue(JSON.stringify({ action : 'reply' }));
+                $(this).closest("div[lsfusion-container='chat']").find(".ql-editor").focus(); // works only in firefox
+            }
+
+            element.replyAuthor.innerHTML = obj.replyAuthor;
+            element.replyText.innerHTML = obj.replyText;
+            element.replyContent.onmousedown = function(event) {
+                controller.changeValue(JSON.stringify({ action : 'goToReply' }));
+            }
+
+            element.time.innerHTML = obj.time;
+            element.text.innerHTML = obj.text;
+
+            while (element.attachments.lastElementChild) {
+                element.attachments.removeChild(element.attachments.lastElementChild);
+            }
+            if (obj.attachment !== '') {
+                var attachmentA = document.createElement("a");
+                attachmentA.classList.add("chat-message-attachment");
+
+                attachmentA.onclick = function(event) {
+                    controller.changeValue(JSON.stringify({ action : 'open', id : obj.id }));
+                }
+
+                var attachmentCaption = document.createTextNode(obj.attachment);
+                attachmentA.appendChild(attachmentCaption);
+
+                element.attachments.appendChild(attachmentA);
+            }
+
+            if (obj.own) {
+                element.message.classList.add('chat-message-own');
+            } else
+                element.message.classList.remove('chat-message-own');
+        }
+    }
+}
+
+function chatMessageInputRender() {
+    var inputQuill = window.inputQuillFiles();
+    return {
+        render: function (element) {
+            var input = document.createElement("div");
+            input.classList.add("chat-message-input");
+
+            var reply = document.createElement("div");
+            reply.classList.add("chat-reply");
+
+            var replyContent = document.createElement("div");
+            replyContent.classList.add("chat-reply-content");
+
+            var replyAuthor = document.createElement("div");
+            replyAuthor.classList.add("chat-reply-author");
+
+            element.replyAuthor = replyAuthor;
+            replyContent.appendChild(replyAuthor);
+
+            var replyText = document.createElement("div");
+            replyText.classList.add("chat-reply-text");
+
+            element.replyText = replyText;
+            replyContent.appendChild(replyText);
+
+            element.replyContent = replyContent;
+            reply.appendChild(replyContent);
+
+            var replyRemove = document.createElement("div");
+            replyRemove.classList.add("chat-reply-remove");
+
+            element.replyRemove = replyRemove;
+            reply.appendChild(replyRemove);
+
+            input.appendChild(reply);
+
+            inputQuill.render(input);
+
+            var attachments = document.createElement("div");
+            attachments.classList.add("chat-attachments");
+
+            element.attachments = attachments;
+            input.appendChild(attachments);
+
+            element.input = input;
+            element.appendChild(input);
+        },
+        update: function (element, controller, value) {
+            if (value !== null) {
+                var obj = JSON.parse(value);
+
+                element.replyAuthor.innerHTML = obj.replyAuthor;
+                element.replyText.innerHTML = obj.replyText;
+
+                element.replyRemove.innerHTML = (obj.replyAuthor === '') ? '' : '❌';
+
+                element.replyRemove.onclick = function(event) {
+                    controller.changeValue(JSON.stringify({ action : 'replyRemove' }));
+                }
+
+                inputQuill.update(element.input, controller, obj.text);
+
+                while (element.attachments.lastElementChild) {
+                    element.attachments.removeChild(element.attachments.lastElementChild);
+                }
+
+                if (obj.attachment !== '') {
+                    var attachmentA = document.createElement("a");
+                    attachmentA.classList.add("chat-message-attachment");
+
+                    attachmentA.onclick = function(event) {
+                        controller.changeValue(JSON.stringify({ action : 'open' }));
+                    }
+
+                    var attachmentCaption = document.createTextNode(obj.attachment);
+                    attachmentA.appendChild(attachmentCaption);
+
+                    element.attachments.appendChild(attachmentA);
+
+                    attachmentDelete = document.createElement("div")
+                    attachmentDelete.classList.add("chat-message-attachment-delete");
+                    attachmentDelete.innerHTML = 'x';
+                    attachmentDelete.onclick = function(event) {
+                        controller.changeValue(JSON.stringify({ action : 'remove' }));
+                    }
+
+                    element.attachments.appendChild(attachmentDelete);
+                }
+            }
+        }
+    }
+}
---
+++ server/src/main/resources/web/property/change/input/input-quill.js
@@ -0,0 +1,114 @@
+function inputQuill(json) {
+    return {
+        render: function (element) {
+            var inputEditor = document.createElement("div");
+            inputEditor.classList.add("quill-input-editor");
+
+            element.inputEditor = inputEditor;
+            element.appendChild(inputEditor);
+
+            var toolbarOptions = [
+              ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+              ['link', 'image'],
+              ['blockquote', 'code-block'],
+
+              [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+              [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+              [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+              [{ 'indent': '-1'}, { 'indent': '+1' }]           // outdent/indent
+
+              [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+              [{ 'align': [] }],
+
+              ['clean']                                         // remove formatting button
+            ];
+
+            var quill = new Quill(inputEditor, {
+                modules: {
+                    toolbar: toolbarOptions
+                },
+                theme: 'bubble',
+                bounds: inputEditor
+            });
+
+            inputEditor.onmousedown = function(event) {
+                event.stopPropagation();
+            }
+
+            inputEditor.onkeydown = function(event) {
+                if (event.keyCode === 13)
+                    event.stopPropagation();
+            }
+
+            inputEditor.onpaste = function(event) {
+                event.stopPropagation();
+            }
+
+            element.quill = quill;
+        },
+        update: function (element, controller, value) {
+            if (value !== element.quill.root.innerHTML)
+                element.quill.root.innerHTML = value;
+
+            element.quill.root.onblur = function (event) {
+                if (! (event.relatedTarget && element.inputEditor.contains(event.relatedTarget)) &&
+                    value !== element.quill.root.innerHTML)
+                    controller.changeValue(json ? JSON.stringify({ action : 'change', value : element.quill.root.innerHTML }) : element.quill.root.innerHTML);
+            }
+        }
+    }
+}
+
+function uploadFiles(items, controller, event) {
+    if (!items) return;
+
+    var reader = new FileReader();
+    var files = [];
+    for (var i = 0; i < items.length; i++)
+        if (items[i].kind === 'file')
+            files.push(items[i].getAsFile());
+
+    function readFile(index) {
+        if (index >= files.length) return;
+        var file = files[index];
+        reader.onload = function(e) {
+            let encoded = e.target.result.toString().replace(/^data:(.*,)?/, '');
+            if ((encoded.length % 4) > 0) {
+               encoded += '='.repeat(4 - (encoded.length % 4));
+            }
+
+            if (file.name !== 'image.png')
+                controller.changeValue(JSON.stringify({ action : 'upload', name : file.name, data : encoded }));
+
+            readFile(index+1);
+        }
+        reader.readAsDataURL(file);
+    }
+    readFile(0);
+}
+
+function inputQuillFiles() {
+    var inputQuill = window.inputQuill(true);
+    return {
+        render: function (element) {
+            inputQuill.render(element);
+        },
+        update: function (element, controller, value) {
+            inputQuill.update(element, controller, value);
+
+            element.quill.root.onpaste = function(event) {
+                event.stopPropagation();
+
+                uploadFiles((event.clipboardData || event.originalEvent.clipboardData).items, controller, event);
+            }
+
+            element.quill.root.ondrop = function (event) {
+                if (event.dataTransfer.files.length > 0) {
+                    event.preventDefault();
+
+                    uploadFiles(event.dataTransfer.items, controller, event);
+                }
+            }
+        }
+    }
+}
