05983bb6bc5a9eb590daf1f8b2c41925aada84ae
a.filipchik <4tefk3j5>
2022-04-27 12:04:32 +0300

    GClassDialog now extends WindowBox instead of ResizableSystemModalWindow

--- web-client/src/main/java/lsfusion/gwt/client/form/classes/view/GClassDialog.java
+++ web-client/src/main/java/lsfusion/gwt/client/form/classes/view/GClassDialog.java
@@ -1,15 +1,13 @@
 package lsfusion.gwt.client.form.classes.view;
 
 import com.google.gwt.dom.client.Style;
-import com.google.gwt.event.dom.client.ClickEvent;
-import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.*;
 import lsfusion.gwt.client.ClientMessages;
-import lsfusion.gwt.client.base.view.ResizableSystemModalWindow;
 import lsfusion.gwt.client.base.view.ResizableVerticalPanel;
+import lsfusion.gwt.client.base.view.WindowBox;
 import lsfusion.gwt.client.classes.GObjectClass;
 
-public class GClassDialog extends ResizableSystemModalWindow {
+public class GClassDialog extends WindowBox {
     private static final ClientMessages messages = ClientMessages.Instance.get();
 
     private final boolean concreate;
@@ -23,10 +21,14 @@ public class GClassDialog extends ResizableSystemModalWindow {
     private GObjectClass chosenClass;
 
     public GClassDialog(GObjectClass baseClass, GObjectClass defaultClass, boolean concreate, final ClassChosenHandler classChosenHandler) {
-        super(messages.choosingClass());
+        super(false, false, true);
 
         this.concreate = concreate;
         this.classChosenHandler = classChosenHandler;
+
+        setModal(true);
+        setGlassEnabled(true);
+        setText(messages.choosingClass());
 
         configureLayout(baseClass, defaultClass);
 
@@ -61,19 +63,13 @@ public class GClassDialog extends ResizableSystemModalWindow {
         mainPanel.setPixelSize(500, 500);
         mainPanel.add(centerPanel);
 
-        setContentWidget(mainPanel);
+        setWidget(mainPanel);
+        center();
     }
 
     private void bindUIHandlers() {
-        btnOk.addClickHandler(new ClickHandler() {
-            @Override
-            public void onClick(ClickEvent event) {
-                okPressed();
-            }
-        });
-
+        btnOk.addClickHandler(event -> okPressed());
         btnCancel.addClickHandler(event -> chooseClass(null));
-        setWindowHiddenHandler(() -> classChosenHandler.onClassChosen(chosenClass));
     }
 
     private void okPressed() {
@@ -90,9 +86,13 @@ public class GClassDialog extends ResizableSystemModalWindow {
         }
     }
 
-    public static GClassDialog showDialog(GObjectClass baseClass, GObjectClass defaultClass, boolean concreate, ClassChosenHandler classChosenHandler) {
-        GClassDialog classDlg = new GClassDialog(baseClass, defaultClass, concreate, classChosenHandler);
-        classDlg.show();
-        return classDlg;
+    public static void showDialog(GObjectClass baseClass, GObjectClass defaultClass, boolean concreate, ClassChosenHandler classChosenHandler) {
+        new GClassDialog(baseClass, defaultClass, concreate, classChosenHandler).show();
+    }
+
+    @Override
+    public void hide(boolean autoClosed) {
+        super.hide(autoClosed);
+        classChosenHandler.onClassChosen(chosenClass);
     }
 }
