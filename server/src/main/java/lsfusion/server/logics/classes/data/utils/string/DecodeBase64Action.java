bad81a1cc0a9034490e0a0a562157e617be2b172
Eveigh <varets.p@gmail.com>
2022-04-29 10:37:35 +0300

    allowNulls in encode\decode base64

--- server/src/main/java/lsfusion/server/logics/classes/data/utils/string/DecodeBase64Action.java
+++ server/src/main/java/lsfusion/server/logics/classes/data/utils/string/DecodeBase64Action.java
@@ -26,12 +26,17 @@ public class DecodeBase64Action extends InternalAction {
 
     @Override
     public void executeInternal(ExecutionContext<ClassPropertyInterface> context) throws SQLException, SQLHandledException {
-        String value = (String) context.getDataKeyValue(stringInterface).getValue();
+        String value = (String) context.getKeyValue(stringInterface).getValue();
         try {
-            String decoded = new String(Base64.decodeBase64(value.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8);
+            String decoded = value!= null ? new String(Base64.decodeBase64(value.getBytes(StandardCharsets.UTF_8)), StandardCharsets.UTF_8) : null;
             findProperty("decodedBase64[]").change(decoded, context);
         } catch (ScriptingErrorLog.SemanticErrorException e) {
             throw Throwables.propagate(e);
         }
+    }
+
+    @Override
+    protected boolean allowNulls() {
+        return true;
     }
 }
