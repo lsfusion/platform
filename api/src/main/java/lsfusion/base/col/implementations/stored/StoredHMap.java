b7e8fadeba65a79baebf9f17f0f7e6c64ecdaee2
Aliaksei <danchenko@gmail.com>
2022-05-10 17:17:52 +0400

    Adds StoredHSet and StoredHMap

---
+++ api/src/main/java/lsfusion/base/col/implementations/stored/StoredHMap.java
@@ -0,0 +1,172 @@
+package lsfusion.base.col.implementations.stored;
+
+import lsfusion.base.BaseUtils;
+import lsfusion.base.col.MapFact;
+import lsfusion.base.col.implementations.abs.AMRevMap;
+import lsfusion.base.col.interfaces.immutable.ImMap;
+import lsfusion.base.col.interfaces.immutable.ImOrderMap;
+import lsfusion.base.col.interfaces.mutable.AddValue;
+import lsfusion.base.col.interfaces.mutable.MExclMap;
+import lsfusion.base.col.interfaces.mutable.mapvalue.ImRevValueMap;
+import lsfusion.base.col.interfaces.mutable.mapvalue.ImValueMap;
+
+public class StoredHMap<K, V> extends AMRevMap<K, V> {
+    public int size;
+    public int[] indexes; // номера в таблице
+
+    public StoredArray<K> table;
+    protected StoredArray<V> vtable;
+
+    private final static float loadFactor = 0.3f;
+
+    public StoredHMap(StoredHMap<? extends K, ? extends V> map, AddValue<K, V> addValue) {
+        super(addValue);
+        size = map.size;
+        indexes = map.indexes.clone();
+        table = new StoredArray<>(map.table);
+        vtable = new StoredArray<>(map.vtable);
+    }
+
+    public StoredHMap(StoredHMap<? extends K, ? extends V> map, boolean clone) {
+        this(map, null);
+        assert clone;
+    }
+
+    public StoredHMap(int size, K[] table, V[] vtable, int[] indexes) throws StoredArray.StoredArrayCreationException {
+        this(
+                size,
+                new StoredArray<>(table, StoredArraySerializer.getInstance()),
+                new StoredArray<>(vtable, StoredArraySerializer.getInstance()),
+                indexes
+        );
+    }
+
+    private StoredHMap(int size, K[] table, int[] indexes) throws StoredArray.StoredArrayCreationException {
+        this(
+                size,
+                new StoredArray<>(table, StoredArraySerializer.getInstance()),
+                new StoredArray<>(table.length, StoredArraySerializer.getInstance()),
+                indexes
+        );
+    }
+
+    private StoredHMap(int size, StoredArray<K> table, int[] indexes) {
+        this(size, table, new StoredArray<>(table.size(), table.getSerializer()), indexes);
+    }
+
+    public StoredHMap(int size, StoredArray<K> table, StoredArray<V> vtable, int[] indexes) {
+        this.size = size;
+        this.indexes = indexes;
+        this.table = table;
+        this.vtable = vtable;
+    }
+
+    public StoredHMap(StoredHMap<K, ?> map) {
+        this(map.size, map.table, map.indexes);
+    }
+
+    public StoredHMap(StoredHSet<K> set) {
+        this(set.size(), set.table, set.indexes);
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public K getKey(int i) {
+        assert i<size;
+        return table.get(indexes[i]);
+    }
+
+    public V getValue(int i) {
+        assert i<size;
+        return vtable.get(indexes[i]);
+    }
+
+    private void resize(int length) {
+        int[] newIndexes = new int[(int) (length * loadFactor) + 1];
+
+        StoredArray<K> newTable = new StoredArray<>(length, table.getSerializer());
+        StoredArray<V> newVTable = new StoredArray<>(length, table.getSerializer());
+        for (int i = 0; i < size; i++) {
+            K key = table.get(indexes[i]);
+            int newHash = MapFact.colHash(key.hashCode()) & (length - 1);
+            while (newTable.get(newHash) != null) {
+                newHash = (newHash == length-1 ? 0 : newHash+1);
+            }
+
+            newTable.set(newHash, key);
+            newVTable.set(newHash, vtable.get(indexes[i]));
+            newIndexes[i] = newHash;
+        }
+
+        table = newTable;
+        vtable = newVTable;
+        indexes = newIndexes;
+    }
+
+    public boolean add(K key, V value) {
+        if (size >= indexes.length) resize(2 * table.size());
+        assert !(value==null && getAddValue().stopWhenNull()); // связано с addValue, которое в свою очередь связано с intersect - в котором важно выйти быстро, если уже не подходит
+
+        int hash = MapFact.colHash(key.hashCode());
+        int i = hash & (table.size() - 1);
+        while (table.get(i) != null) {
+            if (BaseUtils.hashEquals(table.get(i), key)) {
+                AddValue<K, V> addValue = getAddValue();
+                V addedValue = addValue.addValue(key, vtable.get(i), value);
+                if (addValue.stopWhenNull() && addedValue==null)
+                    return false;
+                vtable.set(i, addedValue);
+                return true;
+            }
+            i = (i == table.size()-1 ? 0 : i+1);
+        }
+        table.set(i, key);
+        vtable.set(i, value);
+        indexes[size++] = i;
+        return true;
+    }
+
+    @Override
+    public V getObject(Object key) {
+        int start = MapFact.colHash(key.hashCode()) & (table.size()-1);
+        for (int i = start; table.get(i) != null; i = (i == table.size()-1 ? 0 : i+1))
+            if (BaseUtils.hashEquals(table.get(i), key))
+                return vtable.get(i);
+        return null;
+    }
+
+    public ImMap<K, V> immutable() {
+        return this;
+    }
+
+    protected MExclMap<K, V> copy() {
+        return new StoredHMap<>(this, true);
+    }
+
+    public void mapValue(int i, V value) {
+        assert i<size;
+        vtable.set(indexes[i], value);
+    }
+
+    public <M> ImValueMap<K, M> mapItValues() {
+        return new StoredHMap<>(this);
+    }
+
+    public <M> ImRevValueMap<K, M> mapItRevValues() {
+        return new StoredHMap<>(this);
+    }
+
+    @Override
+    public ImOrderMap<K, V> toOrderMap() {
+        // todo [dale]: uncomment
+//        return new StoredHOrderMap<>(this);
+        return null;
+    }
+
+    @Override
+    public StoredHSet<K> keys() {
+        return new StoredHSet<>(size, table, indexes);
+    }
+}
---
+++ api/src/main/java/lsfusion/base/col/implementations/stored/StoredHSet.java
@@ -0,0 +1,137 @@
+package lsfusion.base.col.implementations.stored;
+
+import lsfusion.base.BaseUtils;
+import lsfusion.base.col.MapFact;
+import lsfusion.base.col.implementations.abs.AMSet;
+import lsfusion.base.col.interfaces.immutable.ImOrderSet;
+import lsfusion.base.col.interfaces.immutable.ImRevMap;
+import lsfusion.base.col.interfaces.immutable.ImSet;
+import lsfusion.base.col.interfaces.mutable.mapvalue.ImRevValueMap;
+import lsfusion.base.col.interfaces.mutable.mapvalue.ImValueMap;
+
+public class StoredHSet<T> extends AMSet<T> {
+    private int size;
+    protected StoredArray<T> table;
+    public int[] indexes;
+
+    public static final float loadFactor = 0.3f;
+
+    public StoredHSet(int size, Object[] table, int[] indexes) throws StoredArray.StoredArrayCreationException {
+        this.size = size;
+        this.indexes = indexes;
+        this.table = new StoredArray<>((T[]) table, StoredArraySerializer.getInstance());
+    }
+
+    public StoredHSet(StoredHSet<T> set) {
+        size = set.size;
+        table = new StoredArray<>(set.table);
+        indexes = set.indexes.clone();
+    }
+
+    public StoredHSet(int size, StoredArray<T> table, int[] indexes) {
+        this.size = size;
+        this.indexes = indexes;
+        this.table = table;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean contains(T element) {
+        int start = MapFact.colHash(element.hashCode()) & (table.size()-1);
+        for (int i = start; table.get(i) != null; i = (i == table.size()-1 ? 0 : i+1)) {
+            if (BaseUtils.hashEquals(table.get(i), element)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    public T getIdentIncl(T element) {
+        int start = MapFact.colHash(element.hashCode()) & (table.size()-1);
+        for (int i = start; table.get(i) != null; i = (i == table.size()-1 ? 0 : i+1)) {
+            if (BaseUtils.hashEquals(table.get(i), element)) {
+                return table.get(i);
+            }
+        }
+        assert false;
+        return null;
+    }
+
+    private void resize(int length) {
+        int[] newIndexes = new int[(int)(length * loadFactor)+1];
+        StoredArray<T> newTable = new StoredArray<>(length, table.getSerializer());
+        for (int i = 0; i < size; ++i) {
+            T object = table.get(indexes[i]);
+            int index = MapFact.colHash(object.hashCode()) & (length-1);
+            while (newTable.get(index) != null) {
+                index = (index == length-1 ? 0 : index+1);
+            }
+            newTable.set(index, object);
+            newIndexes[i] = index;
+        }
+
+        table = newTable;
+        indexes = newIndexes;
+    }
+
+    public boolean add(T element) {
+        if (size >= indexes.length) resize(2 * table.size());
+
+        int i = MapFact.colHash(element.hashCode()) & (table.size()-1);
+        while (table.get(i) != null) {
+            if (BaseUtils.hashEquals(table.get(i), element))
+                return true;
+            i = (i == table.size()-1 ? 0 : i+1);
+        }
+        table.set(i, element);
+        indexes[size++] = i;
+        return false;
+    }
+
+    public T get(int i) {
+        return table.get(indexes[i]);
+    }
+
+    public <M> ImValueMap<T, M> mapItValues() {
+        return new StoredHMap<>(this);
+    }
+
+    public <M> ImRevValueMap<T, M> mapItRevValues() {
+        return new StoredHMap<>(this);
+    }
+
+    public ImSet<T> immutable() {
+//        StoredArray<T> tmpArray = new StoredArray<>(table.getSerializer());
+//        for(int i = 0; i < size; ++i) {
+//            tmpArray.append(get(i));
+//        }
+//        tmpArray.sort();
+//        return new StoredArIndexedSet<>(tmpArray);
+        return this;
+    }
+
+    public ImSet<T> immutableCopy() {
+        return new StoredHSet<>(this).immutable();
+    }
+
+    @Override
+    public StoredHMap<T, T> toMap() {
+        return new StoredHMap<>(size, table, table, indexes);
+    }
+
+    @Override
+    public ImRevMap<T, T> toRevMap() {
+        return toMap();
+    }
+
+    @Override
+    public ImOrderSet<T> toOrderSet() {
+        // todo [dale]: uncomment
+        // return new StoredHOrderSet<>(this);
+        return null;
+    }
+
+}
