f8a20e934e12daa319460dac932c943d8a0aa60a
Aliaksei Kirkouski <crushby@gmail.com>
2022-05-26 12:03:25 +0300

    Added functions to set and get cookies

--- web-client/src/main/webapp/static/js/utils.js
+++ web-client/src/main/webapp/static/js/utils.js
@@ -32,3 +32,25 @@ var getSingleRanges = function (wnd, rangeToday, rangeYesterday, rangeSevenDaysA
 
 var lsfParams = {};
 var lsfFiles = {};
+
+// cookies
+function getCookie(name) {
+    let matches = document.cookie.match(new RegExp(
+        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+    ));
+    return matches ? decodeURIComponent(matches[1]) : undefined;
+}
+
+function setCookie(name, value, options) {
+    let updatedCookie = encodeURIComponent(name) + "=" + encodeURIComponent(value);
+
+    for (let optionKey in options) {
+        updatedCookie += "; " + optionKey;
+        let optionValue = options[optionKey];
+        if (optionValue !== true) {
+            updatedCookie += "=" + optionValue;
+        }
+    }
+
+    document.cookie = updatedCookie;
+}
