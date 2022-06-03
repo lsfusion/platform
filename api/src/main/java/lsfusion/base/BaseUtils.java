23b0b5cb0af422a39fcafa0f3ad84176e1835064
alex-byi <alexeybyimistrov@gmail.com>
2022-06-03 14:02:58 +0300

    apiVersion

--- api/src/main/java/lsfusion/base/BaseUtils.java
+++ api/src/main/java/lsfusion/base/BaseUtils.java
@@ -60,7 +60,7 @@ public class BaseUtils {
     private static final int STRING_SERIALIZATION_CHUNK_SIZE = 65535/3;
 
     public static Integer getApiVersion() {
-        return 202;
+        return 203;
     }
 
     public static String getPlatformVersion() {
