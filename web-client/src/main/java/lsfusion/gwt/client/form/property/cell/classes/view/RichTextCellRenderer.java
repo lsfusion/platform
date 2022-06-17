b8f3ebaeae21893d36930429f32f5bdd45fce905
alex-byi <alexeybyimistrov@gmail.com>
2022-06-17 15:49:16 +0300

    Replace <div> for <p> in RICHTEXT only in tags

--- web-client/src/main/java/lsfusion/gwt/client/form/property/cell/classes/view/RichTextCellRenderer.java
+++ web-client/src/main/java/lsfusion/gwt/client/form/property/cell/classes/view/RichTextCellRenderer.java
@@ -48,7 +48,7 @@ public class RichTextCellRenderer extends StringBasedCellRenderer{
         });
 
         if (innerText != null)
-            quill.root.innerHTML = innerText.includes('div') ? innerText.replaceAll('div', 'p') : innerText;
+            quill.root.innerHTML = innerText.includes('<div>') ? innerText.replaceAll('<div>', '<p>').replaceAll('</div>', '</p>') : innerText;
 
         element.quill = quill;
     }-*/;
