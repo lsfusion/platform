a85f4410c5c19c3636aa5d9db114544817ec447e
alex-byi <alexeybyimistrov@gmail.com>
2022-07-15 11:19:39 +0300

    Fixed multiple <br> in EMAIL action when send HTML file

--- api/src/main/java/lsfusion/base/EscapeUtils.java
+++ api/src/main/java/lsfusion/base/EscapeUtils.java
@@ -1,8 +1,14 @@
 package lsfusion.base;
 
+import java.util.regex.Pattern;
+
 public class EscapeUtils {
 
     public static String escapeLineBreakHTML(String value) {
         return value.replace("\n", "<br/>");
+    }
+
+    public static boolean isContainHtmlTag(String value) {
+        return Pattern.compile("<(\"[^\"]*\"|'[^']*'|[^'\">])*>").matcher(value).find();
     }
 }
