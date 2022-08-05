5599271d05df3c63c44a37d2bbee40cb2707dca3
AFilipchyk <filipchyk.andrei@gmail.com>
2022-08-05 14:07:50 +0300

    EmailReceiver: extra logs

--- server/src/main/java/lsfusion/server/physics/dev/integration/external/to/mail/EmailReceiver.java
+++ server/src/main/java/lsfusion/server/physics/dev/integration/external/to/mail/EmailReceiver.java
@@ -238,7 +238,7 @@ public class EmailReceiver {
             Set<String> usedEmails = new HashSet<>();
             while(count < messageCount && (maxMessagesAccount == null ||  count < maxMessagesAccount)) {
                 try {
-                    ServerLoggers.mailLogger.info(String.format("Reading email %s of %s (max %s)", count, messageCount, maxMessagesAccount));
+                    ServerLoggers.mailLogger.info(String.format("Reading email %s of %s (max %s)", count + 1, messageCount, maxMessagesAccount));
                     Message message = emailFolder.getMessage(messageCount - count);
                     Timestamp dateTimeSentEmail = getSentDate(message);
                     ServerLoggers.mailLogger.info("sentDate: " + dateTimeSentEmail);
@@ -251,7 +251,7 @@ public class EmailReceiver {
                         if (!skipEmails.contains(idEmail)) {
                             message.setFlag(deleteMessagesAccount ? Flags.Flag.DELETED : Flags.Flag.SEEN, true);
                             Object messageContent = getEmailContent(message);
-                            MultipartBody messageEmail = messageContent instanceof Multipart ? getMultipartBody(subjectEmail, (Multipart) messageContent, unpack) : messageContent instanceof FilterInputStream ? getMultipartBodyStream(subjectEmail, (FilterInputStream) messageContent, decodeFileName(message.getFileName()), unpack) : messageContent instanceof String ? new MultipartBody(((String) messageContent).replace("\0", ""), null) : null;
+                            MultipartBody messageEmail = messageContent instanceof Multipart ? getMultipartBody(subjectEmail, (Multipart) messageContent, unpack, "") : messageContent instanceof FilterInputStream ? getMultipartBodyStream(subjectEmail, (FilterInputStream) messageContent, decodeFileName(message.getFileName()), unpack) : messageContent instanceof String ? new MultipartBody(((String) messageContent).replace("\0", ""), null) : null;
                             if (messageEmail == null) {
                                 messageEmail = new MultipartBody(messageContent == null ? null : String.valueOf(messageContent), null);
                                 ServerLoggers.mailLogger.error("Warning: missing attachment '" + messageContent + "' from email '" + subjectEmail + "'");
@@ -344,14 +344,14 @@ public class EmailReceiver {
         return id;
     }
 
-    private MultipartBody getMultipartBody(String subjectEmail, Multipart mp, boolean unpack) throws IOException, MessagingException {
+    private MultipartBody getMultipartBody(String subjectEmail, Multipart mp, boolean unpack, String prefix) throws IOException, MessagingException {
         String body = "";
         Map<String, FileData> attachments = new HashMap<>();
         int parts = mp.getCount();
         for (int i = 0; i < parts; i++) {
             BodyPart bp = mp.getBodyPart(i);
             String disp = bp.getDisposition();
-            ServerLoggers.mailLogger.info(String.format("reading attachment %s of %s", i, parts));
+            ServerLoggers.mailLogger.info(String.format("%sreading attachment %s of %s", prefix, i + 1, parts));
             if (disp != null && (disp.equalsIgnoreCase(BodyPart.ATTACHMENT))) {
                 String fileName = decodeFileName(bp.getFileName());
 
@@ -366,6 +366,7 @@ public class EmailReceiver {
                     }
                     fos.close();
 
+                    ServerLoggers.mailLogger.info(String.format("%sattachment name: %s, size: %s", prefix, fileName, f.length()));
                     if(bp.getContentType() != null && bp.getContentType().contains("application/ms-tnef")) {
                         attachments.putAll(extractWinMail(f).attachments);
                     } else {
@@ -373,7 +374,7 @@ public class EmailReceiver {
                     }
 
                 } catch (IOException ioe) {
-                    ServerLoggers.mailLogger.error("Error reading attachment '" + fileName + "' from email '" + subjectEmail + "'");
+                    ServerLoggers.mailLogger.error(prefix + "Error reading attachment '" + fileName + "' from email '" + subjectEmail + "'");
                     throw ioe;
                 } finally {
                     if(!f.delete())
@@ -385,13 +386,18 @@ public class EmailReceiver {
                     if (content instanceof FilterInputStream) {
                         RawFileData byteArray = new RawFileData((FilterInputStream) content);
                         String fileName = decodeFileName(bp.getFileName());
+                        ServerLoggers.mailLogger.info(prefix + "attachment is FilterInputStream: " + fileName);
                         attachments.putAll(unpack(byteArray, fileName, unpack));
                     } else if (content instanceof MimeMultipart) {
-                        body = getMultipartBody(subjectEmail, (Multipart) content, unpack).message;
+                        ServerLoggers.mailLogger.info(prefix + "attachment is MimeMultipart");
+                        body = getMultipartBody(subjectEmail, (Multipart) content, unpack, prefix + "---").message;
                     } else if(content instanceof IMAPInputStream){
                         body = parseIMAPInputStream(bp, (IMAPInputStream) content);
-                    } else
+                        ServerLoggers.mailLogger.info(prefix + "attachment is IMAPInputStream, length: " + (body != null ? body.length() : "0"));
+                    } else {
                         body = String.valueOf(content);
+                        ServerLoggers.mailLogger.info(prefix + "attachment is String, length: " + (body != null ? body.length() : "0"));
+                    }
                 } catch (IOException e) {
                     throw new RuntimeException("Email subject: " + subjectEmail, e);
                 }
