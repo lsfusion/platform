c16a1e99721b7f2e694498cf6d3755142cb750f1
AFilipchyk <filipchyk.andrei@gmail.com>
2022-08-25 14:42:37 +0300

    prefixSearch2

--- server/src/main/resources/sql/postgres/prefixSearch.sql
+++ server/src/main/resources/sql/postgres/prefixSearch.sql
@@ -2,31 +2,40 @@ CREATE OR REPLACE FUNCTION prefixSearchPrepareQuery(querytext text) RETURNS text
 $$
 SELECT
     TRIM( -- 4. trim spaces
-        TRIM(BOTH '\|' FROM -- 3. remove leading and trailing '|'
-             REPLACE(
-                 REGEXP_REPLACE(querytext, '(?<!\\),', '|', 'g'), -- 1. replace unquoted ',' to '|'
-                 '\,', ',') -- 2. replace quoted ',' to unquoted ','
-            )
+            TRIM(BOTH '\|' FROM -- 3. remove leading and trailing '|'
+                 REPLACE(
+                         REGEXP_REPLACE(querytext, '(?<!\\),', '|', 'g'), -- 1. replace unquoted ',' to '|'
+                         '\,', ',') -- 2. replace quoted ',' to unquoted ','
+                )
         )
-
 ;
 $$ LANGUAGE 'sql' IMMUTABLE;
 
-
-CREATE OR REPLACE FUNCTION prefixSearch(config regconfig, querytext text) RETURNS tsquery AS
+CREATE OR REPLACE FUNCTION prefixSearch(config regconfig, querytext text, isOld boolean) RETURNS tsquery AS
 $$
-
 SELECT CASE
-        -- use websearch if query contains special characters or is empty
-        WHEN queryText ~ '^.*(\(|\)|\&|\:|\*|\!).*$' OR prefixSearchPrepareQuery(querytext) = '' IS NOT FALSE THEN websearch_to_tsquery(config, querytext)
+           -- use websearch/plain if query contains special characters or is empty
+           WHEN queryText ~ '^.*(\(|\)|\&|\:|\*|\!).*$' OR querytext = '' IS NOT FALSE THEN
+			(CASE WHEN isOld THEN plainto_tsquery(config, querytext) ELSE websearch_to_tsquery(config, querytext) END)
         ELSE to_tsquery(config,
             CONCAT (
                 REPLACE(
                     REGEXP_REPLACE(
                         REGEXP_REPLACE(
-                                prefixSearchPrepareQuery(querytext),
+                                queryText,
                                 '[\s\|]*\|[\s\|]*','|', 'g'), -- 1. replace spaces + '|' to '|'
                         '\s+', ':* & ', 'g'), -- 2. replace spaces to '':* & '
                     '|', ':* | '), -- 3. replace '|' to ':* | '
             ':*')) END; -- 4. add ':*' in the end
+$$ LANGUAGE 'sql' IMMUTABLE;
+
+
+CREATE OR REPLACE FUNCTION prefixSearch(config regconfig, querytext text) RETURNS tsquery AS
+$$
+SELECT prefixSearch(config, prefixSearchPrepareQuery(querytext), false);
+$$ LANGUAGE 'sql' IMMUTABLE;
+
+CREATE OR REPLACE FUNCTION prefixSearchOld(config regconfig, querytext text) RETURNS tsquery AS
+$$
+SELECT prefixSearch(config, prefixSearchPrepareQuery(querytext), true);
 $$ LANGUAGE 'sql' IMMUTABLE;
