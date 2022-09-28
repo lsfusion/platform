3a345251a16ee91463cbebadc7242063444aafab
AFilipchyk <filipchyk.andrei@gmail.com>
2022-09-28 12:43:39 +0300

    Email: POP3S support (closes #855)
    (cherry picked from commit 9c83600edb465938341a3353135e954868135a3d)

--- server/src/main/java/lsfusion/server/physics/dev/integration/external/to/mail/AccountType.java
+++ server/src/main/java/lsfusion/server/physics/dev/integration/external/to/mail/AccountType.java
@@ -3,11 +3,13 @@ package lsfusion.server.physics.dev.integration.external.to.mail;
 import static lsfusion.base.BaseUtils.trimToEmpty;
 
 public enum AccountType {
-    POP3, IMAP, IMAPS;
+    POP3, POP3S, IMAP, IMAPS;
 
     public static AccountType get(String name) {
         name = trimToEmpty(name);
         switch (name) {
+            case "POP3S":
+                return AccountType.POP3S;
             case "IMAP":
                 return AccountType.IMAP;
             case "IMAPS":
@@ -20,6 +22,8 @@ public enum AccountType {
 
     public String getProtocol() {
         switch (this) {
+            case POP3S:
+                return "pop3s";
             case IMAP:
                 return "imap";
             case IMAPS:
@@ -29,4 +33,18 @@ public enum AccountType {
                 return "pop3";
         }
     }
+
+    public String getHost() {
+        switch (this) {
+            case POP3S:
+                return "mail.pop3s.host";
+            case IMAP:
+            case IMAPS:
+                return "mail.imap.host";
+            case POP3:
+            default:
+                return "mail.pop3.host";
+        }
+    }
+
 }
