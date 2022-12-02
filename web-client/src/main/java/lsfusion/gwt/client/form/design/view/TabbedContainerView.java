e26026744d18716ca24e648ec1b32a283166ae07
AFilipchyk <filipchyk.andrei@gmail.com>
2022-12-02 14:57:16 +0300

    TabbedContainerView: fix active tab on init

--- web-client/src/main/java/lsfusion/gwt/client/form/design/view/TabbedContainerView.java
+++ web-client/src/main/java/lsfusion/gwt/client/form/design/view/TabbedContainerView.java
@@ -28,10 +28,6 @@ public class TabbedContainerView extends GAbstractContainerView {
 
         panel = new FlexTabbedPanel(vertical);
 
-        if (container.children.size() > 0) {
-            currentChild = container.children.get(0);
-        }
-
         panel.setSelectionHandler(index -> {
             onTabSelected(index, formController, container);
         });
