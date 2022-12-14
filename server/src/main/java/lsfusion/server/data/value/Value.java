9d2d6c9b36f135a9ce4c7e9b10cd89ab912ca7a1
AlexKirkouski <alex.kirkouski@gmail.com>
2022-12-14 15:36:09 +0300

    Added some extra logs for temporary tables (monitor / process, explain)

--- server/src/main/java/lsfusion/server/data/value/Value.java
+++ server/src/main/java/lsfusion/server/data/value/Value.java
@@ -4,11 +4,13 @@ import lsfusion.base.col.interfaces.mutable.add.MAddSet;
 import lsfusion.base.comb.map.GlobalObject;
 import lsfusion.server.data.query.compile.ParseValue;
 
+import java.util.Map;
+
 public interface Value extends ParseValue {
 
     GlobalObject getValueClass();
 
     Value removeBig(MAddSet<Value> usedValues);
 
-    String toDebugString();
+    String toDebugString(Map<String, String> sessionDebugInfo);
 }
