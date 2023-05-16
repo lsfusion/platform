66b6eb21bc3c2031d56c1c6fe82d58d0a12dbc99
AlexKirkouski <alex.kirkouski@gmail.com>
2023-05-16 13:35:49 +0100

    Small bugfix of the incorrect alignment for containers

--- server/src/main/java/lsfusion/server/logics/form/interactive/design/BaseComponentView.java
+++ server/src/main/java/lsfusion/server/logics/form/interactive/design/BaseComponentView.java
@@ -1,5 +1,8 @@
 package lsfusion.server.logics.form.interactive.design;
 
+import lsfusion.interop.base.view.FlexAlignment;
+import lsfusion.server.logics.form.struct.FormEntity;
+
 public class BaseComponentView extends ComponentView {
 
     public BaseComponentView() {
@@ -7,5 +10,15 @@ public class BaseComponentView extends ComponentView {
 
     public BaseComponentView(int ID) {
         super(ID);
+    }
+
+    protected FlexAlignment getDefaultAlignment(FormEntity formEntity) {
+        ContainerView container = getLayoutParamContainer();
+        if (container != null && container.isHorizontal())
+            // in horizontal containers base components better looks when they are aligned to the center
+            // it's important because form-control / buttons in the bootstrap theme has the height that is different from the height of text / boolean input
+            return FlexAlignment.CENTER;
+
+        return super.getDefaultAlignment(formEntity);
     }
 }
