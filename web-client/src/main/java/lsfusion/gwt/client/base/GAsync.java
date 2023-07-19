21114d1723bd8ab13a591faa01d3664e2bae060e
AlexKirkouski <alex.kirkouski@gmail.com>
2023-07-19 18:13:39 +0100

    Bugfix of the incorrect "too short string" message

--- web-client/src/main/java/lsfusion/gwt/client/base/GAsync.java
+++ web-client/src/main/java/lsfusion/gwt/client/base/GAsync.java
@@ -13,7 +13,7 @@ public class GAsync implements IsSerializable, Serializable {
 
     public static final GAsync RECHECK = new GAsync("RECHECK", "RECHECK", null);
     public static final GAsync CANCELED = new GAsync("CANCELED", "CANCELED", null);
-    public static final GAsync NEEDMORE = new GAsync("CANCELED", "CANCELED", null);
+    public static final GAsync NEEDMORE = new GAsync("NEEDMORE", "NEEDMORE", null);
 
     public GAsync() {
     }
