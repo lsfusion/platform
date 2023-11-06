0a258f01a8f33c556a583e8b70bff2d4aa16e0cb
ichavchavadze <filipchyk.andrei@gmail.com>
2023-11-06 09:50:40 +0100

    PopupPanel

--- web-client/src/main/java/lsfusion/gwt/client/base/view/PopupDialogPanel.java
+++ web-client/src/main/java/lsfusion/gwt/client/base/view/PopupDialogPanel.java
@@ -1,9 +1,9 @@
 package lsfusion.gwt.client.base.view;
 
 import com.google.gwt.dom.client.Element;
-import com.google.gwt.user.client.ui.PopupPanel;
 import lsfusion.gwt.client.base.FocusUtils;
 import lsfusion.gwt.client.base.GwtClientUtils;
+import lsfusion.gwt.client.base.view.popup.PopupPanel;
 import lsfusion.gwt.client.view.MainFrame;
 
 // twin of a WindowBox
