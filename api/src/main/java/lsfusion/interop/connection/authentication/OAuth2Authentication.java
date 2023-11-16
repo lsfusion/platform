57e4870552cd4c40969af1e5431acb6a922eccb3
Alexey Buymistrov <alexeybyimistrov@gmail.com>
2023-11-16 14:09:41 +0300

    Support user attributes in LDAP / OAuth authorizations (closes #1008)
    All possible attributes after successful authorisation will be stored in Authentication.attributes(Contact, STRING)

--- api/src/main/java/lsfusion/interop/connection/authentication/OAuth2Authentication.java
+++ api/src/main/java/lsfusion/interop/connection/authentication/OAuth2Authentication.java
@@ -1,8 +1,6 @@
 package lsfusion.interop.connection.authentication;
 
 import lsfusion.base.Pair;
-import java.util.Arrays;
-import java.util.List;
 import java.util.Map;
 
 public class OAuth2Authentication extends Authentication {
@@ -12,48 +10,37 @@ public class OAuth2Authentication extends Authentication {
     private static final String YANDEX_EMAIL = "default_email";
 
     private final String authSecret;
-    private String email;
+    private final String email;
     private String firstName;
     private String lastName;
-    private Pair<String, String> names;
+    private final Map<String, Object> attributes;
 
     public OAuth2Authentication(String login, String authSecret, Map<String, Object> userInfo) {
         super(login);
         this.authSecret = authSecret;
-        fillUserInfo(userInfo);
-    }
+        this.attributes = userInfo;
 
-    private void fillUserInfo(Map<String, Object> userInfo) {
         String email = (String) userInfo.get(EMAIL_KEY);
         String name = (String) userInfo.get(NAME_KEY);
         String yandex_email = (String) userInfo.get(YANDEX_EMAIL);
         String yandex_name = (String) userInfo.get(YANDEX_NAME);
 
-        if (email != null) {
-            this.email = email;
-        } else if (yandex_email != null){
-            this.email = yandex_email;
-        }
+        this.email = email != null ? email : yandex_email;
 
-        if (name != null) {
-            names = splitName(name);
-        } else if (yandex_name != null) {
-            names = splitName(yandex_name);
-        }
-
-        if (names != null){
+        Pair<String, String> names = name != null ? splitName(name) : yandex_name != null ? splitName(yandex_name) : null;
+        if (names != null) {
             this.firstName = names.first;
             this.lastName = names.second;
         }
     }
 
-    private Pair<String, String> splitName(String name){
-        List<String> names = Arrays.asList(name.split(" "));
-        if (names.size() > 1){
-            return new Pair<>(names.get(0), names.get(1));
-        } else {
-            return new Pair<>(names.get(0), null);
-        }
+    private Pair<String, String> splitName(String name) {
+        String[] names = name.split(" ");
+        return new Pair<>(names[0], names.length > 1 ? names[1] : null);
+    }
+
+    public Map<String, Object> getAttributes() {
+        return attributes;
     }
 
     public String getAuthSecret() {
