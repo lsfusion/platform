0e3a71f0e8d79fc64a33baa9328cd4a1acd3e0c7
ichavchavadze <filipchyk.andrei@gmail.com>
2024-01-26 14:49:34 +0100

    WebSocket (not working newSession)

---
+++ server/src/main/java/lsfusion/server/physics/dev/integration/external/to/net/websocket/SendMessageAction.java
@@ -0,0 +1,42 @@
+package lsfusion.server.physics.dev.integration.external.to.net.websocket;
+
+import com.google.common.base.Throwables;
+import lsfusion.server.data.sql.exception.SQLHandledException;
+import lsfusion.server.data.value.DataObject;
+import lsfusion.server.language.ScriptingErrorLog;
+import lsfusion.server.language.ScriptingLogicsModule;
+import lsfusion.server.logics.action.controller.context.ExecutionContext;
+import lsfusion.server.logics.classes.ValueClass;
+import lsfusion.server.logics.property.classes.ClassPropertyInterface;
+import org.java_websocket.WebSocket;
+
+import java.sql.SQLException;
+import java.util.Iterator;
+
+public class SendMessageAction extends WebSocketAction {
+    private final ClassPropertyInterface webSocketClientInterface;
+
+    public SendMessageAction(ScriptingLogicsModule LM, ValueClass... classes) {
+        super(LM, classes);
+
+        Iterator<ClassPropertyInterface> i = getOrderInterfaces().iterator();
+        webSocketClientInterface = i.next();
+    }
+
+    @Override
+    public void executeInternal(ExecutionContext<ClassPropertyInterface> context) throws SQLException, SQLHandledException {
+        DataObject clientObject = context.getDataKeyValue(webSocketClientInterface);
+
+        try {
+            String clientHost = (String) findProperty("id[WebSocketClient]").read(context, clientObject);
+            String message = (String) findProperty("message[WebSocketClient]").read(context, clientObject);
+
+            WebSocket connection = getConnection(clientHost);
+
+            connection.send(message);
+
+        } catch (ScriptingErrorLog.SemanticErrorException e) {
+            throw Throwables.propagate(e);
+        }
+    }
+}
