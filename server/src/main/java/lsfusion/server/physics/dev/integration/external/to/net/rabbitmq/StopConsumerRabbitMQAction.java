b66241d7bd823482ebe11f737ed405e600ecac6d
ichavchavadze <filipchyk.andrei@gmail.com>
2024-01-30 19:51:23 +0100

    rabbitMQ refactoring as MonitorServer

--- server/src/main/java/lsfusion/server/physics/dev/integration/external/to/net/rabbitmq/StopConsumerRabbitMQAction.java
+++ server/src/main/java/lsfusion/server/physics/dev/integration/external/to/net/rabbitmq/StopConsumerRabbitMQAction.java
@@ -1,33 +1,18 @@
 package lsfusion.server.physics.dev.integration.external.to.net.rabbitmq;
 
 import com.google.common.base.Throwables;
-import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.Connection;
-import com.rabbitmq.client.ConnectionFactory;
-import com.rabbitmq.client.DeliverCallback;
-import com.rabbitmq.client.impl.DefaultCredentialsProvider;
-import lsfusion.base.Pair;
-import lsfusion.server.base.controller.manager.MonitorServer;
-import lsfusion.server.base.controller.thread.ThreadLocalContext;
 import lsfusion.server.data.sql.exception.SQLHandledException;
 import lsfusion.server.data.value.DataObject;
-import lsfusion.server.language.ScriptingErrorLog;
 import lsfusion.server.language.ScriptingLogicsModule;
-import lsfusion.server.logics.LogicsInstance;
 import lsfusion.server.logics.action.controller.context.ExecutionContext;
 import lsfusion.server.logics.classes.ValueClass;
 import lsfusion.server.logics.property.classes.ClassPropertyInterface;
 import lsfusion.server.physics.dev.integration.internal.to.InternalAction;
 
-import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 import java.sql.SQLException;
-import java.util.HashMap;
 import java.util.Iterator;
-import java.util.Map;
-import java.util.concurrent.TimeoutException;
 
-public class StopConsumerRabbitMQAction extends RabbitMQAction {
+public class StopConsumerRabbitMQAction extends InternalAction {
     private final ClassPropertyInterface channelInterface;
 
     public StopConsumerRabbitMQAction(ScriptingLogicsModule LM, ValueClass... classes) {
@@ -39,7 +24,6 @@ public class StopConsumerRabbitMQAction extends RabbitMQAction {
 
     @Override
     public void executeInternal(ExecutionContext<ClassPropertyInterface> context) throws SQLException, SQLHandledException {
-        super.executeInternal(context);
         try {
 
             DataObject channelObject = context.getDataKeyValue(channelInterface);
@@ -47,7 +31,7 @@ public class StopConsumerRabbitMQAction extends RabbitMQAction {
             String host = (String) findProperty("host[Channel]").read(context, channelObject); //localhost
             String queue = (String) findProperty("queue[Channel]").read(context, channelObject); //"hello";
 
-            consumerMonitorServer.stopConsume(host, queue);
+            context.getLogicsInstance().getRabbitMQServer().stopConsume(host, queue);
 
         } catch (Exception e) {
             throw Throwables.propagate(e);
