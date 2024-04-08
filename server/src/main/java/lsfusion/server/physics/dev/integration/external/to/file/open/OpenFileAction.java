37978b538ba73bcd160648c4fb36f2a09c8df66d
alex-byi <alexeybyimistrov@gmail.com>
2024-04-08 12:34:38 +0300

    openFile

--- server/src/main/java/lsfusion/server/physics/dev/integration/external/to/file/open/OpenFileAction.java
+++ server/src/main/java/lsfusion/server/physics/dev/integration/external/to/file/open/OpenFileAction.java
@@ -1,15 +1,11 @@
 package lsfusion.server.physics.dev.integration.external.to.file.open;
 
 import com.google.common.base.Throwables;
-import lsfusion.base.BaseUtils;
-import lsfusion.base.file.RawFileData;
+import lsfusion.base.file.NamedFileData;
 import lsfusion.interop.action.OpenFileClientAction;
-import lsfusion.server.data.value.DataObject;
-import lsfusion.server.data.value.ObjectValue;
 import lsfusion.server.logics.BaseLogicsModule;
 import lsfusion.server.logics.action.controller.context.ExecutionContext;
 import lsfusion.server.logics.classes.ValueClass;
-import lsfusion.server.logics.classes.data.file.StaticFormatFileClass;
 import lsfusion.server.logics.property.classes.ClassPropertyInterface;
 import lsfusion.server.physics.dev.integration.internal.to.InternalAction;
 
@@ -17,8 +13,6 @@ import java.util.Iterator;
 
 public class OpenFileAction extends InternalAction {
     private final ClassPropertyInterface sourceInterface;
-    private final ClassPropertyInterface nameInterface;
-    private final ClassPropertyInterface extensionInterface;
     private final ClassPropertyInterface noWaitInterface;
 
     public OpenFileAction(BaseLogicsModule LM, ValueClass... classes) {
@@ -26,22 +20,14 @@ public class OpenFileAction extends InternalAction {
 
         Iterator<ClassPropertyInterface> i = getOrderInterfaces().iterator();
         sourceInterface = i.next();
-        nameInterface = i.next();
-        extensionInterface = i.next();
         noWaitInterface = i.next();
     }
 
     public void executeInternal(ExecutionContext<ClassPropertyInterface> context) {
         try {
-            ObjectValue sourceObject = context.getKeyValue(sourceInterface);
-            RawFileData source = (RawFileData) sourceObject.getValue();
-
-            if (sourceObject instanceof DataObject && source != null) {
-                String extension = (String) context.getKeyValue(extensionInterface).getValue();
-
-                OpenFileClientAction action = new OpenFileClientAction(source,
-                        (String) context.getKeyValue(nameInterface).getValue(),
-                        extension != null ? extension : BaseUtils.firstWord(((StaticFormatFileClass) ((DataObject) sourceObject).objectClass).getOpenExtension(source), ","));
+            NamedFileData source = (NamedFileData) context.getKeyValue(sourceInterface).getValue();
+            if (source != null) {
+                OpenFileClientAction action = new OpenFileClientAction(source.getRawFile(), checkParamEmpty(source.getName()), checkParamEmpty(source.getExtension()));
 
                 if (context.getKeyValue(noWaitInterface).getValue() != null)
                     context.delayUserInteraction(action);
@@ -52,6 +38,10 @@ public class OpenFileAction extends InternalAction {
         } catch (Exception e) {
             throw Throwables.propagate(e);
         }
+    }
+
+    private String checkParamEmpty(String param) {
+        return param != null && !param.equals("empty") ? param : null;
     }
 
     @Override
