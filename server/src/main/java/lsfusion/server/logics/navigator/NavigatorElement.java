86f7781490e3187cf69d48223c177080bf84a8e8
ichavchavadze <filipchyk.andrei@gmail.com>
2024-05-21 10:22:29 +0200

    Revert "Dumb for logo"
    This reverts commit 0fd2a5e74cd52b747bdcd3c373fb55bec5556e13.

--- server/src/main/java/lsfusion/server/logics/navigator/NavigatorElement.java
+++ server/src/main/java/lsfusion/server/logics/navigator/NavigatorElement.java
@@ -1,7 +1,6 @@
 package lsfusion.server.logics.navigator;
 
 import lsfusion.base.col.MapFact;
-import lsfusion.base.col.SetFact;
 import lsfusion.base.col.interfaces.immutable.ImList;
 import lsfusion.base.col.interfaces.immutable.ImOrderMap;
 import lsfusion.server.base.AppServerImage;
@@ -14,15 +13,11 @@ import lsfusion.server.base.version.Version;
 import lsfusion.server.base.version.interfaces.NFComplexOrderSet;
 import lsfusion.server.base.version.interfaces.NFProperty;
 import lsfusion.server.logics.BaseLogicsModule;
-import lsfusion.server.logics.action.SystemAction;
-import lsfusion.server.logics.action.controller.context.ExecutionContext;
-import lsfusion.server.logics.action.flow.FlowResult;
 import lsfusion.server.logics.form.interactive.action.async.AsyncExec;
 import lsfusion.server.logics.form.interactive.action.async.AsyncSerializer;
 import lsfusion.server.logics.form.interactive.controller.remote.serialization.ConnectionContext;
 import lsfusion.server.logics.navigator.window.NavigatorWindow;
 import lsfusion.server.logics.property.Property;
-import lsfusion.server.logics.property.oraction.PropertyInterface;
 import lsfusion.server.physics.admin.Settings;
 import lsfusion.server.physics.admin.authentication.security.policy.SecurityPolicy;
 import lsfusion.server.physics.dev.debug.DebugInfo;
@@ -174,11 +169,7 @@ public abstract class NavigatorElement {
             if(securityPolicy.checkNavigatorPermission(this)) {
                 return singletonOrder(this, Collections.emptyList());
             } else {
-                if(canonicalName.equals("SystemEvents.logoAction")) {
-                    return getDumbSystemChildrenMap();
-                } else {
-                    return MapFact.EMPTYORDER();
-                }
+                return MapFact.EMPTYORDER();
             }
         }
 
@@ -198,18 +189,6 @@ public abstract class NavigatorElement {
         }
 
         return MapFact.EMPTYORDER();
-    }
-
-    private ImOrderMap<NavigatorElement, List<String>> getDumbSystemChildrenMap() {
-        NavigatorAction emptyAction = new NavigatorAction(new SystemAction(this.caption.get(), SetFact.EMPTYORDER()) {
-            @Override
-            protected FlowResult aspectExecute(ExecutionContext<PropertyInterface> context) {
-                return FlowResult.FINISH;
-            }
-        }, canonicalName, ((NavigatorAction)this).getForm());
-        emptyAction.caption = this.caption;
-        emptyAction.window = this.window;
-        return singletonOrder(emptyAction, Collections.emptyList());
     }
 
     public NavigatorElement getChildElement(String elementCanonicalName) {
