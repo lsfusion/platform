f697bfd01c857ca3b10306d4c22f2109cc90c1d2
ichavchavadze <filipchyk.andrei@gmail.com>
2024-05-31 11:37:52 +0200

    FormRefresh button moved if !toolbarTopLeft

--- server/src/main/java/lsfusion/server/logics/form/interactive/design/auto/DefaultFormView.java
+++ server/src/main/java/lsfusion/server/logics/form/interactive/design/auto/DefaultFormView.java
@@ -264,18 +264,21 @@ public class DefaultFormView extends FormView {
         toolbarLeftContainer.add(logMessage, version); // otherwise it will go to OBJECTS container which has types COLUMNS and this type doesnt respect SHOWIF
 
         boolean toolbarTopLeft = Settings.get().isToolbarTopLeft();
-        if(toolbarTopLeft) {
+        if (toolbarTopLeft) {
             toolbarRightContainer.add(okFunction, version);
             toolbarRightContainer.add(closeFunction, version);
+        } else {
+            toolbarRightContainer.add(refreshFunction, version);
         }
         toolbarRightContainer.add(dropFunction, version);
         toolbarRightContainer.add(applyFunction, version);
         toolbarRightContainer.add(cancelFunction, version);
-        if(!toolbarTopLeft) {
+        if (toolbarTopLeft) {
+            toolbarRightContainer.add(refreshFunction, version);
+        } else {
             toolbarRightContainer.add(okFunction, version);
             toolbarRightContainer.add(closeFunction, version);
         }
-        toolbarRightContainer.add(refreshFunction, version);
 
         popupContainer.add(editFunction, version);
         popupContainer.add(shareFunction, version);
