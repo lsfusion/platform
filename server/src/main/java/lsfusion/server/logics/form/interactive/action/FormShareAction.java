d8f4207e4ac926eb70c9f40c2f328eea14258d91
ichavchavadze <filipchyk.andrei@gmail.com>
2024-06-04 12:39:14 +0200

    FormShare hasFlow fix

--- server/src/main/java/lsfusion/server/logics/form/interactive/action/FormShareAction.java
+++ server/src/main/java/lsfusion/server/logics/form/interactive/action/FormShareAction.java
@@ -3,6 +3,7 @@ package lsfusion.server.logics.form.interactive.action;
 import lsfusion.server.data.sql.exception.SQLHandledException;
 import lsfusion.server.logics.BaseLogicsModule;
 import lsfusion.server.logics.action.controller.context.ExecutionContext;
+import lsfusion.server.logics.action.flow.ChangeFlowType;
 import lsfusion.server.logics.classes.ValueClass;
 import lsfusion.server.logics.form.interactive.instance.FormInstance;
 import lsfusion.server.logics.property.classes.ClassPropertyInterface;
@@ -20,5 +21,12 @@ public class FormShareAction extends InternalAction {
     protected void executeInternal(ExecutionContext<ClassPropertyInterface> context) throws SQLException, SQLHandledException {
         FormInstance form = context.getFormInstance(false, true);
         form.instanceFactory.getInstance(form.entity.getShareAction()).execute(context.getEnv(), context.stack, null, null, form);
+    }
+
+    @Override
+    public boolean hasFlow(ChangeFlowType type) {
+        if(type.isChange())
+            return false;
+        return super.hasFlow(type);
     }
 }
