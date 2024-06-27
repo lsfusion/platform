ab7778529b5d5c65d269838e38c38f3a081df720
pavel-miniutka <pavel.miniutka@gmail.com>
2024-06-27 10:13:14 +0300

    Scrollable message dialog

--- web-client/src/main/java/lsfusion/gwt/client/base/view/DialogBoxHelper.java
+++ web-client/src/main/java/lsfusion/gwt/client/base/view/DialogBoxHelper.java
@@ -90,6 +90,8 @@ public class DialogBoxHelper {
 
         private MessageBox(String caption, Widget contents, int timeout, final CloseCallback closeCallback, final OptionType activeOption, OptionType... options) {
             super(caption, false, ModalWindowSize.FIT_CONTENT);
+            
+            dialog.addStyleName("modal-dialog-scrollable");
 
             this.closeCallback = closeCallback;
 
