8031b512cfc50eb99bebe5deeacc935b117c8e6d
Alexey Buymistrov <alexeybyimistrov@gmail.com>
2024-06-28 14:16:03 +0300

    air datepicker

--- web-client/src/main/java/lsfusion/gwt/client/form/property/cell/classes/controller/YearCellEditor.java
+++ web-client/src/main/java/lsfusion/gwt/client/form/property/cell/classes/controller/YearCellEditor.java
@@ -22,17 +22,15 @@ public class YearCellEditor extends IntegralCellEditor {
         super.start(handler, parent, renderContext, notFocusable, oldValue);
 
         if (started) {
-            openYearPicker(inputElement, nvl(PValue.getIntegerValue(oldValue), 0));
+            openYearPicker(inputElement, parent, nvl(PValue.getIntegerValue(oldValue), 0));
             GwtClientUtils.addDropDownPartner(parent, getYearPickerContainer(parent));
         }
     }
 
     @Override
     public void stop(Element parent, boolean cancel, boolean blurred) {
-        if (started) {
-            getYearPickerContainer(parent).removeFromParent();
-            destroyYearPicker(inputElement);
-        }
+        if (started)
+            destroyYearPicker(parent);
         super.stop(parent, cancel, blurred);
     }
 
@@ -40,31 +38,28 @@ public class YearCellEditor extends IntegralCellEditor {
         commit(parent);
     }
 
-    protected native void openYearPicker(Element inputElement, int initYear)/*-{
-        window.$ = $wnd.jQuery;
+    protected native void openYearPicker(Element inputElement, Element parent, int initYear)/*-{
         var thisObj = this;
-
-        var yearpicker = $(inputElement).data("yearpicker");
-        if(!yearpicker) {
-            $(inputElement).yearpicker({
-                onHide: function (value) {
-                    if (value != null && value.toString() !== this.year) {
-                        thisObj.@YearCellEditor::pickerApply(*)(parent);
-                    }
-                }, year: initYear
-            });
-
-            yearpicker = $(inputElement).data("yearpicker");
-        }
-        yearpicker.showView();
+        parent.picker = new $wnd.AirDatepicker(inputElement, {
+            view: 'years', // displaying the years of one decade
+            minView: 'years', // The minimum possible representation of the calendar. It is used, for example, when you need to provide only a choice of the year.
+            visible: true, // Shows the calendar immediately after initialization.
+            startDate: initYear !== 0 ? new Date().setFullYear(initYear) : new Date(),
+            dateFormat: function (date) {
+                return date.getFullYear(); // to return a number, not a Date object
+            },
+            onHide: function(isFinished) {
+                if (isFinished) // isFinished — animation completion indicator
+                    thisObj.@YearCellEditor::pickerApply(*)(parent);
+            }
+        });
     }-*/;
 
-    protected native void destroyYearPicker(Element inputElement)/*-{
-        window.$ = $wnd.jQuery;
-        $(inputElement).removeData("yearpicker");
+    protected native void destroyYearPicker(Element parent)/*-{
+        parent.picker.destroy();
     }-*/;
 
     protected native Element getYearPickerContainer(Element parent)/*-{
-        return parent.getElementsByClassName("yearpicker-container")[0];
+        return parent.picker.$datepicker;
     }-*/;
 }
