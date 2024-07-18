1cd3a3bcef950c40a845040f63c25445dae3ff7e
ichavchavadze <filipchyk.andrei@gmail.com>
2024-07-18 09:59:21 +0200

    Use jserialcomm lib instead of jssc to connect to com-port (by option) (closes #1196)

--- server/src/main/java/lsfusion/server/physics/dev/integration/external/to/equ/com/WriteToComPortAction.java
+++ server/src/main/java/lsfusion/server/physics/dev/integration/external/to/equ/com/WriteToComPortAction.java
@@ -2,7 +2,6 @@ package lsfusion.server.physics.dev.integration.external.to.equ.com;
 
 import lsfusion.base.file.FileData;
 import lsfusion.interop.action.MessageClientAction;
-import lsfusion.server.data.sql.exception.SQLHandledException;
 import lsfusion.server.language.ScriptingLogicsModule;
 import lsfusion.server.logics.action.controller.context.ExecutionContext;
 import lsfusion.server.logics.classes.ValueClass;
@@ -10,13 +9,13 @@ import lsfusion.server.logics.property.classes.ClassPropertyInterface;
 import lsfusion.server.physics.dev.integration.external.to.equ.com.client.WriteToComPortClientAction;
 import lsfusion.server.physics.dev.integration.internal.to.InternalAction;
 
-import java.sql.SQLException;
 import java.util.Iterator;
 
 public class WriteToComPortAction extends InternalAction {
     private final ClassPropertyInterface fileInterface;
     private final ClassPropertyInterface baudRateInterface;
     private final ClassPropertyInterface comPortInterface;
+    private final ClassPropertyInterface useJSerialCommInterface;
 
     public WriteToComPortAction(ScriptingLogicsModule LM, ValueClass... classes) {
         super(LM, classes);
@@ -25,15 +24,17 @@ public class WriteToComPortAction extends InternalAction {
         fileInterface = i.next();
         baudRateInterface = i.next();
         comPortInterface = i.next();
+        useJSerialCommInterface = i.next();
     }
 
     public void executeInternal(ExecutionContext<ClassPropertyInterface> context) {
         FileData file = (FileData) context.getKeyValue(fileInterface).getValue();
         Integer baudRate = (Integer) context.getKeyValue(baudRateInterface).getValue();
         Integer comPort = (Integer) context.getKeyValue(comPortInterface).getValue();
+        boolean useJSerialComm = context.getKeyValue(useJSerialCommInterface).getValue() != null;
 
         if(file != null && baudRate != null && comPort != null) {
-            String result = (String) context.requestUserInteraction(new WriteToComPortClientAction(file.getRawFile(), baudRate, comPort));
+            String result = (String) context.requestUserInteraction(new WriteToComPortClientAction(file.getRawFile(), baudRate, comPort, useJSerialComm));
             if (result != null) {
                 context.delayUserInteraction(new MessageClientAction(result, "Ошибка"));
             }
