850b918b0befe752d3ec6a73d61755cf8df3d78a
Alexey Buymistrov <alexeybyimistrov@gmail.com>
2024-08-21 15:44:26 +0300

    permanent redirect to getResource

--- web-client/src/main/java/lsfusion/http/controller/file/DownloadFileRequestHandler.java
+++ web-client/src/main/java/lsfusion/http/controller/file/DownloadFileRequestHandler.java
@@ -2,10 +2,8 @@ package lsfusion.http.controller.file;
 
 import com.google.common.io.ByteStreams;
 import lsfusion.base.BaseUtils;
-import lsfusion.gwt.client.base.exception.AppServerNotAvailableDispatchException;
 import lsfusion.gwt.server.FileUtils;
 import lsfusion.http.controller.MainController;
-import lsfusion.http.provider.logics.LogicsProvider;
 import lsfusion.interop.session.ExternalUtils;
 import org.apache.commons.httpclient.util.URIUtil;
 import org.springframework.web.HttpRequestHandler;
@@ -13,23 +11,18 @@ import org.springframework.web.HttpRequestHandler;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.InputStream;
 import java.nio.charset.Charset;
 
 public class DownloadFileRequestHandler implements HttpRequestHandler {
 
-    private final LogicsProvider logicsProvider;
-    public DownloadFileRequestHandler(LogicsProvider logicsProvider) {
-        this.logicsProvider=logicsProvider;
-    }
+    public DownloadFileRequestHandler() {}
 
     @Override
     public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws IOException {
 
         String pathInfo = request.getPathInfo();
 
-        boolean staticFile = false;
-        boolean file = false;
+        boolean staticFile;
         String prefix;
         if(pathInfo.startsWith(prefix = "/" + FileUtils.STATIC_PATH + "/"))
             staticFile = true;
@@ -37,13 +30,11 @@ public class DownloadFileRequestHandler implements HttpRequestHandler {
             staticFile = false;
         else if(pathInfo.startsWith(prefix = "/" + FileUtils.DEV_PATH + "/"))
             staticFile = true;
-        else if(pathInfo.startsWith(prefix = "/" + FileUtils.FILE_PATH + "/"))
-            file = true;
         else {
-            response.sendRedirect(MainController.getURLPreservingParameters("/exec?action=getCachedResource&p=" + pathInfo.replaceFirst("/", ""), null, request));
+            response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+            response.setHeader("Location", MainController.getURLPreservingParameters("/exec?action=getResource&p=" + pathInfo.replaceFirst("/", ""), null, request));
             return;
         }
-
         String fileName = pathInfo.substring(prefix.length());
 
         String extension = BaseUtils.getFileExtension(fileName);
@@ -58,27 +49,14 @@ public class DownloadFileRequestHandler implements HttpRequestHandler {
         response.setContentType(ExternalUtils.getContentType(extension, charset).toString());
         //inline = open in browser, attachment = download
         response.addHeader("Content-Disposition", "inline; filename*=" + charset.name() + "''" + URIUtil.encodeQuery(getFileName(displayName, extension)));
-        // expiration will be set in urlRewrite.xml /downloadFile (just to have it at one place)
+        // expiration will be set in urlRewrite.xml /file (just to have it at one place)
 
         // in theory e-tag and last modified may be send but since we're using "version" it's not that necessary
 
-        if (file) {
-            try {
-                String finalFileName = fileName;
-                fillResponse(logicsProvider.runRequest(request, (sessionObject, retry) ->
-                        sessionObject.remoteLogics.getFile(finalFileName)).getInputStream(), response);
-            } catch (AppServerNotAvailableDispatchException e) {
-                throw new RuntimeException(e);
-            }
-        } else {
-            // it seems that the browser might resend the request (for concurrent reading or whatever)
-            FileUtils.readFile(FileUtils.APP_DOWNLOAD_FOLDER_PATH, fileName, !staticFile, true,
-                    inStream -> fillResponse(inStream, response));
-        }
-    }
-
-    private void fillResponse(InputStream inputStream, HttpServletResponse response) throws IOException {
-        ByteStreams.copy(inputStream, response.getOutputStream());
+        // it seems that the browser might resend the request (for concurrent reading or whatever)
+        FileUtils.readFile(FileUtils.APP_DOWNLOAD_FOLDER_PATH, fileName, !staticFile, true, inStream -> {
+            ByteStreams.copy(inStream, response.getOutputStream());
+        });
     }
 
     private String getFileName(String name, String extension) {
