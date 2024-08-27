7b5bdc155a5284e8acad4d5487bb49bf73d0fe8c
ichavchavadze <filipchyk.andrei@gmail.com>
2024-08-27 11:55:01 +0200

    refactoring

--- server/src/main/java/lsfusion/server/logics/action/flow/ForAction.java
+++ server/src/main/java/lsfusion/server/logics/action/flow/ForAction.java
@@ -185,18 +185,13 @@ public class ForAction<I extends PropertyInterface> extends ExtendContextAction<
                     ExecutionStackAspect.popProgressStackItem(stackItem);
                     stackItem = ExecutionStackAspect.pushProgressStackItem(progressCaption, i + 1, rowUpdate.rows.size());
 
-                    boolean isLast = i == size - 1;
-                    FlowResult actionResult = executeFor(overrideMoreSessionUsages && (recursive || !isLast) // is not last
+                    FlowResult actionResult = executeFor(overrideMoreSessionUsages && (recursive || i < size - 1) // is not last
                                                         ? context.override(true): context, newValues);
-                    if (actionResult != FlowResult.FINISH) {
+                    if (actionResult != FlowResult.FINISH && actionResult != FlowResult.CONTINUE) {
                         if (actionResult != FlowResult.BREAK) {
                             result = actionResult;
                         }
-                        if (actionResult != FlowResult.CONTINUE) {
-                            break RECURSIVE;
-                        } else if (isLast) {
-                            result = FlowResult.FINISH;
-                        }
+                        break RECURSIVE;
                     }
                 }
             } finally {
