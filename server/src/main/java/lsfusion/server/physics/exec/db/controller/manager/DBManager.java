53936aff38933dc65b3e11d9094b53b76174d52a
AlexKirkouski <alex.kirkouski@gmail.com>
2024-10-05 16:30:18 +0100

    Bug in async values cache mechanism (closes #1289)

--- server/src/main/java/lsfusion/server/physics/exec/db/controller/manager/DBManager.java
+++ server/src/main/java/lsfusion/server/physics/exec/db/controller/manager/DBManager.java
@@ -1117,6 +1117,7 @@ public class DBManager extends LogicsManager implements InitializingBean {
         ImSet<Property> flushedChanges;
         synchronized (changesListLock) {
             flushedChanges = mChanges.immutable();
+            mChanges = SetFact.mSet();
         }
         if(flushedChanges.isEmpty())
             return;
