7932c25a32bc9e2696c6419d184e4541b0946840
ichavchavadze <filipchyk.andrei@gmail.com>
2025-02-01 20:44:32 +0100

    zip4j; ReadMultipleResourceAction; copyReportResources and loadFontExtensions refactoring

--- server/src/main/java/lsfusion/server/physics/dev/integration/external/to/file/report/CopyReportResourcesClientAction.java
+++ server/src/main/java/lsfusion/server/physics/dev/integration/external/to/file/report/CopyReportResourcesClientAction.java
@@ -4,22 +4,20 @@ import com.google.common.base.Throwables;
 import lsfusion.base.file.FileData;
 import lsfusion.interop.action.ClientAction;
 import lsfusion.interop.action.ClientActionDispatcher;
+import net.lingala.zip4j.ZipFile;
 import net.sf.jasperreports.engine.DefaultJasperReportsContext;
 import net.sf.jasperreports.engine.fonts.FontExtensionsCollector;
 import net.sf.jasperreports.engine.fonts.SimpleFontExtensionHelper;
+import org.apache.commons.io.FileUtils;
 
 import java.io.File;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.net.URL;
 import java.net.URLClassLoader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.stream.Collectors;
 import java.util.stream.Stream;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipInputStream;
 
 public class CopyReportResourcesClientAction implements ClientAction {
     public final FileData zipFile;
@@ -41,48 +39,16 @@ public class CopyReportResourcesClientAction implements ClientAction {
                     loadFontExtensions();
                 return exists;
             } else {
+                FileUtils.cleanDirectory(jasperFontsTempDir);
                 File zip = new File(jasperFontsTempDir, md5);
                 zipFile.getRawFile().write(zip);
-                unpackZip(zip);
+                try(ZipFile zf = new ZipFile(zip)) {
+                    zf.extractAll(jasperFontsTempDir.getAbsolutePath());
+                }
                 loadFontExtensions();
                 return null;
             }
         } catch (Exception e) {
-            throw Throwables.propagate(e);
-        }
-    }
-
-    private void unpackZip(File zip) {
-        try {
-            byte[] buffer = new byte[1024];
-            if (zip.exists()) {
-                ZipInputStream inputStream = new ZipInputStream(Files.newInputStream(zip.toPath()));
-
-                ZipEntry ze = inputStream.getNextEntry();
-                while (ze != null) {
-                    String filePath = ze.getName();
-
-                    String[] splitted = filePath.split("/");
-                    if (splitted.length > 1) {
-                        String path = "";
-                        for (int i = 0; i < splitted.length - 1; i++) {
-                            path += "/" + splitted[i];
-                            new File(jasperFontsTempDir.getPath() + path).mkdir();
-                        }
-                    }
-                    FileOutputStream outputStream = new FileOutputStream(jasperFontsTempDir.getPath() + "/" + filePath);
-                    int len;
-                    while ((len = inputStream.read(buffer)) > 0) {
-                        outputStream.write(buffer, 0, len);
-                    }
-                    outputStream.close();
-
-                    ze = inputStream.getNextEntry();
-                }
-                inputStream.closeEntry();
-                inputStream.close();
-            }
-        } catch (IOException e) {
             throw Throwables.propagate(e);
         }
     }
@@ -93,13 +59,8 @@ public class CopyReportResourcesClientAction implements ClientAction {
         DefaultJasperReportsContext context = DefaultJasperReportsContext.getInstance();
         FontExtensionsCollector extensionsCollector = new FontExtensionsCollector();
 
-        java.nio.file.Path urlPath = Paths.get(jasperFontsTempDir.getPath());
-        if (Files.exists(urlPath)) {
-            try (Stream<Path> pathStream = (Files.walk(urlPath))) {
-                for (Path path : pathStream.filter(path -> path.toString().endsWith(".xml")).collect(Collectors.toList())) {
-                    fontExtensionHelper.loadFontExtensions(context, path.toString(), extensionsCollector);
-                }
-            }
+        try (Stream<Path> walkStream = Files.walk(Paths.get(jasperFontsTempDir.getPath()))) {
+            walkStream.filter(path -> path.toString().endsWith(".xml")).forEach(path -> fontExtensionHelper.loadFontExtensions(context, path.toString(), extensionsCollector));
         }
     }
 }
