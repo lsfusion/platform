5e6ecfce4c4fb508452cc307b74c5c04a7ad3d0b
Aliaksei Kirkouski <crushby@gmail.com>
2025-03-07 13:40:28 +0300

    Upgraded frappe-gantt to 1.0.3

--- server/src/main/resources/web/objects/gantt/custom_frappe-gantt.js
+++ server/src/main/resources/web/objects/gantt/custom_frappe-gantt.js
@@ -1,39 +1,15 @@
-function findGanttElementById(element, id) {
-    return $(element).find("[data-id='" + id + "']");
-}
-
-function gantt_quarter_day() {
-    return gantt({ view_mode: 'Quarter Day' });
-}
-function gantt_half_day() {
-    return gantt({ view_mode: 'Half Day' });
-}
-function gantt_day() {
-    return gantt({ view_mode: 'Day' });
-}
-function gantt_week() {
-    return gantt({ view_mode: 'Week' });
-}
-function gantt_month() {
-    return gantt({ view_mode: 'Month' });
-}
-function gantt_year() {
-    return gantt({ view_mode: 'Year' });
-}
-
-function gantt(options) {
+function gantt() {
     return {
         render: function (element) {
-            element.style.setProperty("min-height", "0px");
-            element.style.setProperty("flex-shrink", "1");
+            // element.style.setProperty("min-height", "0px");
+            // element.style.setProperty("flex-shrink", "1");
         },
-        update: function (element, controller, list) {
+        update: function (element, controller, list, options) {
             if (list.length > 0) {
                 if (!element.gantt) {
-                    element.gantt = new FrappeGantt(element, list, options);
-                    setTimeout(function() {
-                        element.gantt.set_scroll_position();
-                    }, 0);
+                    options.infinite_padding = false; // doesn't work so far
+                    options.readonly_progress = true; // doesn't work so far
+                    element.gantt = new Gantt(element, list, options);
                 } else {
                     let scrollElement = element.gantt.$container;
                     let topPos = scrollElement.scrollTop;
@@ -52,15 +28,16 @@ function gantt(options) {
                     }
 
                 element.gantt.options.on_date_change = function(task, start, end) {
-                    controller.changeProperties(['start', 'end'], [task, task], [start, end]);
+                    controller.changeProperties(['start', 'end'], [task, task], [start, new Date(end.getTime() + 1000)], true);
                 }
                 element.gantt.options.on_progress_change = function(task, progress) {
                     controller.changeProperty('progress', task, progress);
                 }
-                element.gantt.options.on_click = function(task) {
-                    controller.changeObject(task, false, element.gantt.popup_wrapper);
+                element.gantt.options.popup = function (el, chart) {
+                    let activeTask = $(element).find("[data-id='" + el.task.id + "']");
+                    controller.changeObject(el.task, false, activeTask[0]);
+                    return false;
                 }
-
             }
         }
     }
