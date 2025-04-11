d6f449db6e2820bf41ed9874707d7afb5b43fba8
ichavchavadze <filipchyk.andrei@gmail.com>
2025-04-11 16:16:47 +0200

    Divided CheckCopyReportResources into 2 actions

--- server/src/main/java/lsfusion/server/physics/dev/integration/external/to/file/report/CheckCopyReportResources.java
+++ server/src/main/java/lsfusion/server/physics/dev/integration/external/to/file/report/CheckCopyReportResources.java
@@ -1,8 +1,8 @@
 package lsfusion.server.physics.dev.integration.external.to.file.report;
 
 import com.google.common.base.Throwables;
-import lsfusion.base.file.FileData;
 import lsfusion.server.data.sql.exception.SQLHandledException;
+import lsfusion.server.data.value.DataObject;
 import lsfusion.server.language.ScriptingErrorLog;
 import lsfusion.server.logics.UtilsLogicsModule;
 import lsfusion.server.logics.action.controller.context.ExecutionContext;
@@ -12,9 +12,9 @@ import lsfusion.server.physics.dev.integration.internal.to.InternalAction;
 
 import java.sql.SQLException;
 
-public class CopyReportResources extends InternalAction {
+public class CheckCopyReportResources extends InternalAction {
 
-    public CopyReportResources(UtilsLogicsModule LM, ValueClass... classes) {
+    public CheckCopyReportResources(UtilsLogicsModule LM, ValueClass... classes) {
         super(LM, classes);
     }
 
@@ -22,11 +22,12 @@ public class CopyReportResources extends InternalAction {
     protected void executeInternal(ExecutionContext<ClassPropertyInterface> context) {
         try {
             String logicsName = (String) findProperty("logicsName[]").read(context);
-            FileData zipFile = (FileData) getParam(0, context);
-            String md5 = (String) getParam(1, context);
-            Object result = context.requestUserInteraction(new CopyReportResourcesClientAction(logicsName, zipFile, md5));
-            if (result instanceof String)
-                throw new RuntimeException("Copy report resources error: " + result);
+            String md5 = (String) getParam(0, context);
+            Object result = context.requestUserInteraction(new CopyReportResourcesClientAction(logicsName, null, md5));
+            if (result instanceof Boolean && !(boolean) result)
+                findProperty("needCopyReportResources[]").change(new DataObject(true), context);
+            else if (result instanceof String)
+                throw new RuntimeException("Check copy report resources error: " + result);
         } catch (ScriptingErrorLog.SemanticErrorException | SQLException | SQLHandledException e) {
             throw Throwables.propagate(e);
         }
