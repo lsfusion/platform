01919bc9b1be907a56fa050f1ea4529e92cae53b
ichavchavadze <filipchyk.andrei@gmail.com>
2025-07-07 15:48:56 +0200

    apiVersion

--- api/src/main/java/lsfusion/base/BaseUtils.java
+++ api/src/main/java/lsfusion/base/BaseUtils.java
@@ -62,7 +62,7 @@ public class BaseUtils {
     private static final int STRING_SERIALIZATION_CHUNK_SIZE = 65535/3;
 
     public static Integer getApiVersion() {
-        return 231;
+        return 232;
     }
 
     public static String getPlatformVersion() {
