fbfc1220153f08db77e7b8af55b6c7b83c2f7590
Ilia Chavchavadze <40162373+ichavchavadze@users.noreply.github.com>
2025-07-08 08:23:49 +0200

    Forbid "Group Change" context menu action if has interactive form (but not inside request) (closes #1462)

--- server/src/main/java/lsfusion/server/logics/form/open/interactive/FormInteractiveAction.java
+++ server/src/main/java/lsfusion/server/logics/form/open/interactive/FormInteractiveAction.java
@@ -203,6 +203,8 @@ public class FormInteractiveAction<O extends ObjectSelector> extends FormAction<
             return true;
         if(type == ChangeFlowType.NEEDMORESESSIONUSAGES && syncType == null)
             return true;
+        if(type == ChangeFlowType.NOGROUPCHANGE)
+            return true;
         return super.hasFlow(type, recursiveAbstracts);
     }
 
