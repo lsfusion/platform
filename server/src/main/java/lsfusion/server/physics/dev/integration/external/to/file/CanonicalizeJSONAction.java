a7b3d999a65dfd2d3f8f18e275791ae8c85a52f2
ichavchavadze <filipchyk.andrei@gmail.com>
2025-07-09 17:43:28 +0200

    CanonicalizeJSON: encode unicode parametrized

--- server/src/main/java/lsfusion/server/physics/dev/integration/external/to/file/CanonicalizeJSONAction.java
+++ server/src/main/java/lsfusion/server/physics/dev/integration/external/to/file/CanonicalizeJSONAction.java
@@ -28,33 +28,34 @@ public class CanonicalizeJSONAction extends InternalAction {
     @Override
     protected void executeInternal(ExecutionContext<ClassPropertyInterface> context) {
         try {
-            final RawFileData jsonFile = (RawFileData) context.getSingleDataKeyValue().getValue();
+            final RawFileData jsonFile = (RawFileData) context.getKeys().getValue(0).getValue();
+            boolean encodeUnicode = context.getKeys().getValue(1).getValue() != null;
             ObjectMapper mapper = new ObjectMapper();
-            JsonCanonicalizer jc = new JsonCanonicalizer(mapper.writeValueAsString(escapeAllStrings(mapper.readTree(jsonFile.getBytes()))));
+            JsonCanonicalizer jc = new JsonCanonicalizer(mapper.writeValueAsString(escapeAllStrings(mapper.readTree(jsonFile.getBytes()), encodeUnicode)));
             LM.findProperty("canonicalizedJSON[]").change(new DataObject(getEncodedJson(jc)), context);
         } catch (IOException | ScriptingErrorLog.SemanticErrorException | SQLException | SQLHandledException e) {
             throw Throwables.propagate(e);
         }
     }
 
-    public static JsonNode escapeAllStrings(JsonNode node) {
+    public static JsonNode escapeAllStrings(JsonNode node, boolean encodeUnicode) {
         if (node.isTextual()) {
-            return new TextNode(escapeUnicode(node.asText()));
+            return new TextNode(escapeUnicode(node.asText(), encodeUnicode));
         } else if (node.isObject()) {
             ObjectNode objectNode = (ObjectNode) node;
             ObjectNode result = new ObjectNode(JsonNodeFactory.instance);
             Iterator<Map.Entry<String, JsonNode>> fields = objectNode.fields();
             while (fields.hasNext()) {
                 Map.Entry<String, JsonNode> entry = fields.next();
-                String escapedKey = escapeUnicode(entry.getKey());
-                result.set(escapedKey, escapeAllStrings(entry.getValue()));
+                String escapedKey = escapeUnicode(entry.getKey(), encodeUnicode);
+                result.set(escapedKey, escapeAllStrings(entry.getValue(), encodeUnicode));
             }
             return result;
         } else if (node.isArray()) {
             ArrayNode arrayNode = (ArrayNode) node;
             ArrayNode result = new ArrayNode(JsonNodeFactory.instance);
             for (JsonNode element : arrayNode) {
-                result.add(escapeAllStrings(element));
+                result.add(escapeAllStrings(element, encodeUnicode));
             }
             return result;
         } else {
@@ -62,21 +63,31 @@ public class CanonicalizeJSONAction extends InternalAction {
         }
     }
 
-    private static String escapeUnicode(String input) {
-        StringBuilder sb = new StringBuilder();
-        for (char c : input.toCharArray()) {
-            if (c >= 0x20 && c <= 0x7E) {
-                sb.append(c);
-            } else {
-                sb.append(String.format("\\u%04x", (int) c));
+    private static String escapeUnicode(String input, boolean encodeUnicode) {
+        if (encodeUnicode) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : input.toCharArray()) {
+                if (c >= 0x20 && c <= 0x7E) {
+                    sb.append(c);
+                } else {
+                    sb.append(String.format("\\u%04x", (int) c));
+                }
             }
+            return sb.toString();
+
+        } else {
+            return input;
         }
-        return sb.toString();
     }
 
     private String getEncodedJson(JsonCanonicalizer jc) {
         String encodedString = jc.getEncodedString();
         //replace double slash to single slash in \\uxxxx
         return encodedString.replaceAll("\\\\\\\\u", "\\\\u");
+    }
+
+    @Override
+    protected boolean allowNulls() {
+        return true;
     }
 }
