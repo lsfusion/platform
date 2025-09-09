8ca1564c9469373378b8c2a1e9c7f685df238a7e
Ilia Chavchavadze <40162373+ichavchavadze@users.noreply.github.com>
2025-09-09 12:55:13 +0200

    PrefixSearch: increase rank for exact search (closes #1512)

--- server/src/main/java/lsfusion/server/data/expr/where/classes/data/MatchWhere.java
+++ server/src/main/java/lsfusion/server/data/expr/where/classes/data/MatchWhere.java
@@ -45,12 +45,16 @@ public class MatchWhere extends BinaryWhere<MatchWhere> {
     public static String getPrefixSearchQuery(SQLSyntax syntax, String source, String language, boolean exact) {
         return syntax.getPrefixSearchQuery(exact) + "('" + language + "', " + source + ", '" + Settings.get().getMatchSearchSeparator() + "')";
     }
+    public static String getPrefixSearchLikeQuery(SQLSyntax syntax, String search, String match) {
+        return syntax.getPrefixSearchLikeQuery() + "(" + search + "," + match + ")";
+    }
     public static String getMatch(SQLSyntax syntax, String search, String match, String language, boolean isTSVectorType, boolean isTSQueryType) {
         return (isTSVectorType ? search : getPrefixSearchVector(search, language)) + " @@ " + (isTSQueryType ? match : getPrefixSearchQuery(syntax, match, language));
     }
     public static String getRank(SQLSyntax syntax, String search, String match, String language) {
-        return "ts_rank(" + getPrefixSearchVector(search, language) + "," + getPrefixSearchQuery(syntax, match, language, false) + ") + " +
-                "ts_rank_cd(" + getPrefixSearchVector(search, language) + "," + getPrefixSearchQuery(syntax, match, language, true) + ") * 3";
+         return "ts_rank_cd(" + getPrefixSearchVector(search, language) + "," + getPrefixSearchQuery(syntax, match, language, false) + ") + " +
+                "ts_rank_cd(" + getPrefixSearchVector(search, language) + "," + getPrefixSearchQuery(syntax, match, language, true) + ") * 2 + " +
+                 getPrefixSearchLikeQuery(syntax, search, match);
     }
     public static String getHighlight(SQLSyntax syntax, String search, String match, String language) {
         return "ts_headline('" + language + "'," + search + "," + getPrefixSearchQuery(syntax, match, language) + ")";
