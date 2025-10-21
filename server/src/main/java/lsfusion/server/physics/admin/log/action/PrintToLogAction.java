9f3e3a8e035fd7fa82a583f42b4391cac38a62b3
ichavchavadze <filipchyk.andrei@gmail.com>
2025-10-21 14:08:19 +0200

    Utils.printToLog: customize logs and levels (сloses #1532)

--- server/src/main/java/lsfusion/server/physics/admin/log/action/PrintToLogAction.java
+++ server/src/main/java/lsfusion/server/physics/admin/log/action/PrintToLogAction.java
@@ -1,27 +1,51 @@
 package lsfusion.server.physics.admin.log.action;
 
+import lsfusion.base.BaseUtils;
 import lsfusion.server.logics.UtilsLogicsModule;
 import lsfusion.server.logics.action.controller.context.ExecutionContext;
 import lsfusion.server.logics.classes.ValueClass;
 import lsfusion.server.logics.property.classes.ClassPropertyInterface;
-import lsfusion.server.physics.admin.log.ServerLoggers;
 import lsfusion.server.physics.dev.integration.internal.to.InternalAction;
+import org.apache.log4j.*;
 
-import java.util.Iterator;
-
+import static lsfusion.base.BaseUtils.nvl;
 
 public class PrintToLogAction extends InternalAction {
-    private final ClassPropertyInterface stringInterface;
-
     public PrintToLogAction(UtilsLogicsModule LM, ValueClass... classes) {
         super(LM, classes);
-
-        Iterator<ClassPropertyInterface> i = interfaces.iterator();
-        stringInterface = i.next();
     }
 
     @Override
     protected void executeInternal(ExecutionContext<ClassPropertyInterface> context) {
-        ServerLoggers.systemLogger.info(context.getDataKeyValue(stringInterface).object);
+        String message = (String) getParamValue(0, context).getValue();
+        String name = nvl((String) getParamValue(1, context).getValue(), "system");
+        String level = nvl(((String) getParamValue(2, context).getValue()), "info");
+
+        String loggerName = BaseUtils.capitalize(name) + "Logger";
+        Logger logger = LogManager.exists(loggerName);
+        if(logger == null) {
+            logger = LogManager.getLogger(loggerName);
+            FileAppender fa = new FileAppender();
+            fa.setName(loggerName);
+            fa.setFile("logs/" + name + ".log");
+            fa.setLayout(new EnhancedPatternLayout("%d{DATE} %5p %c{1} - %m%n%throwable{1000}"));
+            fa.setAppend(true);
+            fa.activateOptions();
+            logger.setLevel(Level.ALL);
+            logger.addAppender(fa);
+        }
+
+        switch (level) {
+            case "error":
+                logger.error(message);
+                break;
+            case "warn":
+                logger.warn(message);
+                break;
+            case "info":
+            default:
+                logger.info(message);
+                break;
+        }
     }
 }
