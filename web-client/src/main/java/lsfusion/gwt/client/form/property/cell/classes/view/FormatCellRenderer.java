56018c1a8a60abcc036a29dae6d658de52dea46f
Alexey Buymistrov <alexeybyimistrov@gmail.com>
2025-11-19 15:47:29 +0300

    input number 4

--- web-client/src/main/java/lsfusion/gwt/client/form/property/cell/classes/view/FormatCellRenderer.java
+++ web-client/src/main/java/lsfusion/gwt/client/form/property/cell/classes/view/FormatCellRenderer.java
@@ -1,5 +1,6 @@
 package lsfusion.gwt.client.form.property.cell.classes.view;
 
+import lsfusion.gwt.client.classes.GFullInputType;
 import lsfusion.gwt.client.classes.data.GFormatType;
 import lsfusion.gwt.client.form.property.GPropertyDraw;
 import lsfusion.gwt.client.form.property.PValue;
@@ -11,13 +12,15 @@ public abstract class FormatCellRenderer<T> extends TextBasedCellRenderer {
         return property.getFormatType(rendererType);
     }
 
+    protected boolean isNative(GFullInputType fullInputType) {
+        return false;
+    }
+
     @Override
     public String format(PValue value, RendererType rendererType, String pattern) {
-        // input type number does not support formatting(e.g., spaces).
-        if(rendererType == RendererType.CELL && isTagInput() && getInputType(rendererType).inputType.isNumber())
-            return PValue.getStringValue(value);
-
-        return getFormatType(rendererType).formatString(value, pattern);
+        GFormatType formatType = getFormatType(rendererType);
+        return rendererType == RendererType.CELL && isTagInput() && isNative(getInputType(rendererType)) ?
+                formatType.formatISOString(value) : formatType.formatString(value, pattern);
     }
 
     public FormatCellRenderer(GPropertyDraw property) {
