2be24ee75798e563b095679038795064883bc1e2
ichavchavadze <filipchyk.andrei@gmail.com>
2025-11-26 09:37:36 +0100

    [server] Removed unused imports of MessageClientAction

--- server/src/main/java/lsfusion/server/physics/admin/service/action/OverCalculateStatsMultiThreadAction.java
+++ server/src/main/java/lsfusion/server/physics/admin/service/action/OverCalculateStatsMultiThreadAction.java
@@ -1,6 +1,5 @@
 package lsfusion.server.physics.admin.service.action;
 
-import lsfusion.interop.action.MessageClientAction;
 import lsfusion.server.logics.classes.ValueClass;
 import lsfusion.server.logics.property.controller.init.GroupPropertiesSingleTask;
 import lsfusion.server.physics.admin.service.ServiceLogicsModule;
