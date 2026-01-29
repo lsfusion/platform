e6c01e9cbfa3509facff91d1c7ba0375ab9fb07c
ichavchavadze <filipchyk.andrei@gmail.com>
2026-01-29 15:06:35 +0100

    ExprEqualsJoin: createThis fix

--- server/src/main/java/lsfusion/server/data/expr/join/select/ExprEqualsJoin.java
+++ server/src/main/java/lsfusion/server/data/expr/join/select/ExprEqualsJoin.java
@@ -1,19 +1,6 @@
 package lsfusion.server.data.expr.join.select;
 
-import lsfusion.base.Result;
-import lsfusion.base.col.MapFact;
-import lsfusion.base.col.SetFact;
-import lsfusion.base.col.interfaces.immutable.ImMap;
-import lsfusion.base.col.interfaces.immutable.ImSet;
-import lsfusion.base.mutability.TwinImmutableObject;
-import lsfusion.server.data.caches.AbstractOuterContext;
-import lsfusion.server.data.caches.OuterContext;
-import lsfusion.server.data.caches.hash.HashContext;
 import lsfusion.server.data.expr.BaseExpr;
-import lsfusion.server.data.expr.join.inner.InnerJoins;
-import lsfusion.server.data.expr.join.where.WhereJoin;
-import lsfusion.server.data.stat.*;
-import lsfusion.server.data.translate.MapTranslate;
 
 public class ExprEqualsJoin extends ExprCompareJoin<BaseExpr, ExprEqualsJoin> {
 
@@ -23,6 +10,6 @@ public class ExprEqualsJoin extends ExprCompareJoin<BaseExpr, ExprEqualsJoin> {
 
     @Override
     protected ExprEqualsJoin createThis(BaseExpr expr1, BaseExpr expr2) {
-        return createThis(expr1, expr2);
+        return new ExprEqualsJoin(expr1, expr2);
     }
 }
