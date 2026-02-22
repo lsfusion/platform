3c92f7fbd01cb02e913836b4e7a323097378fe1b
AlexKirkouski <alex.kirkouski@gmail.com>
2026-02-22 14:47:51 +0000

    Reapply "Support ORDER-based optimizations using `ImOrderSet` in GroupJoin and refactor related logic"
    This reverts commit 0ed6a54ff46ef0fbef5dffcd34e2af3ec808540e.

--- server/src/main/java/lsfusion/server/data/translate/MapTranslate.java
+++ server/src/main/java/lsfusion/server/data/translate/MapTranslate.java
@@ -64,6 +64,8 @@ public interface MapTranslate extends MapObject {
 
     ImList<Expr> translate(ImList<Expr> list);
 
+    ImOrderSet<Expr> translate(ImOrderSet<Expr> list);
+
     ImSet<Expr> translate(ImSet<Expr> set);
 
     <K extends Expr> ClassWhere<K> translate(ClassWhere<K> classes);
