49309a36b08f870dab78dd26617d622033c02301
AlexKirkouski <alex.kirkouski@gmail.com>
2026-03-16 20:52:51 +0000

    Small fixes

--- server/src/main/java/lsfusion/server/logics/action/session/LocalNestedType.java
+++ server/src/main/java/lsfusion/server/logics/action/session/LocalNestedType.java
@@ -6,11 +6,14 @@ public enum LocalNestedType {
     public static final LocalNestedType DATACHANGED = null;
     public static final LocalNestedType REQUESTCANCELED = LocalNestedType.ALL;
 
-    public static final LocalNestedType RETURN = null;
+    // need to survive newsession and even apply if called inside APPLY
+    public static final LocalNestedType RETURN = LocalNestedType.ALL;
 
+    // all this props are immediately read after write (so we don't need to nest them)
     public static final LocalNestedType INPUT = null;
     public static final LocalNestedType IMPORT = null;
     public static final LocalNestedType FOR = null;
 
+    // ACTIVE / VIEW / SELECT should survive session operations
     public static final LocalNestedType FORM = LocalNestedType.ALL;
 }
