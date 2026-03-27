141be000acccb5901bf8199fc8de131190a2b26c
Ilia Chavchavadze <40162373+ichavchavadze@users.noreply.github.com>
2026-03-27 08:17:22 +0100

    SQLUtils: setAutoCommit
    (cherry picked from commit 719f5d59b6c19c7f63bf914155fb2730d4633968)

--- server/src/main/java/lsfusion/server/physics/dev/integration/external/to/ExternalDBAction.java
+++ server/src/main/java/lsfusion/server/physics/dev/integration/external/to/ExternalDBAction.java
@@ -5,19 +5,15 @@ import lsfusion.base.col.interfaces.immutable.ImList;
 import lsfusion.server.data.OperationOwner;
 import lsfusion.server.data.sql.exception.SQLHandledException;
 import lsfusion.server.data.sql.syntax.DefaultSQLSyntax;
-import lsfusion.server.data.sql.syntax.SQLSyntax;
 import lsfusion.server.data.type.Type;
 import lsfusion.server.language.property.LP;
-import lsfusion.server.logics.action.controller.context.ConnectionService;
 import lsfusion.server.logics.action.controller.context.ExecutionContext;
 import lsfusion.server.logics.property.oraction.PropertyInterface;
 import lsfusion.server.physics.exec.db.controller.manager.DBManager;
 
 import java.io.IOException;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.List;
 import java.util.concurrent.ExecutionException;
 
 public class ExternalDBAction extends CallDBAction {
@@ -29,29 +25,12 @@ public class ExternalDBAction extends CallDBAction {
         if (connectionString.equals("LOCAL"))
             throw new UnsupportedOperationException("EXTERNAL SQL 'LOCAL' is not supported, Use INTERNAL DB instead");
 
-        SQLSyntax syntax = DefaultSQLSyntax.getSyntax(connectionString);
-
-        Connection conn = null;
-        ConnectionService connectionService = context.getConnectionService();
-        if(connectionService != null)
-            conn = connectionService.getSQLConnection(connectionString);
-        else if (connectionString.isEmpty())
-            throw new UnsupportedOperationException("Empty connection string is supported only inside of NEWCONNECTION operator");
-
-        if (conn == null) {
-            conn = DriverManager.getConnection(connectionString);
-            if (connectionService != null)
-                connectionService.putSQLConnection(connectionString, conn);
-        }
-        conn.setReadOnly(false);
-
-        try {
-            readJDBC(context, conn, syntax, OperationOwner.unknown);
+        try (ExecutionContext.ManagedConnection managedConn = context.getSQLConnection(connectionString)) {
+            Connection conn = managedConn.getConnection();
+            conn.setReadOnly(false);
+            readJDBC(context, conn, DefaultSQLSyntax.getSyntax(connectionString), OperationOwner.unknown);
         } catch (IOException | ExecutionException e) {
             throw Throwables.propagate(e);
-        } finally {
-            if (connectionService == null)
-                conn.close();
         }
     }
 }
